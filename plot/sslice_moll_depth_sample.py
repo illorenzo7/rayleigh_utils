1eefede4513c3d69cff66c6f53811fe0f368e1f9
Loren <loren.matilsky@gmail.com>
2019-02-27 17:30:37 -0800

    Adding plot/sslice_moll_fluidvars_sample.py

--- plot/sslice_moll_depth_sample.py
+++ plot/sslice_moll_depth_sample.py
@@ -8,9 +8,8 @@ import sys, os
 sys.path.append(os.environ['co'])
 sys.path.append(os.environ['rapp'])
 from common import get_file_lists, strip_dirname
-from sslice_util import plot_ortho
+from sslice_util import plot_moll
 from rayleigh_diagnostics import Shell_Slices
-from get_parameter import get_parameter
 
 # Get command line arguments
 dirname = sys.argv[1]
@@ -21,6 +20,7 @@ file_list, int_file_list, nfiles = get_file_lists(radatadir)
 minmax = None
 iiter = nfiles - 1 # by default plot the last iteration
 idepth = 0 # by default plot just below the surface
+varname = 'vr' # by default plot the radial velocity
 clon = 0
 clat = 20
 
@@ -32,6 +32,8 @@ for i in range(nargs):
         minmax = float(args[i+1]), float(args[i+2])
     elif (arg == '-d'):
         idepth = int(args[i+1])
+    elif (arg == '-var'):
+        varname = args[i+1]
     elif (arg == '-iter'):
         desired_iter = int(args[i+1])
         iiter = np.argmin(np.abs(int_file_list - desired_iter))
@@ -48,7 +50,7 @@ a = Shell_Slices(radatadir + fname, '')
 
 # Create the plot using subplot axes
 # Offset axes slightly (at the end) to deal with annoying white space cutoff
-fig_width_inches = 6.
+fig_width_inches = 12.
 
 # General parameters for main axis/color bar
 margin_bottom_inches = 1.
@@ -56,7 +58,7 @@ margin_top_inches = 1.
 margin_inches = 1/8
 
 subplot_width_inches = fig_width_inches - 2*margin_inches
-subplot_height_inches = subplot_width_inches
+subplot_height_inches = 0.5*subplot_width_inches
 fig_height_inches = margin_bottom_inches + subplot_height_inches +\
     margin_top_inches
 fig_aspect = fig_height_inches/fig_width_inches
@@ -71,30 +73,19 @@ subplot_width = subplot_width_inches/fig_width_inches
 subplot_height = subplot_height_inches/fig_height_inches
 
 # Loop over depths and make plots
-plotdir = dirname + '/plots/ortho/fluidvars_sample/'
+plotdir = dirname + '/plots/moll/depth_sample/'
 if not os.path.isdir(plotdir):
     os.makedirs(plotdir)
-
-magnetism = get_parameter(dirname, 'magnetism')
-if magnetism:
-    varlist = ['vr_prime', 'vt_prime', 'vp_prime', 's_prime',\
-            'p_prime', 's_prime_sph', 'br', 'bt', 'bp' ]
-else:
-    varlist = ['vr_prime', 'vt_prime', 'vp_prime', 's_prime',\
-            'p_prime', 's_prime_sph']
-
-count = 0
-for varname in varlist: 
-    savename = 'ortho_iter' + fname + ('_depth%02i_' %idepth) +\
-            str(count).zfill(2) + '-' + varname  + '.png'
-    count += 1
-    print('Plotting ortho: ' + varname + (', depth %02i, ' %idepth) +\
+    
+for idepth in range(a.nr):
+    savename = 'moll_' + varname + '_iter' + fname + ('_depth%02i' %idepth) + '.png'
+    print('Plotting moll: ' + varname + (', depth %02i, ' %idepth) +\
           'iter ' + fname + ' ...')
     fig = plt.figure(figsize=(fig_width_inches, fig_height_inches))
     ax = fig.add_axes([margin_x, margin_bottom, subplot_width, subplot_height])
     
-    plot_ortho(fig, ax, a, dirname, varname, idepth=idepth, minmax=minmax,\
-                clon=clon, clat=clat) 
+    plot_moll(fig, ax, a, dirname, varname, idepth=idepth, minmax=minmax,\
+                clon=clon) 
     fig.text(margin_x + 0.5*subplot_width, 1. - 0.5*margin_top,\
             strip_dirname(dirname), ha='center', va='bottom', **csfont, fontsize=14)
     plt.savefig(plotdir + savename, dpi=300)
--- plot/sslice_moll_fluidvars_sample.py
+++ plot/sslice_moll_fluidvars_sample.py
@@ -8,7 +8,7 @@ import sys, os
 sys.path.append(os.environ['co'])
 sys.path.append(os.environ['rapp'])
 from common import get_file_lists, strip_dirname
-from sslice_util import plot_ortho
+from sslice_util import plot_moll
 from rayleigh_diagnostics import Shell_Slices
 from get_parameter import get_parameter
 
@@ -22,7 +22,6 @@ minmax = None
 iiter = nfiles - 1 # by default plot the last iteration
 idepth = 0 # by default plot just below the surface
 clon = 0
-clat = 20
 
 args = sys.argv[2:]
 nargs = len(args)
@@ -48,7 +47,7 @@ a = Shell_Slices(radatadir + fname, '')
 
 # Create the plot using subplot axes
 # Offset axes slightly (at the end) to deal with annoying white space cutoff
-fig_width_inches = 6.
+fig_width_inches = 12.
 
 # General parameters for main axis/color bar
 margin_bottom_inches = 1.
@@ -56,7 +55,7 @@ margin_top_inches = 1.
 margin_inches = 1/8
 
 subplot_width_inches = fig_width_inches - 2*margin_inches
-subplot_height_inches = subplot_width_inches
+subplot_height_inches = 0.5*subplot_width_inches
 fig_height_inches = margin_bottom_inches + subplot_height_inches +\
     margin_top_inches
 fig_aspect = fig_height_inches/fig_width_inches
@@ -71,7 +70,7 @@ subplot_width = subplot_width_inches/fig_width_inches
 subplot_height = subplot_height_inches/fig_height_inches
 
 # Loop over depths and make plots
-plotdir = dirname + '/plots/ortho/fluidvars_sample/'
+plotdir = dirname + '/plots/moll/fluidvars_sample/'
 if not os.path.isdir(plotdir):
     os.makedirs(plotdir)
 
@@ -85,16 +84,16 @@ else:
 
 count = 0
 for varname in varlist: 
-    savename = 'ortho_iter' + fname + ('_depth%02i_' %idepth) +\
+    savename = 'moll_iter' + fname + ('_depth%02i_' %idepth) +\
             str(count).zfill(2) + '-' + varname  + '.png'
     count += 1
-    print('Plotting ortho: ' + varname + (', depth %02i, ' %idepth) +\
+    print('Plotting moll: ' + varname + (', depth %02i, ' %idepth) +\
           'iter ' + fname + ' ...')
     fig = plt.figure(figsize=(fig_width_inches, fig_height_inches))
     ax = fig.add_axes([margin_x, margin_bottom, subplot_width, subplot_height])
     
-    plot_ortho(fig, ax, a, dirname, varname, idepth=idepth, minmax=minmax,\
-                clon=clon, clat=clat) 
+    plot_moll(fig, ax, a, dirname, varname, idepth=idepth, minmax=minmax,\
+                clon=clon) 
     fig.text(margin_x + 0.5*subplot_width, 1. - 0.5*margin_top,\
             strip_dirname(dirname), ha='center', va='bottom', **csfont, fontsize=14)
     plt.savefig(plotdir + savename, dpi=300)
