c98ace259bac76d13aaf7f9c5f460ce15822b344
Loren <loren.matilsky@gmail.com>
2019-07-15 12:31:51 -0700

    First try at making radial collocation points/weights

--- compute/grid_info.py
+++ compute/grid_info.py
@@ -1,46 +1,55 @@
+# This file has been re-purposed on 07/15/2019 to compute the grid
+# information for a Rayleigh run using the domain bounds, 
+# number of radial points in each domain, number of theta points,
+# and whether use_extrema is True or False
+# Computes the Chebyshev (radial) weights in accordance to the code
+# version 0.9.1 as it is NOW, although probably these weights are
+# incorrect when use_extrema = False
+
 import numpy as np
 import sys, os
 sys.path.append(os.environ['rapp'])
 sys.path.append(os.environ['co'])
-from rayleigh_diagnostics import AZ_Avgs
-from common import get_file_lists
-dirname = sys.argv[1]
-datadir = dirname + '/data/'
+#dirname = sys.argv[1]
 
-# Create 'datadir' if it doesn't exist already
-if (not os.path.isdir(datadir)):
-    os.makedirs(datadir)
+def compute_grid_info(domain_bounds, ncheby, nt, use_extrema=False):
+    if not isinstance(ncheby, tuple): # this is probably because
+        # ncheby was specified as nr, not (nr,)
+        ncheby = (ncheby,)
+    ndomains = len(ncheby)
+    nr = np.sum(ncheby)
 
-# Get all the file names in datadir and their integer counterparts
-azdir = dirname + '/AZ_Avgs/'
-radatadir = dirname + '/AZ_Avgs/'
-file_list, int_file_list, nfiles = get_file_lists(radatadir)
+    r = np.zeros(nr)
+    rw = np.zeros(nr)
+    tt = np.zeros(nt)
+    tw = np.zeros(nt)
 
-file_to_use = file_list[-1]
-nargs = len(sys.argv[2:])
-args = sys.argv[2:]
-for i in range(nargs):
-    arg = args[i]
-    if arg == '-use': # use a different iteration number
-        iter_to_use = int(args[i+1])
-        file_to_use = file_list[np.argmin(np.abs(int_file_list - iter_to_use))]
+    # Compute the radial collocation points/weights
+    ir_min, ir_max = 0, ncheby[0] - 1
+    for idomain in range(ndomains):
+        rmin, rmax = domain_bounds[idomain], domain_bounds[idomain+1]
+        nr_loc = ncheby[idomain]
+        x = np.zeros(nr_loc)
+        r_loc = np.zeros(nr_loc)
+        for ix in range(nr_loc):
+            if use_extrema:
+                x[ix] = np.cos(ix*np.pi/(nr_loc - 1))
+            else:
+                x[ix] = np.cos((ix + 0.5)*np.pi/(nr_loc))
+        # Transform x --> r via an affine transformation
+        xmin, xmax = np.min(x), np.max(x)
+        r_loc = rmin + (x - xmin)*(rmax - rmin)/(xmax - xmin)
+        int_scale = 3.*np.pi/(rmax**3. - rmin**3.)*nr_loc*\
+                (rmax - rmin)/(xmax - xmin)
+        rw_loc = int_scale * r_loc**2. * np.sqrt(1. - x**2.)
+        rw_loc[0] *= 0.5 # These multiplications are only justified for
+        rw_loc[-1] *= 0.5 # Guass-Lobatto (use_extrema = True)
 
-print ('Getting grid_info from AZ_Avgs/%s ...' %file_to_use)
-az0 = AZ_Avgs(azdir + file_to_use, '')
+        r[ir_min:ir_max + 1] = r_loc
+        rw[ir_min:ir_max + 1] = rw_loc
 
-rr = az0.radius
-cost = az0.costheta
-sint = az0.sintheta
-tt = np.arccos(cost)
+        if idomain < ndomains - 1:
+            ir_min += ncheby[idomain]
+            ir_max += ncheby[idomain + 1]
 
-# Shell dimensions
-ro = rr[0]
-ri = rr[len(rr) - 1]
-d = ro - ri
-# radial grid in "percent depth"
-rr_depth = (ro - rr)/d
-
-np.save(datadir + 'grid_info.npy',(rr,tt,cost,sint,rr_depth,ri,ro,d))
-
-nr, nt = len(rr), len(tt)
-print ('nr = %i, nth = %i' %(nr, nt))
+    return (r, rw)
