74b8ff61579e5bff5fc2104d47726f898bd7c1ed
Loren Matilsky <loren.matilsky@gmail.com>
2019-07-22 17:11:53 -0600

    Rudimentary routine to generate a custom reference state of tachocline system in the new cref version; added reference_tools.py from Nick's repository

---
+++ compute/hydro_ideal_reference_state/generate_CZ_RZ_reference-quart_newcref.py
@@ -0,0 +1,132 @@
+# Author: Loren Matilsky
+# Created: 05/01/2019
+# Purpose: generate a binary file (for Rayleigh to read) that contains
+# a reference state, consisting of a stable region (stiffness k) underlying 
+# a neutrally stable CZ
+
+# Parameters: output_dir (first argument), 
+# Inner, outer, and transition radii (default ri = 3.4139791e10, 
+# rm = 5e10, ro = 6.5860209e10...for CZ corresponding to 3 density
+# scale heights )
+# nr1 (number of Chebyshevs to use for CZ, -nr1) default 96
+# nr2 (number of Chebyshevs to use for RZ, -nr2) default 64
+# Density at transition boundary (-rhom) default 0.18053428
+# Temperature at transition boundary (-tm) default 2.111256e6
+# Stiffness k (-k) default 2
+# Transition width delta (-delta) (as a fraction of rm) default 0.005
+# gamma (-gam) default 5/3
+# pressure specific heat cp (-cp) default 3.5e8
+# central mass (-M) default M_sun
+
+import numpy as np
+import matplotlib.pyplot as plt
+import sys, os
+from arbitrary_atmosphere import arbitrary_atmosphere
+sys.path.append(os.environ['co'])
+from write_reference import write_reference
+import reference_tools as rt
+
+import basic_constants as bc
+
+# Set default constants
+ri = 4.176e10  # Set RZ width about 0.5x CZ width
+rm = bc.rm
+ro = bc.ro
+cp = bc.cp
+
+Tm = bc.Tm
+pm = bc.pm
+rhom = bc.rhom
+gam = bc.gamma
+k = 2.0
+delta = 0.005*ro
+nr = 5000 # make the grid super-fine by default
+
+# Get directory to save binary files for reference state and heating
+dirname = sys.argv[1]
+
+args = sys.argv[2:]
+nargs = len(args)
+for i in range(nargs):
+    arg = args[i]
+    if arg == '-ri':
+        ri = float(args[i+1])
+    elif arg == '-rm':
+        rm = float(args[i+1])
+    elif arg == '-ro':
+        ro = float(args[i+1])
+    elif arg == '-nr':
+        nr = int(args[i+1])
+    elif arg == '-rhom':
+        rhom = float(args[i+1])        
+    elif arg == '-tm':
+        tm = float(args[i+1])   
+    elif arg == '-k':
+        k = float(args[i+1])  
+    elif arg == '-delta':
+        delta = float(args[i+1])*ro
+    elif arg == '-gam':
+        gam = float(args[i+1]) 
+    elif arg == '-cp':
+        cp = float(args[i+1]) 
+    elif arg == '-M':
+        M = float(args[i+1])  
+    elif arg == '-nr':
+        nr = int(args[i+1])
+
+        
+# First, compute reference state on evenly spaced grid, possibly letting
+# Rayleigh interpolate later    
+print(ri, ro, nr)
+rr = np.linspace(ro, ri, nr)
+
+# Define an entropy profile that is +1 for r < rm, 0 for r > rm, and 
+# continuously differentiable (a quartic) in between
+
+s = np.zeros(nr)
+dsdr = np.zeros(nr)
+d2sdr2 = np.zeros(nr)
+
+for i in range(nr):
+    rloc = rr[i]
+    if rloc <= rm - delta:
+        s[i] = (8.0/15.0)*(k*cp)*(delta/rm) + k*cp*(rloc/rm - 1.0)
+        dsdr[i] = k*cp/rm
+        d2sdr2[i] = 0.0
+    elif rloc > rm - delta and rloc < rm:
+        x = (rloc - rm)/delta
+        s[i] = (k*cp)*(delta/rm)*((2.0/3.0)*x**3.0 - (1.0/5.0)*x**5.0)
+        dsdr[i] = (k*cp/rm)*(1.0 - (1.0 - x**2.0)**2.0)
+        d2sdr2[i] = (4.0/delta)*(k*cp/rm)*(1.0 - x**2.0)*x
+    else:
+        s[i] = 0.0
+        dsdr[i] = 0.0
+        d2sdr2[i] = 0.0
+
+g = bc.G*bc.M/rr**2
+dgdr = -2.0*g/rr
+
+T, rho, p, dlnT, dlnrho, dlnp, d2lnrho =\
+    arbitrary_atmosphere(rr, s, dsdr, d2sdr2, g,\
+                         dgdr, rm, Tm, pm, cp, gam)
+
+thefile = dirname + '/custom_reference_binary'
+
+cref = rt.equation_coefficients(rr)
+buoy = rho*g/cp
+
+cref.set_function(rho, 1)
+cref.set_function(buoy, 2)
+# nu(r) would go --> 3
+cref.set_function(T, 4)
+# kappa(r) would --> 5
+# heating(r) would --> 6
+# eta(r) would --> 7
+cref.set_function(dlnrho, 8)
+cref.set_function(d2lnrho, 9)
+cref.set_function(dlnT, 10)
+cref.set_function(dsdr, 14)
+cref.set_function(s, 15)
+cref.set_function(p, 16)
+
+cref.write(thefile)
