02a97990aa075023711c604220d51eeca489c420
Loren Matilsky <loren.matilsky@gmail.com>
2019-08-05 13:41:26 -0600

    made new routine plot/equation_coefficients; modified plot/transport to actually work

---
+++ plot/equation_coefficients.py
@@ -0,0 +1,79 @@
+# Created: 08/05/2019
+# Author: Loren Matilsky
+
+import matplotlib as mpl
+import numpy as np
+mpl.use('TkAgg')
+import matplotlib.pyplot as plt
+plt.rcParams['mathtext.fontset'] = 'dejavuserif'
+csfont = {'fontname':'DejaVu Serif'}
+
+import sys, os
+sys.path.append(os.environ['rapp'])
+sys.path.append(os.environ['co'])
+sys.path.append(os.environ['idref'])
+
+from common import strip_dirname
+from plotref import plotref
+from reference_tools import equation_coefficients
+from get_parameter import get_parameter
+# Get the run directory on which to perform the analysis
+dirname = sys.argv[1]
+dirname_stripped = strip_dirname(dirname)
+
+# Get other arguments
+xminmax = None
+
+args = sys.argv[2:]
+nargs = len(args)
+for i in range(nargs):
+    arg = args[i]
+    if arg == '-xminmax':
+        xminmax = float(args[i+1]), float(args[i+2])
+
+# Directory with data and plots, make the plotting directory if it doesn't
+# already exist    
+datadir = dirname + '/data/'
+plotdir = dirname + '/plots/'
+if (not os.path.isdir(plotdir)):
+    os.makedirs(plotdir)
+
+eq = equation_coefficients()
+eq.read(dirname + '/equation_coefficients')
+
+# radius
+r = eq.radius
+nr = eq.nr
+
+# thermodynamic variables, including pressure
+T = eq.functions[3, :]
+rho = eq.functions[0, :]
+cp = get_parameter(dirname, 'pressure_specific_heat')
+gam = 5/3
+R = (gam - 1.)*cp/gam
+p = rho*R*T
+
+# heating
+heating = eq.functions[5, :]/(rho*T)
+
+# log derivatives
+dlnrho = eq.functions[7, :]
+d2lnrho = eq.functions[8, :]
+dlnT = eq.functions[9, :]
+dlnp = dlnT + dlnrho
+
+dsdr = eq.functions[13, :]
+# compute s from integral of dsdr:
+s = np.zeros(nr) 
+for i in range(1, nr):
+    s[i] = np.trapz(dsdr[:i+1], x=r[:i+1])
+
+fig, axs = plotref(r, T, rho, p, dlnT, dlnrho, dlnp, s, dsdr,\
+    d2lnrho, color='k', xminmax=xminmax)
+
+plt.tight_layout() 
+    
+axs[0,0].set_title('          ' + dirname_stripped, **csfont)
+    
+plt.savefig(plotdir + dirname_stripped + '_reference_state.png', dpi=300)
+plt.show()
--- plot/transport.py
+++ plot/transport.py
@@ -2,6 +2,7 @@
 # Author: Loren Matilsky
 
 import matplotlib as mpl
+import numpy as np
 mpl.use('TkAgg')
 import matplotlib.pyplot as plt
 plt.rcParams['mathtext.fontset'] = 'dejavuserif'
@@ -13,11 +14,15 @@ sys.path.append(os.environ['co'])
 from get_parameter import get_parameter
 
 from common import strip_dirname
-from plotref import plotref
 from rayleigh_diagnostics import TransportCoeffs
+from common import rsun
+
 # Get the run directory on which to perform the analysis
 dirname = sys.argv[1]
 dirname_stripped = strip_dirname(dirname)
+
+# defaults
+ylog = False
 
 # Directory with data and plots, make the plotting directory if it doesn't
 # already exist    
@@ -26,25 +31,114 @@ plotdir = dirname + '/plots/'
 if (not os.path.isdir(plotdir)):
     os.makedirs(plotdir)
 
+args = sys.argv[2:]
+nargs = len(args)
+for i in range(nargs):
+    arg = args[i]
+    if arg == '-log':
+        ylog = True
+
 t = TransportCoeffs(dirname + '/transport', '')
 r = t.radius
 nu = t.nu
 dlnu = t.dlnu
-k = t.kappa
-dlnk = t.dlnkappa
+kappa = t.kappa
+dlnkappa = t.dlnkappa
 
 magnetism = get_parameter(dirname, 'magnetism')
 if magnetism:
     eta = t.eta
     dlneta = t.dlneta
 
-fig = plt.figure(
-fig, axs = plotref(r, T, rho, p, dlnT, dlnrho, dlnp, s, dsdr,\
-    d2lnrho, color='k')
+if magnetism:
+    figsize = 7., 10.
+    fig, axs = plt.subplots(3, 2, figsize=figsize, sharex=True)
+else:
+    figsize = 7., 7.
+    fig, axs = plt.subplots(2, 2, figsize=figsize, sharex=True)
 
-plt.tight_layout() 
+# Plot transport coeff and log derivatives one-by-one
+lw = 1.
+axs[0,0].plot(r/rsun, nu, linewidth=lw)
+#    axs[1,0].yaxis.set_major_formatter(yfmt)
+axs[0,0].ticklabel_format(scilimits = (0,0), useMathText=True, axis='y')
+axs[0,0].set_ylabel(r'$\nu(r)\ $' +  r'$\rm{[cm^2\ s^{-1}]}$')
+if ylog:
+    axs[0,0].set_yscale('log')
+
+axs[0,1].plot(r/rsun, dlnu, linewidth=lw)
+#    axs[1,0].yaxis.set_major_formatter(yfmt)
+axs[0,1].ticklabel_format(scilimits = (0,0), useMathText=True, axis='y')
+axs[0,1].set_ylabel(r'$d\ln\nu/dr\ $' +  r'$\rm{[cm^{-1}]}$')
+
+axs[1,0].plot(r/rsun, kappa, linewidth=lw)
+#    axs[1,0].yaxis.set_major_formatter(yfmt)
+axs[1,0].ticklabel_format(scilimits = (0,0), useMathText=True, axis='y')
+axs[1,0].set_ylabel(r'$\kappa(r)\ $' +  r'$\rm{[cm^2\ s^{-1}]}$')
+if ylog:
+    axs[1,0].set_yscale('log')
+
+axs[1,1].plot(r/rsun, dlnu, linewidth=lw)
+#    axs[1,0].yaxis.set_major_formatter(yfmt)
+axs[1,1].ticklabel_format(scilimits = (0,0), useMathText=True, axis='y')
+axs[1,1].set_ylabel(r'$d\ln\kappa/dr\ $' +  r'$\rm{[cm^{-1}]}$')
+
+# Get ticks everywhere
+plt.sca(axs[0,0])
+plt.minorticks_on()
+plt.tick_params(top=True, right=True, direction='in', which='both')
+
+plt.sca(axs[1,0])
+plt.minorticks_on()
+plt.tick_params(top=True, right=True, direction='in', which='both')
+
+plt.sca(axs[1,0])
+plt.minorticks_on()
+plt.tick_params(top=True, right=True, direction='in', which='both')
+
+plt.sca(axs[1,1])
+plt.minorticks_on()
+plt.tick_params(top=True, right=True, direction='in', which='both')    
+
+
+if magnetism:
+    axs[2,0].plot(r/rsun, eta, linewidth=lw)
+    #    axs[1,0].yaxis.set_major_formatter(yfmt)
+    axs[2,0].ticklabel_format(scilimits = (0,0), useMathText=True, axis='y')
+    axs[2,0].set_ylabel(r'$\nu(r)\ $' +  r'$\rm{[cm^2\ s^{-1}]}$')
+    if ylog:
+        axs[2,0].set_yscale('log')
+
+    axs[2,1].plot(r/rsun, dlneta, linewidth=lw)
+    #    axs[1,0].yaxis.set_major_formatter(yfmt)
+    axs[2,1].ticklabel_format(scilimits = (0,0), useMathText=True, axis='y')
+    axs[2,1].set_ylabel(r'$d\ln\eta/dr\ $' +  r'$\rm{[cm^{-1}]}$')
     
-axs[0,0].set_title('          ' + dirname_stripped, **csfont)
+    # ticks everywhere
+    plt.sca(axs[2,0])
+    plt.minorticks_on()
+    plt.tick_params(top=True, right=True, direction='in', which='both')
+
+    plt.sca(axs[2,1])
+    plt.minorticks_on()
+    plt.tick_params(top=True, right=True, direction='in', which='both')    
+
+# Put some metadata in the title
+axs[0,0].set_title(dirname_stripped +'\n' + 'transport coefficients',\
+        ha='center', pad=20)
+
+# Label the x axes of the bottom plots
+xlabel = r'$r/R_\odot$'
+if magnetism:
+    axs[2,0].set_xlabel(xlabel)
+    axs[2,1].set_xlabel(xlabel)
+else:
+    axs[1,0].set_xlabel(xlabel)
+    axs[1,1].set_xlabel(xlabel)
+
+# Set the x axis limits
+axs[0,0].set_xlim(np.min(r)/rsun, np.max(r)/rsun)
+plt.subplots_adjust(wspace=0.3)
     
-plt.savefig(plotdir + dirname_stripped + '_reference_state.png', dpi=300)
+plt.savefig(plotdir + dirname_stripped + '_transport.png', dpi=300)
 plt.show()
