29ae2c17aa3d293e0c42482da8da509eba78ea1e
Loren <loren.matilsky@gmail.com>
2019-08-07 20:55:15 -0700

    Updated outer_dsdr to use equation_coefficients but still compute via polytrope by default

--- compute/outer_dsdr.py
+++ compute/outer_dsdr.py
@@ -7,64 +7,72 @@
 # file
 
 import numpy as np
-import sys
+import sys, os
 from polytrope import compute_polytrope
 from common import lsun, rhom, rm, ro
-from read_reference import read_reference
+
+sys.path.append(os.environ['rapp'])
+from reference_tools import equation_coefficients
+from get_parameter import get_parameter
 
 # Set default to compute necessary dsdr to carry out a solar luminosity,
-# for ktop = 3e12, cp = 3.5x10^8, ri, ro = 5, 6.5860209 x 10^10, nrho = 3,
+# for kappa_top = 3e12, cp = 3.5x10^8, ri, ro = 5, 6.5860209 x 10^10,
+# nrho = 3,
 # adiabatic polytrope (n = 1.5), rho_i = 0.18053428 (units all c.g.s.)
 
 lum = lsun
-ktop = 3.0e12
 poly_n = 1.5
-nrho = 3.0
+poly_nrho = 3.0
 polytropic_reference = True
-file_reference = False
+
+# Get directory name
+dirname = sys.argv[1]
 
 # Now change the defaults via command-line arguments
-args = sys.argv[1:]
+args = sys.argv[2:]
 nargs = len(args)
 for i in range(nargs):
     arg = args[i]
     if arg == '-lum':
         lum = float(args[i+1])
-    elif arg == '-ktop':
-        ktop = float(args[i+1])
-    elif arg == '-polyn':
+    elif arg == '-n':
         poly_n = float(args[i+1])
     elif arg == '-nrho':
-        nrho = float(args[i+1])
-    elif arg == '-ref':
+        poly_nrho = float(args[i+1])
+    elif arg == '-file':
         polytropic_reference = False
-        file_reference = True
-        ref_file = args[i+1]
     elif arg == '-rhom':
         rhom = float(args[i+1])
-    elif arg == '-ri':
-        ri = float(args[i+1])
+    elif arg == '-rm':
+        rm = float(args[i+1])
     elif arg == '-ro':
         ro = float(args[i+1])
 
 nr = 128 # this is arbitrary -- the top thermodynamic values will be
          # independent of the number of interior grid points
+         # We just need arrays (nr > 1) for compute_polytrope to work
+         # properly
 
 if polytropic_reference:
-    di = compute_polytrope(rm, ro, nrho, nr, poly_n, rhom)
+    di = compute_polytrope(rm, ro, poly_nrho, nr, poly_n, rhom)
     rho = di['density']
     T = di['temperature']
-elif file_reference:
-    nr, rr, rho, dlnrho, d2lnrho, p, T, dlnT, dsdr, s, g =\
-            read_reference(ref_file)
-    ro = np.max(rr)
+else: # get rho, T from equation_coefficients file
+    eq = equation_coefficients()
+    eq.read(dirname + '/equation_coefficients')
+    rho = eq.functions[0]
+    T = eq.functions[3]
 
+# get kappa_top from main_input
+kappa_top = get_parameter(dirname, 'kappa_top')
+
+# Now compute desired entropy gradient
 flux_top = lum/(4*np.pi*ro**2)
-desired_dsdr = -flux_top/rho[0]/T[0]/ktop
+desired_dsdr = -flux_top/rho[0]/T[0]/kappa_top
 
-print('For lum=%1.3e, ro=%1.7e, ktop=%1.3e' %(lum, ro, ktop))
+print('For lum=%1.3e, ro=%1.7e, kappa_top=%1.3e' %(lum, ro, kappa_top))
 if polytropic_reference:
-    print('and polytropic reference: nrho=%1.1f, poly_n=%1.1f, rhom=%1.7e' %(nrho, poly_n, rhom))
-elif file_reference:
-    print('and custom reference ' + ref_file)
+    print('and polytropic reference: nrho=%1.1f, poly_n=%1.1f, rhom=%1.7e' %(poly_nrho, poly_n, rhom))
+else:
+    print('equation_coefficients file ')
 print ('Set outer_dsdr (dtdr_top) to %1.8e' %desired_dsdr)
