d9799c468d7cee4ebae6dc60ede9b0e98f118f4c
Loren Matilsky <loren.matilsky@gmail.com>
2019-09-29 11:35:40 -0600

    Added some infrastructure for plotting parity vs time, for each m and for each fluid variable

---
+++ compute/timetrace/parity_vs_m.py
@@ -0,0 +1,131 @@
+# Routine to trace Rayleigh G_Avgs data in time
+# Created by: Loren Matilsky
+# On: 09/29/2019
+############################################################################
+# This routine computes the trace in time/spherical harmonic degree m of
+# the power in even/odd modes (for even: l even if m is even, l odd if m is odd;
+# for odd: l odd if m is even, l even if m is odd) for quantities in the 
+# Shell_Spectra data for a particular simulation (typically these are the fluid # variables v-vector, b-vector, S, and P).
+# 
+
+# The final datacube output ('vals') will have shape
+# (ntimes, nm, ndepths, nq, 2)
+
+# Import relevant modules
+import numpy as np
+import pickle
+import sys, os
+sys.path.append(os.environ['rapp'])
+sys.path.append(os.environ['raco'])
+from rayleigh_diagnostics import Shell_Spectra
+from common import get_file_lists, get_desired_range, strip_dirname
+from get_parameter import get_parameter
+
+# Get the name of the run directory
+dirname = sys.argv[1]
+# Get the stripped name to use in file naming
+dirname_stripped = strip_dirname(dirname)
+
+# Find the relevant place to store the data, and create the directory if it
+# doesn't already exist
+datadir = dirname + '/data/'
+if (not os.path.isdir(datadir)):
+    os.makedirs(datadir)
+
+radatadir = dirname + '/Shell_Spectra/'
+
+# Get all the file names in datadir and their integer counterparts
+file_list, int_file_list, nfiles = get_file_lists(radatadir)
+
+# Read in CLAs
+
+args = sys.argv[2:]
+nargs = len(args)
+
+if nargs == 0:
+    index_first, index_last = 0, nfiles - 1  
+    # By default trace over all files
+else:
+    index_first, index_last = get_desired_range(int_file_list, args)
+
+# Set the timetrace savename by the directory, what we are saving, 
+# and first and last iteration files for the trace
+savename = dirname_stripped + '_parity_vs_m_' +\
+        file_list[index_first] + '_' + file_list[index_last] + '.pkl'
+savefile = datadir + savename    
+
+# Read in first Shell_Spectra file for (spectral) grid info
+spec0 = Shell_Spectra(radatadir + file_list[index_first], '')
+
+# Create 1d array of (float) l-values 
+lmax = spec0.lmax
+mmax = spec0.mmax
+nm = spec0.nm
+lvals = np.arange(lmax + 1, dtype='float')
+mvals = np.arange(mmax + 1, dtype='float')
+qv = spec0.qv
+nq = spec0.nq
+rvals = spec0.radius
+nr = spec0.nr
+
+# Trace over the relevant data range
+print ('Considering Shell_Spectra files %s through %s for the trace ...'\
+       %(file_list[index_first], file_list[index_last]))
+
+iter1, iter2 = int_file_list[index_first], int_file_list[index_last]
+
+vals = []
+times = []
+iters = []
+
+for i in range(index_first, index_last + 1):
+    print ('Adding Shell_Spectra/%s to the trace ...' %file_list[i])
+    if i == index_first:
+        spec = spec0
+    else:   
+        spec = Shell_Spectra(radatadir + file_list[i], '')
+
+    local_ntimes = spec.niter
+    for j in range(local_ntimes):
+        vals_loc = spec.vals[:, :, :, :, j]
+        parity_loc = np.zeros((nm, nr, nq, 2)) 
+        # last index: 0 for even, 1 for odd
+        # conveniently, im = mval, il = lval
+        for im in range(nm):
+            if im % 2 == 0: # m is even: for even parity (index = 0), sum over
+                            # even l; for odd parity (index = 1), sum over odd l
+                parity_loc[im, :, :, 0] =\
+                        np.sum(np.abs(vals_loc[::2, im, :, :])**2., axis=0)
+                parity_loc[im, :, :, 1] =\
+                        np.sum(np.abs(vals_loc[1::2, im, :, :])**2., axis=0)
+            elif im % 2 == 1: # m is odd: for even parity (index = 0), sum over
+                            # odd l; for odd parity (index = 1), sum over even l
+                parity_loc[im, :, :, 0] =\
+                        np.sum(np.abs(vals_loc[1::2, im, :, :])**2., axis=0)
+                parity_loc[im, :, :, 1] =\
+                        np.sum(np.abs(vals_loc[::2, im, :, :])**2., axis=0)
+
+        vals.append(parity_loc.tolist())
+        times.append(spec.time[j])
+        iters.append(spec.iters[j])
+
+# Convert lists into arrays
+vals = np.array(vals)
+times = np.array(times)
+iters = np.array(iters)
+
+# Get final shape of "vals" array
+niter = len(iters)
+
+print ('Traced over %i Shell_Spectra slice(s) ...' %niter)
+
+# Save the trace
+print ('Saving file at ' + savefile + ' ...')
+f = open(savefile, 'wb')
+pickle.dump({'vals': vals, 'times': times, 'iters': iters, \
+'iter1': iter1, 'iter2': iter2, 'niter': niter, 'lut': spec0.lut, \
+'qv': qv, 'nq': spec0.nq, 'rvals': rvals, 'rinds': spec0.inds,\
+'nr': spec0.nr, 'lvals': lvals, 'nell': spec0.nell, 'lmax': lmax,\
+'mvals': mvals, 'nm': nm, 'mmax': spec0.mmax},\
+    f, protocol=4)
+f.close()
---
+++ plot/parity_vs_m.py
@@ -0,0 +1,207 @@
+# Author: Loren Matilsky
+# Created: 09/12/2019
+# This script generates parity traces (amplitude-squared parity power (even
+# or odd) vs time for a particular m-value and for a given variable at a
+# a given radius, from the Shell_Spectra data
+# the Rayleigh run directory indicated by [dirname]. To use  time-trace
+# Shell_Spectra file different than the one associated with the 
+# longest averaging range, use -usefile [complete path-name of desired vavg 
+# file]
+# Saves plot in
+# [dirname]_Pspec_l_[varname]_[rval]_[first iter]_[last iter].npy
+
+import matplotlib as mpl
+mpl.use('TkAgg')
+import matplotlib.pyplot as plt
+plt.rcParams['mathtext.fontset'] = 'dejavuserif'
+csfont = {'fontname':'DejaVu Serif'}
+import numpy as np
+import sys, os
+sys.path.append(os.environ['raco'])
+sys.path.append(os.environ['rapp'])
+from varprops import texunits, texlabels, var_indices
+
+from common import strip_dirname, get_widest_range_file, get_iters_from_file,\
+        get_dict, rsun, sci_format, allthrees_start
+
+from plotcommon import default_axes_1by1, axis_range
+from get_parameter import get_parameter
+
+# Get directory name and stripped_dirname for plotting purposes
+dirname = sys.argv[1]
+dirname_stripped = strip_dirname(dirname)
+
+# Directory with data and plots, make the plotting directory if it doesn't
+# already exist    
+datadir = dirname + '/data/'
+
+# Set defaults
+varname = 'bp'
+desired_rvals = ['all']
+desired_mval = 0.
+ir_vals = None
+showplot = False
+rnorm = None
+minmax = None
+the_file = get_widest_range_file(datadir, 'parity_vs_m')
+
+# Read command-line arguments (CLAs)
+args = sys.argv[2:]
+nargs = len(args)
+for i in range(nargs):
+    arg = args[i]
+    if arg == '-var':
+        varname = args[i+1]
+    elif arg == '-rvals':
+        my_str = args[i+1].split()
+        desired_rvals = []
+        for j in range(len(my_str)):
+            desired_rvals.append(float(my_str[j]))
+    elif arg == '-ir':
+        my_str = args[i+1].split()
+        ir_vals = []
+        for j in range(len(my_str)):
+            ir_vals.append(int(my_str[j]))
+    elif arg == '-usefile':
+        Shell_Spectra_file = args[i+1]
+        Shell_Spectra_file = Shell_Spectra_file.split('/')[-1]
+    elif arg == '-rnorm':
+        rnorm = float(args[i+1])
+    elif arg == '-minmax':
+        minmax = float(args[i+1]), float(args[i+2])
+    elif arg == '-show':
+        showplot = True
+    elif arg == '-mval':
+        desired_mval = float(args[i+1])
+
+# directory for plots (depends on whether logscale = True, so do this after
+# command-line-arguments are read
+plotdir = dirname + '/plots/Pspec/parity_vs_m/'
+if (not os.path.isdir(plotdir)):
+    os.makedirs(plotdir)
+
+# Read in Pspec data
+print ('Reading parity trace data from ' + datadir + the_file +\
+        ' ...')
+di = get_dict(datadir + the_file)
+rvals = di['rvals']/rsun
+mvals = di['mvals']
+qv = di['qv']
+nr = di['nr']
+lvals = di['lvals']
+nell = di['nell']
+iter1, iter2 = di['iter1'], di['iter2']
+
+# Get trace values
+vals = di['vals']
+Om0 = get_parameter(dirname, 'angular_velocity')
+Prot = 2*np.pi/Om0
+times = di['times']/Prot - allthrees_start
+
+if ir_vals is None:
+    ir_vals = []
+    if desired_rvals == ['all']:
+        ir_vals = np.arange(nr)
+    else:
+        for i in range(len(desired_rvals)):
+            rval = desired_rvals[i]
+            ir_vals.append(np.argmin(np.abs(rvals - rval)))
+
+# Get the m-index
+im = np.argmin(np.abs(mvals - desired_mval))
+
+# What is given is now what is desired
+desired_rvals = rvals[ir_vals]
+
+# Get power associated with desired quantity (should really have a contigency
+# where the routine exits if the desired quantity isn't present
+if not (varname == 'vtot' or varname == 'btot'):
+    desired_qv = var_indices[varname]
+    iq = np.argmin(np.abs(qv - desired_qv))
+    varlabel = texlabels[varname]
+    units = texunits[varname] 
+else:
+    if varname == 'vtot':
+        desired_qv_vals = [1, 2, 3]
+        varlabel = r'$|\mathbf{v}|$'
+        units = r'$\rm{m}\ \rm{s}^{-1}$'
+    elif varname == 'btot':
+        desired_qv_vals = [801, 802, 803]
+        varlabel = r'$|\mathbf{B}|$'
+        units = r'$\rm{G}$'
+    iq_vals = []
+    for desired_qv in desired_qv_vals:
+        iq_vals.append(np.argmin(np.abs(qv - desired_qv)))
+
+for ir in range(len(ir_vals)):
+    rval =  desired_rvals[ir]
+    savedir = plotdir + varname + ('/rval%0.3f/' %rval)
+    if (not os.path.isdir(savedir)):
+        os.makedirs(savedir)
+
+    savename = savedir + ('m%04d_' %im) +\
+            str(iter1).zfill(8) + '_' + str(iter2).zfill(8) + '.png'
+    print('Plotting parity_vs_time: ' + varname +\
+            (', r/rsun = %0.3f (ir = %02i), ' %(rval, ir_vals[ir])) + ' ...')
+    if varname == 'vtot' or varname == 'btot':
+        parity_even = vals[:, im, ir, iq_vals[0], 0] +\
+                vals[:, im, ir, iq_vals[1], 0] + vals[:, im, ir, iq_vals[2], 0]
+        parity_odd = vals[:, im, ir, iq_vals[0], 1] +\
+                vals[:, im, ir, iq_vals[1], 1] + vals[:, im, ir, iq_vals[2], 1]
+        parity_even_allm = np.sum(vals[:, :, ir, iq_vals[0], 0] +\
+                vals[:, :, ir, iq_vals[1], 0] +\
+                vals[:, :, ir, iq_vals[2], 0], axis=1)
+        parity_odd_allm = np.sum(vals[:, :, ir, iq_vals[0], 1] +\
+                vals[:, :, ir, iq_vals[1], 1] +\
+                vals[:, :, ir, iq_vals[2], 1], axis=1)
+    else:
+        parity_even = vals[:, im, ir, iq, 0]
+        parity_odd = vals[:, im, ir, iq, 1]
+        parity_even_allm = np.sum(vals[:, :, ir, iq, 0], axis=1)
+        parity_odd_allm = np.sum(vals[:, :, ir, iq, 1], axis=1)
+
+    power_allm = parity_even_allm + parity_odd_allm
+    # compute the actual parity
+    parity = (parity_even - parity_odd)/(parity_even + parity_odd)
+
+    # Now make the plot, finally
+    plt.figure(figsize=(7.25, 3))
+    lw = 0.3
+    fs = 7
+    plt.plot(times, parity_even/(parity_even + parity_odd), label='even',\
+            linewidth=lw)
+    plt.plot(times, parity_odd/(parity_even + parity_odd), label='odd',\
+            linewidth=lw)
+    plt.plot(times, parity, label='parity', linewidth=lw)
+
+    # set bounds
+    plt.xlim(times[0], times[-1])
+    plt.ylim(-1, 1)
+
+    # make legend
+    plt.legend(fontsize=fs)
+
+    # label axes
+    plt.xlabel('time (rotations)', fontsize=fs)
+
+    # Get ticks everywhere
+    plt.minorticks_on()
+    plt.tick_params(top=True, right=True, direction='in', which='both')
+
+    # Make title
+    title = varlabel + '     ' + (r'$r/R_\odot\ =\ %0.3f$' %rval) +\
+            '     ' + (r'$m = %i$' %im) + '\n' +\
+            ('%08i to %08i' %(iter1, iter2))    
+    plt.title(title, fontsize=fs)
+    # set tick fontsize
+    plt.tick_params(labelsize=fs)
+
+    # Final command
+    plt.tight_layout()
+
+    print ('Saving ' + plotdir + savename + ' ...')
+
+    plt.savefig(savename, dpi=300)
+    if showplot:
+        plt.show()
+    plt.close()
