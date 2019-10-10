0e86366ef0ae78a5ed1643d105353cb7211f736e
Loren Matilsky <loren.matilsky@gmail.com>
2019-10-10 11:45:53 -0600

    Changed compute/print_translated_times to take argument -sub and work in time frame starting from allthrees_start

--- compute/print_translated_times.py
+++ compute/print_translated_times.py
@@ -6,11 +6,22 @@
 
 import sys
 from translate_times import translate_times
+from common import allthrees_start
+import numpy as np
 
 # Get the run directory on which to perform the analysis
 dirname = sys.argv[1]
 args = sys.argv[2:]
 nargs = len(args)
+
+# See if user wants to work in frame where allthrees_start has been subtracted
+# from time
+sub = False
+for i in range(nargs):
+    arg = args[i]
+    if arg == '-sub':
+        sub = True
+
 for i in range(nargs):
     arg = args[i]
     if arg == '-sec': 
@@ -30,9 +41,16 @@ for i in range(nargs):
         val_day = di['val_day']; val_prot = di['val_prot']
     elif arg == '-prot':
         time = float(args[i+1])
+        if sub:
+            time += allthrees_start
         di = translate_times(time, dirname, translate_from='prot')
         val_sec = di['val_sec']; val_iter = di['val_iter']
         val_day = di['val_day']; val_prot = di['val_prot']
+
+if sub:
+    val_sec -= (allthrees_start*2*np.pi/8.61e-6)
+    val_day -= (allthrees_start*2*np.pi/8.61e-6/86400)
+    val_prot -= allthrees_start
 
 print ('%1.6e sec   = %08i iter   = %.2f day    = %.3f Prot'\
         %(val_sec, val_iter, val_day, val_prot))
