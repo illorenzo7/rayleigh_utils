663c8a5ca933099055885c08a325189a891dcf0a
Loren <loren.matilsky@gmail.com>
2020-04-14 16:13:24 -0700

    Bare bones routines for computing and plotting a variety of length scales

---
+++ plot/length_scales.py
@@ -0,0 +1,155 @@
+###############################################
+# Author: Loren Matilsky
+# Date created: 04/14/2020
+#
+# This script plots varius length_scales as functions of
+# radius using from the Shell_Avgs/Shell_Spectra data
+
+import matplotlib as mpl
+mpl.use('TkAgg')
+import matplotlib.pyplot as plt
+plt.rcParams['mathtext.fontset'] = 'dejavuserif'
+csfont = {'fontname':'DejaVu Serif'}
+import numpy as np
+import sys, os
+sys.path.append(os.environ['rapp'])
+sys.path.append(os.environ['raco'])
+from common import get_widest_range_file, strip_dirname,\
+        get_iters_from_file, get_dict, rsun
+from get_length_scales import get_length_scales
+from get_parameter import get_parameter
+
+# Get the run directory on which to perform the analysis
+dirname = sys.argv[1]
+dirname_stripped = strip_dirname(dirname)
+
+# Get command-line arguments to adjust the interval of averaging files
+minmax = None
+rnorm = None
+ynorm = None
+rvals = None
+log = False
+
+args = sys.argv[2:]
+nargs = len(args)
+for i in range(nargs):
+    arg = args[i]
+    if arg == '-minmax':
+        minmax = float(args[i+1]), float(args[i+2])
+    elif arg == '-rnorm':
+        rnorm = float(args[i+1])
+    elif arg == '-ynorm':
+        ynorm = float(args[i+1])
+    elif arg == '-rvals':
+        rvals_str = args[i+1].split()
+        rvals = []
+        for rval_str in rvals_str:
+            rvals.append(float(rval_str))
+    elif arg == '-log':
+        log = True
+
+# Directory with plots, make the plotting directory if it doesn't
+# already exist    
+plotdir = dirname + '/plots/'
+if not os.path.isdir(plotdir):
+    os.makedirs(plotdir)
+
+di = get_length_scales(dirname)
+rr = di['rr']
+rr_spec = di['rr_spec']
+nr = di['nr']
+nr_spec = di['nr_spec']
+iter1, iter2 = di['iter1'], di['iter2']
+
+# User can specify what to normalize the radius by
+# By default, normalize by the solar radius
+if rnorm is None:
+    rr_n = rr/rsun
+    rr_spec_n = rr_spec/rsun
+else:
+    rr_n = rr/rnorm                                           
+    rr_spec_n = rr_spec/rnorm
+
+if ynorm is None:
+    ynorm = rsun
+
+scale_names = ['L_rho', 'L_omr', 'L_omh', 'L_om', 'L_vr', 'L_vh', 'L_v']
+tex_names = [r'$L_\rho$', r'$L_{\omega_r^\prime}$', r'$L_{\omega_h^\prime}$', r'$L_{\omega^\prime}$',  r'$L_{v_r^\prime}$', r'$L_{v_h^\prime}$',  r'$L_{v^\prime}$']
+
+magnetism = get_parameter(dirname, 'magnetism')
+if magnetism:
+    scale_names += ['L_J', 'L_Bp', 'L_Bm', 'L_B']
+    tex_names += [r'$L_{J^\prime}$', r'$L_{B_\phi^\prime}$', r'$L_{B_m^\prime}$',  r'$L_{B^\prime}$']
+
+# Make the plot name, labelling the first/last iterations we average over
+savename = dirname_stripped + '_length_scales_' +\
+    str(iter1).zfill(8) + '_' + str(iter2).zfill(8) + '.png'
+
+# Loop through and plot length scales
+count = 0
+for scale_name in scale_names:
+    length_scale = di[scale_name]
+    if len(length_scale) == nr: # plot a line
+        plt.plot(rr_n, length_scale/rsun, label=tex_names[count])
+    elif len(length_scale) == nr_spec:
+        plt.scatter(rr_spec_n, length_scale/rsun,\
+                label=tex_names[count], s=10.)
+    count += 1
+
+# Get ticks everywhere
+plt.minorticks_on()
+plt.tick_params(top=True, right=True, direction='in', which='both')
+
+# Set the x limits
+xmin, xmax = np.min(rr_n), np.max(rr_n)
+delta_x = xmax - xmin
+plt.xlim(xmin, xmax)
+
+# Set the y-limits (the following values seem to "work well" for my models
+# so far...perhaps adjust this in the future. 
+
+if minmax is None:
+    minmax = -0.7, 1.3
+#plt.ylim(minmax[0], minmax[1])
+
+# Label the axes
+if rnorm is None:
+    plt.xlabel(r'$r/R_\odot$',fontsize=12, **csfont)
+else:
+    plt.xlabel(r'r/(%.1e cm)' %rnorm, fontsize=12, **csfont)
+
+# Mark radii if desired
+if not rvals is None:
+    yvals = np.linspace(minmax[0], minmax[1], 100)
+    for rval in rvals:
+        if rnorm is None:
+            rval_n = rval/rsun
+        else:
+            rval_n = rval/rnorm
+#        plt.ylim(ymin, ymax)
+        plt.plot(rval_n + np.zeros(100), yvals, 'k--')
+
+plt.ylabel('length/ynorm (default rsun)',\
+        fontsize=12, **csfont)
+
+# Make title
+plt.title(dirname_stripped + '\n' + 'length scales, ' +\
+          str(iter1).zfill(8) + ' to ' + str(iter2).zfill(8), **csfont)
+
+# Create a see-through legend
+plt.legend(loc='lower left', shadow=True, ncol=3, fontsize=14, framealpha=0.5)
+
+if log:
+    plt.yscale('log')
+
+# Last command
+plt.tight_layout()
+
+# Save the plot
+print ('Saving the eflux plot at ' + plotdir + savename + ' ...')
+plt.savefig(plotdir + savename, dpi=300)
+
+# Show the plot
+plt.show()
+
+plt.show()
