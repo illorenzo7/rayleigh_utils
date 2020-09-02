bf0d64dfa9c3be89bf7d1d0938365e4a3e69e782
Loren <loren.matilsky@gmail.com>
2020-09-02 16:07:56 -0700

    Made bash/count_files count all the files in the main directory before moving on to the subdirectories

--- bash/count_files.sh
+++ bash/count_files.sh
@@ -10,14 +10,20 @@ subdirs=`find $maindir -maxdepth 1 -mindepth 1 -type d -printf '%f\n'`
 # Return to our current location at the end of the script
 currentdir=`pwd`
 
-echo "==============================="
-echo "Scanning $fullpath/"
-echo "==============================="
+echo "===================================================================="
+echo "===================================================================="
+echo "In $fullpath/:"
+tree -a . | tail -n 1
+echo "===================================================================="
+echo "===================================================================="
+
+# First get the number of files in the whole directory
+
 # Loop over subdirectories and count their contents with "tree"
 for subdir in $subdirs
 do
     cd $fullpath/$subdir
-    echo "In $subdir/:"
+    echo "In subdir $subdir/:"
     tree -a . | tail -n 1
     echo "==============================="
 done
