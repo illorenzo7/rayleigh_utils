cdfaab75c9cb433ea392be7ef51696bc68b65451
Loren Matilsky <loren.matilsky@gmail.com>
2020-11-23 23:26:20 -0700

    still getting timetrace/G_Avgs to be parallel

--- compute/timetrace/G_Avgs.py
+++ compute/timetrace/G_Avgs.py
@@ -27,12 +27,13 @@ import time
 # initialize communication
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
-
+print("rank = ", rank)
 # Get the name of the run directory
 dirname = sys.argv[1]
 # Get the stripped name to use in file naming
 dirname_stripped = strip_dirname(dirname)
 
+print('line')
 # Get the Rayleigh data directory
 radatadir = dirname + '/G_Avgs/'
 
@@ -42,6 +43,7 @@ file_list, int_file_list, nfiles = get_file_lists(radatadir)
 # Read in CLAs
 args = sys.argv[2:]
 nargs = len(args)
+print('line')
 
 if nargs == 0:
     index_first, index_last = nfiles - 101, nfiles - 1  
@@ -54,6 +56,7 @@ file_list = file_list[index_first:index_last + 1]
 int_file_list = int_file_list[index_first:index_last + 1]
 nfiles = index_last - index_first + 1
 
+print('line')
 # Get Parallel problem size
 nproc = comm.Get_size()
 nproc_min, nproc_max, n_per_proc_min, n_per_proc_max =\
@@ -81,21 +84,26 @@ if rank == 0:
     # Will need the first G_Avgs file for a number of things
     g0 = G_Avgs(radatadir + file_list[0], '')
 
-    # Will need the last G_Avgs file to get the size of the arrays
-    gf = G_Avgs(radatadir + file_list[-1], '')
+    # Will need nrec (last G_Avgs.niter) to get proper time axis size
+    nrec_last = np.array([0])
+    if nproc > 1: # can't receive from myself!
+        comm.Recv(nrec_last, source=nproc-1)
+        nrec_last = nrec_last[0]
+    else:
+        gf = G_Avgs(radatadir + file_list[0], '')
+        nrec_last = gf.niter
     nrec_full = g0.niter
-    nrec_last = gf.niter
     ntimes = (nfiles - 1)*nrec_full + nrec_last
     nq = g0.nq
 
     # State what we will analyze
     print ('Considering %i G_Avgs files for the trace: %s through %s'\
-        %(ntasks, file_list[0], file_list[-1]))
+        %(nfiles, file_list[0], file_list[-1]))
 
     # will also need first and last iteration files
     iter1, iter2 = int_file_list[0], int_file_list[-1]
 
-    # Initialize empty "vals/times/iters" arrays to store the data
+    # Initialize zero-filled "vals/times/iters" arrays to store the data
     vals = np.zeros((nq, ntimes))
     times = np.zeros(ntimes)
     iters = np.zeros(ntimes, dtype='int')
@@ -114,7 +122,6 @@ if rank == 0:
         comm.Send(my_files, dest=k)
         comm.Send(np.array([nq]), dest=k)
         comm.Send(np.array([nrec_full]), dest=k)
-        comm.Send(np.array([nrec_last]), dest=k)
 
     # Make sure proc 0 also analyzes n_per_proc_min files
     my_ntimes = n_per_proc_min*nrec_full
@@ -125,12 +132,14 @@ if rank == 0:
     my_vals = np.zeros((nq, my_ntimes))
 
     for i in range(my_nfiles):
+        print ("rank %i analyzing G_Avgs/%s"\
+                %(rank, str(my_files[i]).zfill(8)))
         if i == 0:
             g = g0
         else:   
             g = G_Avgs(radatadir + str(my_files[i]).zfill(8), '')
         my_count = 0
-        for j in range(nrec_full):
+        for j in range(g.niter):
             my_vals[:, my_count] = g.vals[j, :]
             my_times[my_count] = g.time[j] 
             my_iters[my_count] = g.iters[j]
@@ -139,8 +148,10 @@ if rank == 0:
 else: # other processes receive the data
     # all processes need nrec_full and nq from proc 0
     nrec_full = np.array([0])
+    print ("about to ")
     comm.Recv(nrec_full, source=0)
     nrec_full = nrec_full[0]
+    print ("rank ", rank, "received nrec_full")
 
     nq = np.array([0])
     comm.Recv(nq, source=0)
@@ -152,9 +163,6 @@ else: # other processes receive the data
         nrec_last = np.array([gf.niter])
         comm.Send(nrec_last, dest=0)
         nrec_last = nrec_last[0]
-
-        # get length of file list and total number of slices from
-        # proc 0 
         my_nfiles = n_per_proc_max
         my_ntimes = (my_nfiles - 1)*nrec_full + nrec_last
     elif rank < nproc_min:
@@ -165,7 +173,7 @@ else: # other processes receive the data
         my_ntimes = my_nfiles*nrec_full
 
     # Get the partial file list from proc 0 
-    my_files = np.empty(my_nfiles, dtype='int')
+    my_files = np.zeros(my_nfiles, dtype='int')
     comm.Recv(my_files, source=0)
    
     # inialize local vals array with proper size
@@ -174,6 +182,8 @@ else: # other processes receive the data
     my_vals = np.zeros((nq, my_ntimes))
 
     for i in range(my_nfiles):
+        print ("rank %i analyzing G_Avgs/%s"\
+                %(rank, str(my_files[i]).zfill(8)))
         if rank == nproc - 1 and i == my_nfiles - 1:
             g = gf
         else:   
@@ -185,10 +195,12 @@ else: # other processes receive the data
             my_iters[my_count] = g.iters[j]
             my_count += 1
 
-# Now collect the results from each process
+# proc 0 now collects the results from each process
 if rank == 0:
     # First, proc 0 already has some results
-    vals[nq, :my_ntimes] = my_vals
+    times[:my_ntimes] = my_times
+    iters[:my_ntimes] = my_iters
+    vals[:, :my_ntimes] = my_vals
     # now get the results from the other processes
     istart = np.copy(my_ntimes)
     for j in range(1, nproc):
@@ -201,25 +213,27 @@ if rank == 0:
         else:
             my_nfiles = n_per_proc_max
             my_ntimes = my_nfiles*nrec_full
-        my_vals = np.zeros((nq, my_ntimes))
         comm.Recv(my_vals, source=j)
-        vals[nq, istart:istart + my_ntimes] = my_vals
+        comm.Recv(my_times, source=j)
+        comm.Recv(my_iters, source=j)
+
+        my_times = np.zeros(my_ntimes)
+        my_iters = np.zeros(my_ntimes, dtype='int')
+        my_vals = np.zeros((nq, my_ntimes))
+        vals[:, istart:istart+my_ntimes] = my_vals
         istart += my_ntimes
-    else: # other processes receive the data
-        comm.Send(my_vals, dest=0)
-        comm.Send(my_times, dest=0)
-        comm.Send(my_iters, dest=0)
+else: # other processes send their data
+    comm.Send(my_times, dest=0)
+    comm.Send(my_iters, dest=0)
+    comm.Send(my_vals, dest=0)
 
-
-
-print ('Traced over %i G_Avgs slice(s)' %count)
-
-# Save the avarage
-print ('Saving file at ' + savefile)
-f = open(savefile, 'wb')
-pickle.dump({'vals': vals, 'times': times, 'iters': iters, 'lut': g0.lut, 'count': count, 'iter1': iter1, 'iter2': iter2, 'qv': g0.qv, 'nq': g0.nq},\
-        f, protocol=4)
-f.close()
-t2 = time.time()
-print(fill_str("total time", lent, char), end="")
-print ("%8.2e s" %(t2 - t1))
+# proc 0 saves the data
+if rank == 0:
+    print ('Traced over %i G_Avgs slice(s)' %ntimes)
+    f = open(savefile, 'wb')
+    pickle.dump({'vals': vals, 'times': times, 'iters': iters, 'lut': g0.lut, 'count': ntimes, 'iter1': iter1, 'iter2': iter2, 'qv': g0.qv, 'nq': g0.nq},\
+            f, protocol=4)
+    f.close()
+    t2 = time.time()
+    print(fill_str("total time", lent, char), end="")
+    print ("%8.2e s" %(t2 - t1))
