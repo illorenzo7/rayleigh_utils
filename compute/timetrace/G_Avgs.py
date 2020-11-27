4eb014300f12d76a11a93983e415fce649e9c4ca
Loren <loren.matilsky@gmail.com>
2020-11-26 17:32:42 -0800

    revamped compute/timetrace/G_Avgs using smarter MPI; removed about 75 lines of code

--- compute/timetrace/G_Avgs.py
+++ compute/timetrace/G_Avgs.py
@@ -1,6 +1,7 @@
 # Routine to trace Rayleigh G_Avgs data in time
 # Created by: Loren Matilsky
 # On: 12/18/2018
+# Parallelized: 11/26/2020
 ##################################################################
 # This routine computes the trace in time of the values in the G_Avgs data 
 # for a particular simulation. 
@@ -11,73 +12,72 @@
 # -range iter1 iter2 (names of start and stop data files; iter2 can be 'last')
 # -centerrange iter0 nfiles (trace about central file iter0 over nfiles)
 
-# Import relevant modules
-from mpi4py import MPI
-import numpy as np
-import pickle
-import sys, os
-sys.path.append(os.environ['rapp'])
-sys.path.append(os.environ['raco'])
-from mpi_util import opt_workload
-from common import get_file_lists, get_desired_range, fill_str,\
-    strip_dirname
-import time
-
-# Get data type and reading function
-from rayleigh_diagnostics import G_Avgs
-dataname = 'G_Avgs'
-reading_func = G_Avgs
-
 # initialize communication
+from mpi4py import MPI
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
-nproc = comm.Get_size()
 
+# Start timing immediately
+comm.Barrier()
 if rank == 0:
+    # timing module
+    import time
+    # info for print messages
+    import sys, os
+    sys.path.append(os.environ['raco'])
+    from common import fill_str
+    lent = 50
+    char = '.'
+    nproc = comm.Get_size()
+    t1_glob = time.time()
+    t1 = t1_glob + 0.0
     if nproc > 1:
         print ('processing in parallel with %i ranks' %nproc)
+        print ('communication initialized')
     else:
         print ('processing in serial with 1 rank')
+    print(fill_str('processes importing necessary modules', lent, char),\
+            end='')
 
-# info for future print messages
-lent = 50
-char = '.'
+# modules needed by everyone
+import numpy as np
+# data type and reading function
+import sys, os
+sys.path.append(os.environ['rapp'])
+from rayleigh_diagnostics import G_Avgs
+reading_func = G_Avgs
+dataname = 'G_Avgs'
 
-# Get the name of the run directory (all procs)
-dirname = sys.argv[1]
-
-# Get the Rayleigh data directory (all procs)
-radatadir = dirname + '/' + dataname + '/'
+if rank == 0:
+    # modules needed only by proc 0 
+    import pickle
+    from common import get_file_lists, get_desired_range, strip_dirname
+    from mpi_util import opt_workload
 
 # Checkpoint and time
+comm.Barrier()
 if rank == 0:
-    for k in range(1, nproc): # check each process has reached this block
-        pi_int = np.array([0])
-        comm.Recv(pi_int, source=k)
-        pi_int = pi_int[0]
-    # they're all at this block, start the clock
-    t1_glob = time.time()
-    t1 = np.copy(t1_glob)
+    t2 = time.time()
+    print ('%8.2e s' %(t2 - t1))
     print(fill_str('proc 0 distributing the file lists', lent, char),\
             end='')
-    for k in range(1, nproc): # tell each process to continue
-        comm.Send(np.array([pi_int]), dest=k)
-else:
-    pi_int = 314
-    comm.Send(np.array([pi_int]), dest=0)
-    pi_int = np.array([0])
-    comm.Recv(pi_int, source=0)
-    pi_int = pi_int[0]
+    t1 = time.time()
 
 # proc 0 reads the file lists and distributes them
 if rank == 0:
+    # Get the name of the run directory
+    dirname = sys.argv[1]
+    print ("rank 0: dirname = ", dirname)
+
+    # Get the Rayleigh data directory
+    radatadir = dirname + '/' + dataname + '/'
+
     # Get all the file names in datadir and their integer counterparts
     file_list, int_file_list, nfiles = get_file_lists(radatadir)
 
-    # Read in CLAs
+    # Get desired file list from command-line arguments
     args = sys.argv[2:]
     nargs = len(args)
-
     if nargs == 0:
         index_first, index_last = nfiles - 101, nfiles - 1  
         # By default trace over the last 100 files
@@ -98,85 +98,54 @@ if rank == 0:
     nrec_full = a0.niter
     nq = a0.nq
 
-    for k in range(1, nproc):
+    # Will need nrec (last niter) to get proper time axis size
+    af = reading_func(radatadir + file_list[-1], '')
+    nrec_last = af.niter
+    ntimes = (nfiles - 1)*nrec_full + nrec_last
+
+    # Distribute file_list and my_ntimes to each process
+    for k in range(nproc - 1, -1, -1):
         # distribute the partial file list to other procs 
-        if k < nproc_min: # first processes analyze fewer files
-            my_nfiles = np.copy(n_per_proc_min)
-            istart = k*my_nfiles
-            iend = istart + my_nfiles
-        else: # last processes analyze more files
+        if k >= nproc_min: # last processes analyzes more files
             my_nfiles = np.copy(n_per_proc_max)
             istart = nproc_min*n_per_proc_min + (k - nproc_min)*my_nfiles
             iend = istart + my_nfiles
+        else: # first processes analyze fewer files
+            my_nfiles = np.copy(n_per_proc_min)
+            istart = k*my_nfiles
+            iend = istart + my_nfiles
 
-        # Send some meta parameters
-        arr = np.array([nproc_min, nproc_max, n_per_proc_min,\
-                n_per_proc_max, nq, nrec_full, my_nfiles])
-        comm.Send(arr, dest=k)
+        if k == nproc - 1: # last process may have nrec_last != nrec_full
+            my_ntimes = (my_nfiles - 1)*nrec_full + nrec_last
+        else:
+            my_ntimes = my_nfiles*nrec_full
 
-        # send the file_list
+        # Get the file list portion for rank k
         my_files = np.copy(int_file_list[istart:iend])
-        comm.Send(my_files, dest=k)
 
-    # Will need nrec (last niter) to get proper time axis size
-    nrec_last = np.array([0])
-    if nproc > 1: # can't receive from myself!
-        comm.Recv(nrec_last, source=nproc-1)
-        nrec_last = nrec_last[0]
-    else:
-        af = reading_func(radatadir + file_list[-1], '')
-        nrec_last = af.niter
-        print ('nrec_last = ', nrec_last)
+        # send  my_files, my_nfiles, my_ntimes if nproc > 1
+        if k >= 1:
+            comm.send([my_files, my_nfiles, my_ntimes], dest=k)
+else: # recieve my_files, my_nfiles, my_ntimes
+    my_files, my_nfiles, my_ntimes = comm.recv(source=0)
 
-    # Make sure proc 0 also analyzes n_per_proc_min files
-    if nproc > 1:
-        my_ntimes = n_per_proc_min*nrec_full
-    else:
-        my_ntimes = (n_per_proc_min - 1)*nrec_full + nrec_last
-    my_nfiles = n_per_proc_min
-    my_files = int_file_list[:my_nfiles]
-
-else: # other processes receive the data
-    # all processes need parameters from proc 0
-    arr = np.zeros(7, dtype='int') 
-    comm.Recv(arr, source=0)
-    nproc_min, nproc_max, n_per_proc_min, n_per_proc_max,\
-            nq, nrec_full, my_nfiles = arr
-
-    # Get the partial file list from proc 0 
-    my_files = np.zeros(my_nfiles, dtype='int')
-    comm.Recv(my_files, source=0)
-
-    if rank == nproc - 1:
-        # last process gets nrec_last
-        af = reading_func(radatadir + str(my_files[-1]).zfill(8), '')
-        nrec_last = af.niter
-        comm.Send(np.array([nrec_last]), dest=0)
-        my_ntimes = (my_nfiles - 1)*nrec_full + nrec_last
-    else:
-        my_ntimes = my_nfiles*nrec_full
+# Broadcast dirname, radatadir, nq
+if rank == 0:
+    meta = [dirname, radatadir, nq]
+else:
+    meta = None
+dirname, radatadir, nq = comm.bcast(meta, root=0)
 
 # Checkpoint and time
+comm.Barrier()
 if rank == 0:
-    for k in range(1, nproc): # check each process has reached this block
-        pi_int = np.array([0])
-        comm.Recv(pi_int, source=k)
-        pi_int = pi_int[0]
-    # they're all at this block, restart the clock
     t2 = time.time()
     print ('%8.2e s' %(t2 - t1))
     print ('Considering %i %s files for the trace: %s through %s'\
         %(nfiles, dataname, file_list[0], file_list[-1]))
+    print ("ntimes for trace = %i" %ntimes)
     print(fill_str('computing', lent, char), end='\r')
     t1 = time.time()
-    for k in range(1, nproc): # tell each process to continue
-        comm.Send(np.array([pi_int]), dest=k)
-else:
-    pi_int = 314
-    comm.Send(np.array([pi_int]), dest=0)
-    pi_int = np.array([0])
-    comm.Recv(pi_int, source=0)
-    pi_int = pi_int[0]
 
 # Now analyze the data
 my_times = np.zeros(my_ntimes)
@@ -187,8 +156,6 @@ my_count = 0
 for i in range(my_nfiles):
     if rank == 0 and i == 0:
         a = a0
-    elif rank == nproc - 1 and i == my_nfiles - 1:
-        a = af
     else:   
         a = reading_func(radatadir + str(my_files[i]).zfill(8), '')
     for j in range(a.niter):
@@ -202,71 +169,37 @@ for i in range(my_nfiles):
                 ('rank 0 %5.1f%% done' %pcnt_done), end='\r')
 
 # Checkpoint and time
+comm.Barrier()
 if rank == 0:
-    for k in range(1, nproc): # check each process has reached this block
-        pi_int = np.array([0])
-        comm.Recv(pi_int, source=k)
-        pi_int = pi_int[0]
-    # they're all at this block, restart the clock
     t2 = time.time()
     print(fill_str('\ncomputing time', lent, char), end='')
     print ('%8.2e s                                 ' %(t2 - t1))
     print(fill_str('rank 0 collecting and saving the results',\
             lent, char), end='')
     t1 = time.time()
-    for k in range(1, nproc): # tell each process to continue
-        comm.Send(np.array([pi_int]), dest=k)
-else:
-    pi_int = 314
-    comm.Send(np.array([pi_int]), dest=0)
-    pi_int = np.array([0])
-    comm.Recv(pi_int, source=0)
-    pi_int = pi_int[0]
 
 # proc 0 now collects the results from each process
 if rank == 0:
-    # Get dimensions of output
-    ntimes = (nfiles - 1)*nrec_full + nrec_last
-
     # Initialize zero-filled 'vals/times/iters' arrays to store the data
     vals = np.zeros((ntimes, nq))
     times = np.zeros(ntimes)
     iters = np.zeros(ntimes, dtype='int')
 
-    # First, proc 0 already has some results
-    times[:my_ntimes] = my_times
-    iters[:my_ntimes] = my_iters
-    vals[:my_ntimes, :] = my_vals
-    # now get the results from the other processes
-    istart = np.copy(my_ntimes)
-    for j in range(1, nproc):
-        if j == nproc - 1:
-            my_nfiles = n_per_proc_max
-            my_ntimes = (my_nfiles - 1)*nrec_full + nrec_last
-        elif j < nproc_min:
-            my_nfiles = n_per_proc_min
-            my_ntimes = my_nfiles*nrec_full
-        else:
-            my_nfiles = n_per_proc_max
-            my_ntimes = my_nfiles*nrec_full
-            
-        my_times = np.zeros(my_ntimes)
-        my_iters = np.zeros(my_ntimes, dtype='int')
-        my_vals = np.zeros((my_ntimes, nq))
-
-        comm.Recv(my_times, source=j)
-        comm.Recv(my_iters, source=j)
-        comm.Recv(my_vals, source=j)
-
+    # Gather the results into these "master" arrays
+    istart = 0
+    for j in range(nproc):
+        if j >= 1:
+            # Get my_ntimes, my_times, my_iters, my_vals from rank j
+            my_ntimes, my_times, my_iters, my_vals = comm.recv(source=j)
         times[istart:istart+my_ntimes] = my_times
         iters[istart:istart+my_ntimes] = my_iters
         vals[istart:istart+my_ntimes, :] = my_vals
-
         istart += my_ntimes
 else: # other processes send their data
-    comm.Send(my_times, dest=0)
-    comm.Send(my_iters, dest=0)
-    comm.Send(my_vals, dest=0)
+    comm.send([my_ntimes, my_times, my_iters, my_vals], dest=0)
+
+# Make sure proc 0 collects all data
+comm.Barrier()
 
 # proc 0 saves the data
 if rank == 0:
