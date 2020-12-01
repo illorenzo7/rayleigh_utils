0ab3873c73c2e207e71ea1cf2b1702c27d36ad47
Loren <loren.matilsky@gmail.com>
2020-11-30 16:55:48 -0800

    finished parallelizing Shell_Avgs; should be straightforward now doing the rest

--- compute/timeavg/Shell_Avgs.py
+++ compute/timeavg/Shell_Avgs.py
@@ -1,7 +1,7 @@
 # Routine to trace Rayleigh G_Avgs data in time
 # Created by: Loren Matilsky
 # On: 12/18/2018
-# Parallelized: 11/26/2020
+# Parallelized: 11/30/2020
 ##################################################################
 # This routine computes the trace in time of the values in the G_Avgs data 
 # for a particular simulation. 
@@ -53,9 +53,6 @@ if rank == 0:
     import pickle
     from common import get_file_lists, get_desired_range, strip_dirname
     from mpi_util import opt_workload
-    from rayleigh_diagnostics import GridInfo
-    from get_parameter import get_parameter
-    from get_eq import get_eq
 
 # Checkpoint and time
 comm.Barrier()
@@ -82,7 +79,7 @@ if rank == 0:
     nargs = len(args)
     if nargs == 0:
         index_first, index_last = nfiles - 101, nfiles - 1  
-        # By default trace over the last 100 files
+        # By default average over the last 100 files
     else:
         index_first, index_last = get_desired_range(int_file_list, args)
 
@@ -98,7 +95,7 @@ if rank == 0:
     # Will need the first data file for a number of things
     a0 = reading_func(radatadir + file_list[0], '')
     nrec_full = a0.niter
-    nq = a0.nq + 3 # will add the three internal energies after
+    nq = a0.nq  # will add the three internal energies after
 
     # Will need nrec (last niter) to get proper time axis size
     af = reading_func(radatadir + file_list[-1], '')
@@ -106,25 +103,12 @@ if rank == 0:
     ntimes = (nfiles - 1)*nrec_full + nrec_last
 
     # get grid information
-    gi = GridInfo(dirname + '/grid_info', '')
-    rw = gi.rweights
-    nr = gi.nr
-    ir_bcz = get_parameter(dirname, 'ncheby')[1] - 1
-    nr_cz = ir_bcz + 1
-    nr_rz = nr - nr_cz
-
-    # get averaging weights for CZ and RZ separately
-    rw_cz = np.copy(rw[:nr_cz])
-    rw_rz = np.copy(rw[nr_cz:])
-    rw_cz /= np.sum(rw_cz)
-    rw_rz /= np.sum(rw_rz)
-    rw = rw.reshape((nr, 1))
-    rw_cz = rw_cz.reshape((nr_cz, 1))
-    rw_rz = rw_rz.reshape((nr_rz, 1))
-
-    # Get rho*T
-    eq = get_eq(dirname)
-    rhot = (eq.density*eq.temperature).reshape((1, nr))
+    rr = a0.radius
+    nr = a0.nr
+    ri, ro = np.min(rr), np.max(rr)
+    d = ro - ri
+    rr_height = (rr - ri)/d
+    rr_depth = (ro - rr)/d
 
     # Distribute file_list and my_ntimes to each process
     for k in range(nproc - 1, -1, -1):
@@ -148,76 +132,46 @@ if rank == 0:
 
         # send  my_files, my_nfiles, my_ntimes if nproc > 1
         if k >= 1:
-            comm.send([my_files, my_nfiles, my_ntimes], dest=k)
+            comm.send([my_files, my_nfiles, my_ntimes, ntimes], dest=k)
 else: # recieve my_files, my_nfiles, my_ntimes
-    my_files, my_nfiles, my_ntimes = comm.recv(source=0)
+    my_files, my_nfiles, my_ntimes, ntimes = comm.recv(source=0)
 
-# Broadcast dirname, radatadir, nq
+# Broadcast dirname, radatadir, nq, etc.
 if rank == 0:
-    meta = [dirname, radatadir, nq, nr, nr_cz, nr_rz, rw, rw_cz, rw_rz,\
-            ir_bcz, rhot]
+    meta = [dirname, radatadir, nq, nr, ntimes]
 else:
     meta = None
-dirname, radatadir, nq, nr, nr_cz, nr_rz, rw, rw_cz, rw_rz, ir_bcz, rhot =\
-    comm.bcast(meta, root=0)
+dirname, radatadir, nq, nr, ntimes = comm.bcast(meta, root=0)
 
 # Checkpoint and time
 comm.Barrier()
 if rank == 0:
     t2 = time.time()
     print ('%8.2e s' %(t2 - t1))
-    print ('Considering %i %s files for the trace: %s through %s'\
+    print ('Considering %i %s files for the average: %s through %s'\
         %(nfiles, dataname, file_list[0], file_list[-1]))
-    print ("ntimes for trace = %i" %ntimes)
+    print ("no. slices = %i" %ntimes)
     print(fill_str('computing', lent, char), end='\r')
     t1 = time.time()
 
 # Now analyze the data
-my_times = np.zeros(my_ntimes)
-my_iters = np.zeros(my_ntimes, dtype='int')
-my_vals = np.zeros((my_ntimes, nq))
-my_vals_cz = np.zeros((my_ntimes, nq))
-my_vals_rz = np.zeros((my_ntimes, nq))
+my_vals = np.zeros((nr, nq))
+# "my_vals will be a weighted sum"
+my_weight = my_ntimes/ntimes
 
-my_count = 0
 for i in range(my_nfiles):
     if rank == 0 and i == 0:
         a = a0
     else:   
         a = reading_func(radatadir + str(my_files[i]).zfill(8), '')
-    for j in range(a.niter):
-        vals_loc = np.copy(a.vals[:, 0, :, j])
-        # add in internal energy
-        inte_loc = rhot*vals_loc[:, a.lut[501]]
-        # top S subtracted
-        inte_loc_subt = rhot*(vals_loc[:, a.lut[501]] -\
-                vals_loc[0, a.lut[501]])
-        # bottom S subtracted
-        inte_loc_subb = rhot*(vals_loc[:, a.lut[501]] -\
-                vals_loc[-1, a.lut[501]])
+    # take mean along the time axis;
+    # get the spherical average (not rms/skew/kurt)
+    my_vals = np.mean(a.vals[:, 0, :, :], axis=2)*my_weight
+    if rank == 0:
+        pcnt_done = (i + 1)/my_nfiles*100.
+        print(fill_str('computing', lent, char) +\
+                ('rank 0 %5.1f%% done' %pcnt_done), end='\r')
 
-        # add in the three energies
-        vals_loc = np.hstack((vals_loc, inte_loc.T, inte_loc_subt.T,\
-                inte_loc_subb.T))
-
-        # Get the values in the CZ/RZ separately
-        vals_cz_loc = vals_loc[:ir_bcz + 1]
-        vals_rz_loc = vals_loc[ir_bcz + 1:]
-
-        gav = np.sum(rw*vals_loc, axis=0)
-        gav_cz = np.sum(rw_cz*vals_cz_loc, axis=0)
-        gav_rz = np.sum(rw_rz*vals_rz_loc, axis=0)
-
-        my_vals[my_count, :] = gav
-        my_vals_cz[my_count, :] = gav_cz
-        my_vals_rz[my_count, :] = gav_rz
-        my_times[my_count] = a.time[j] 
-        my_iters[my_count] = a.iters[j]
-        my_count += 1
-        if rank == 0:
-            pcnt_done = my_count/my_ntimes*100.
-            print(fill_str('computing', lent, char) +\
-                    ('rank 0 %5.1f%% done' %pcnt_done), end='\r')
 # Checkpoint and time
 comm.Barrier()
 if rank == 0:
@@ -230,29 +184,19 @@ if rank == 0:
 
 # proc 0 now collects the results from each process
 if rank == 0:
-    # Initialize zero-filled 'vals/times/iters' arrays to store the data
-    times = np.zeros(ntimes)
-    iters = np.zeros(ntimes, dtype='int')
-    vals = np.zeros((ntimes, nq))
-    vals_cz = np.zeros((ntimes, nq))
-    vals_rz = np.zeros((ntimes, nq))
+    # Initialize zero-filled 'vals' array to store the data
+    vals = np.zeros((nr, nq))
 
-    # Gather the results into these "master" arrays
-    istart = 0
+    # Gather the results into this "master" array
     for j in range(nproc):
         if j >= 1:
             # Get my_ntimes, my_times, my_iters, my_vals from rank j
-            my_ntimes, my_times, my_iters,\
-                    my_vals, my_vals_cz, my_vals_rz = comm.recv(source=j)
-        times[istart:istart+my_ntimes] = my_times
-        iters[istart:istart+my_ntimes] = my_iters
-        vals[istart:istart+my_ntimes, :] = my_vals
-        vals_cz[istart:istart+my_ntimes, :] = my_vals_cz
-        vals_rz[istart:istart+my_ntimes, :] = my_vals_rz
-        istart += my_ntimes
+            my_vals = comm.recv(source=j)
+        # "my_vals" are all weighted: their sum equals the overall average
+        vals += my_vals 
+
 else: # other processes send their data
-    comm.send([my_ntimes, my_times, my_iters,\
-            my_vals, my_vals_cz, my_vals_rz], dest=0)
+    comm.send(my_vals, dest=0)
 
 # Make sure proc 0 collects all data
 comm.Barrier()
@@ -267,21 +211,15 @@ if rank == 0:
     # Set the timetrace savename by the directory, what we are saving,
     # and first and last iteration files for the trace
     dirname_stripped = strip_dirname(dirname)
-    savename = dirname_stripped + '_trace_2dom_G_Avgs_' +\
+    savename = dirname_stripped + '_Shell_Avgs_' +\
             file_list[0] + '_' + file_list[-1] + '.pkl'
     savefile = datadir + savename
 
     # save the data
     # Get first and last iters of files
     iter1, iter2 = int_file_list[0], int_file_list[-1]
-    # append the lut (making the inte, inte_subt, and inte_subb quantities
-    # (4000, 4001, 4002)
-    lut_app = np.array([a0.nq, a0.nq + 1, a0.nq + 2])
-    lut = np.hstack((a0.lut, lut_app))
-    qv_app = np.array([4000, 4001, 4002])
-    qv = np.hstack((a0.qv, qv_app))
     f = open(savefile, 'wb')
-    pickle.dump({'vals': vals, 'vals_cz': vals_cz, 'vals_rz': vals_rz, 'times': times, 'iters': iters, 'lut': lut, 'ntimes': ntimes, 'iter1': iter1, 'iter2': iter2, 'rr': a0.radius, 'nr': a0.nr, 'qv': qv, 'nq': nq}, f, protocol=4)
+    pickle.dump({'vals': vals, 'lut': a0.lut, 'count': ntimes, 'iter1': iter1, 'iter2': iter2, 'qv': a0.qv, 'nq': a0.nq, 'rr': rr, 'rr_depth': rr_depth, 'rr_height': rr_height, 'nr': nr, 'ri': ri, 'ro': ro, 'd': d}, f, protocol=4)
     f.close()
     t2 = time.time()
     print ('%8.2e s' %(t2 - t1))
