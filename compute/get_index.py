b865139fbf8567ee92ea36813465616e5d6bf288
Loren <loren.matilsky@gmail.com>
2020-12-16 12:18:41 -0800

    small update to compute/get_index---make quantiity string treated as case-independent

--- compute/get_index.py
+++ compute/get_index.py
@@ -3,11 +3,11 @@ import sys
 def get_index(lut, quantity):
     found_quantity = False
     for key in lut.keys():
-        if (lut[key] == quantity):
+        if (lut[key].lower() == quantity):
             found_quantity = True
             index = key
-    if (found_quantity):
-        return (index)
+    if found_quantity:
+        return index
     else:
         print ('The quantity ', quantity, ' does not exist in Rayleigh')
         print ('Exiting ...')
