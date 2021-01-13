cf2553cb197cccef3b6544eb7a44ae7357adc906
Loren <loren.matilsky@gmail.com>
2021-01-12 17:17:52 -0800

    added ability to search and replace line by line in compute/text/freplace

--- compute/text/freplace.py
+++ compute/text/freplace.py
@@ -5,12 +5,24 @@
 # 
 # searches [dirname] / files with .ext extension block1.txt
 # and replace with block2.txt
+#
+# if -l is specified, will search line by line; if block1 is contained in
+# line, will replace line with block2 
+# (block1 and block2 should both be one line)
 
 import sys, os
-dirname = sys.argv[1]
-ext = sys.argv[2]
-blockfile1 = sys.argv[3]
-blockfile2 = sys.argv[4]
+args = sys.argv[1:]
+nargs = len(args)
+dirname = args[0]
+ext = args[1]
+blockfile1 = args[2]
+blockfile2 = args[3]
+
+linebyline = False
+for i in range(nargs):
+    arg = args[i]
+    if arg == '-l':
+        linebyline = True
 
 block1 = open(blockfile1).read()
 block2 = open(blockfile2).read()
@@ -37,13 +49,26 @@ print ("-----------------------------------------")
 fnames_changed = []
 for fname in fnames:
     try:
-        st = open(dirname + '/' + fname).read()
-        if block1 in st:
-            fnames_changed.append(dirname + '/' + fname)
-            stnew = st.replace(block1, block2)
-            f = open(dirname + '/' + fname, "w")
-            f.write(stnew)
-            f.close()
+        if linebyline:
+            block1 = block1.replace('\n','')
+            st = open(dirname + '/' + fname).read()
+            lines = open(dirname + '/' + fname).readlines()
+            for line in lines:
+                if block1 in line:
+                    fnames_changed.append(dirname + '/' + fname)
+                    stnew = st.replace(line, block2)
+                    f = open(dirname + '/' + fname, "w")
+                    f.write(stnew)
+                    f.close()
+                    
+        else:
+            st = open(dirname + '/' + fname).read()
+            if block1 in st:
+                fnames_changed.append(dirname + '/' + fname)
+                stnew = st.replace(block1, block2)
+                f = open(dirname + '/' + fname, "w")
+                f.write(stnew)
+                f.close()
     except:
         print (dirname + '/' + fname)
 
