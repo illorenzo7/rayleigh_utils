cf76b61a79c1e117afc37ab033074ff290ef984e
Loren <loren.matilsky@gmail.com>
2021-01-12 20:40:01 -0800

    done with rvals formalism

--- plot/az_half.py
+++ plot/az_half.py
@@ -96,10 +96,23 @@ for i in range(nargs):
     elif arg == '-plotlat':
         plotlatlines = True
     elif arg == '-depths':
-        rvals = []
         strings = args[i+1].split()
         for st in strings:
             rval = ro - float(st)*d
+            rvals.append(rval)
+    elif arg == '-depthscz':
+        rm = domain_bounds[1]
+        dcz = ro - rm
+        strings = args[i+1].split()
+        for st in strings:
+            rval = ro - float(st)*dcz
+            rvals.append(rval)
+    elif arg == '-depthsrz':
+        rm = domain_bounds[1]
+        drz = rm - ri
+        strings = args[i+1].split()
+        for st in strings:
+            rval = rm - float(st)*drz
             rvals.append(rval)
     elif arg == '-rvals':
         rvals = []
--- plot/tach_rprofiles.py
+++ plot/tach_rprofiles.py
@@ -56,10 +56,23 @@ for i in range(nargs):
     elif (arg == '-rnorm'):
         rnorm = float(args[i+1])
     elif arg == '-depths':
-        rvals = []
         strings = args[i+1].split()
         for st in strings:
             rval = ro - float(st)*d
+            rvals.append(rval)
+    elif arg == '-depthscz':
+        rm = domain_bounds[1]
+        dcz = ro - rm
+        strings = args[i+1].split()
+        for st in strings:
+            rval = ro - float(st)*dcz
+            rvals.append(rval)
+    elif arg == '-depthsrz':
+        rm = domain_bounds[1]
+        drz = rm - ri
+        strings = args[i+1].split()
+        for st in strings:
+            rval = rm - float(st)*drz
             rvals.append(rval)
     elif arg == '-rvals':
         rvals = []
