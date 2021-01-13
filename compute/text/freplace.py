98c3411cf384618f8fcdffe9ed0d2315b0d8f488
Loren <loren.matilsky@gmail.com>
2021-01-12 19:50:08 -0800

    made freplace go through subidrectory as well

--- compute/text/freplace.py
+++ compute/text/freplace.py
@@ -5,10 +5,6 @@
 # 
 # searches [dirname] / files with .ext extension block1.txt
 # and replace with block2.txt
-#
-# if -l is specified, will search line by line; if block1 is contained in
-# line, will replace line with block2 
-# (block1 and block2 should both be one line)
 
 import sys, os
 args = sys.argv[1:]
@@ -22,10 +18,13 @@ block1 = open(blockfile1).readlines()
 nlines1 = len(block1)
 block2 = open(blockfile2).read()
 
-fnames_all = os.listdir(dirname)
+fnames_all = []
+for path, subdirs, files in os.walk(dirname):
+    for name in files:
+        fnames_all.append(os.path.join(path, name))
 fnames = []
 for fname in fnames_all:
-    if ext in fname:
+    if ext == fname[-len(ext):]:
         fnames.append(fname)
 
 print ("=========================================")
@@ -47,8 +46,8 @@ print ("-----------------------------------------")
 fnames_changed = []
 for fname in fnames:
     try:
-        st = open(dirname + '/' + fname).read()
-        lines = open(dirname + '/' + fname).readlines()
+        st = open(fname).read()
+        lines = open(fname).readlines()
         nlines = len(lines)
         for i in range(nlines - nlines1 + 1):
             lines_to_compare = lines[i:i+nlines1]
@@ -59,16 +58,16 @@ for fname in fnames:
 
             if the_truth and lines_to_compare != block1:
                 # replace the correponding block
-                fnames_changed.append(dirname + '/' + fname)
+                fnames_changed.append(fname)
                 block_we_compared = ''
                 for j in range(nlines1):
                     block_we_compared += lines_to_compare[j]
                 stnew = st.replace(block_we_compared, block2)
-                f = open(dirname + '/' + fname, "w")
+                f = open(fname, "w")
                 f.write(stnew)
                 f.close()
     except:
-        print (dirname + '/' + fname)
+        print (fname)
 
 print ("=========================================")
 print ("found and replaced in:")
