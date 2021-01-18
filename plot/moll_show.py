d733162493997a7c2bd955d1a4040f982acb193c
Loren <loren.matilsky@gmail.com>
2021-01-17 18:04:54 -0800

    greatly improved plot/moll_show to be able to view and save multiple variables on the same figure

--- plot/moll_show.py
+++ plot/moll_show.py
@@ -36,8 +36,10 @@ iiter = nfiles - 1 # by default plot the last iteration
 ir = 0 # by default plot just below the surface
 rval = None # can also find ir by finding the closest point
             # to a local radius divided by rsun
-varname = 'vr' # by default plot the radial velocity
+varlist = ['vr'] # by default plot the radial velocity
 clon = 0
+saveplot = False
+ncol = 2
 
 args = sys.argv[2:]
 nargs = len(args)
@@ -50,7 +52,7 @@ for i in range(nargs):
     elif arg == '-rval':
         rval = float(args[i+1])
     elif arg == '-var' or arg == '-qval':
-        varname = args[i+1]
+        varlist = args[i+1].split()
     elif arg == '-symlog':
         symlog = True
     elif arg == '-log':
@@ -75,6 +77,11 @@ for i in range(nargs):
         iiter = np.argmin(np.abs(int_file_list - desired_iter))
     elif arg == '-clon':
         clon = float(args[i+1])
+    elif arg == '-save':
+        saveplot = True
+        tag = args[i+1] + '_'
+    elif arg == '-ncol':
+        ncol = int(args[i+1])
 
 # Get the baseline time unit
 rotation = get_parameter(dirname, 'rotation')
@@ -90,34 +97,25 @@ fname = file_list[iiter]
 
 # Read in desired shell slice
 a = Shell_Slices(radatadir + fname, '')
-vals = get_sslice(a, varname, dirname=dirname)
 
-# Get local time (in seconds)
-t_loc = a.time[0]
-
-# Find desired radius (by default ir=0--near outer surface)
-if not rval is None:
-    ir = np.argmin(np.abs(a.radius/rsun - rval))
-field = vals[:, :, ir]
-rval = a.radius[ir]/rsun # in any case, this is the actual rvalue we get
-
-# Display at terminal what we are plotting
-print('Plotting moll: ' + varname + (', r/rsun = %0.3f (ir = %02i), '\
-        %(rval, ir)) + 'iter ' + fname)
-
-# Create the plot using subplot axes
-# Offset axes slightly (at the end) to deal with annoying white space cutoff
-fig_width_inches = 6.
+# figure dimensions
+nplots = len(varlist)
+if ncol > nplots:
+    ncol = nplots # (no point in having empty columns)
+nrow = np.int(np.ceil(nplots/ncol))
+fig_width_inches = 6.*ncol
 
 # General parameters for main axis/color bar
 margin_bottom_inches = 1./2.
-margin_top_inches = 3./4.
+margin_top_inches = 3./4. # margin for big title
+margin_subplot_top_inches = 1/4 # margin to accommodate just subplot titles
 margin_inches = 1./8.
 
-subplot_width_inches = fig_width_inches - 2*margin_inches
+subplot_width_inches = (fig_width_inches - (ncol + 1)*margin_inches)\
+    /ncol
 subplot_height_inches = 0.5*subplot_width_inches
-fig_height_inches = margin_bottom_inches + subplot_height_inches +\
-    margin_top_inches
+fig_height_inches = margin_top_inches + nrow*(margin_bottom_inches +\
+    subplot_height_inches + margin_subplot_top_inches)
 fig_aspect = fig_height_inches/fig_width_inches
 
 # "Non-dimensional" figure parameters
@@ -125,38 +123,74 @@ margin_x = margin_inches/fig_width_inches
 margin_y = margin_inches/fig_height_inches
 margin_bottom = margin_bottom_inches/fig_height_inches
 margin_top = margin_top_inches/fig_height_inches
+margin_subplot_top = margin_subplot_top_inches/fig_height_inches
 
 subplot_width = subplot_width_inches/fig_width_inches
 subplot_height = subplot_height_inches/fig_height_inches
 
-
-# Main axis plot: orthographic projection
+# Main figure
 fig = plt.figure(figsize=(fig_width_inches, fig_height_inches))
-ax = fig.add_axes([margin_x, margin_bottom, subplot_width, subplot_height])
 
-plot_moll(field, a.costheta, fig=fig, ax=ax, minmax=minmax, clon=clon,\
-        varname=varname, symlog=symlog, logscale=logscale) 
+# loop over desired vars and make plots
+for iplot in range(nplots):
+    varname = varlist[iplot]
+    ax_left = margin_x + (iplot%ncol)*(subplot_width + margin_x)
+    ax_bottom = 1. - margin_top - margin_subplot_top -\
+            subplot_height - (iplot//ncol)*(subplot_height +\
+            margin_subplot_top + margin_bottom)
+    ax = fig.add_axes((ax_left, ax_bottom, subplot_width, subplot_height))
 
-# Make title
-ax_xmin, ax_xmax, ax_ymin, ax_ymax = axis_range(ax)
-ax_delta_x = ax_xmax - ax_xmin
-ax_delta_y = ax_ymax - ax_ymin
-ax_center_x = ax_xmin + 0.5*ax_delta_x    
+    vals = get_sslice(a, varname, dirname=dirname)
 
-if rotation:
-    time_string = ('t = %.1f ' %(t_loc/time_unit)) + time_label +\
-            ' (1 ' + time_label + (' = %.2f days)'\
-            %(time_unit/86400.))
-else:
-    time_string = ('t = %.3f ' %(t_loc/time_unit)) + time_label +\
-            ' (1 ' + time_label + (' = %.1f days)'\
-            %(time_unit/86400.))
-varlabel = texlabels.get(varname, 'qval = ' + varname)
+    # Get local time (in seconds)
+    t_loc = a.time[0]
 
-title = dirname_stripped +\
-    '\n' + r'$\rm{Mollweide}$' + '     '  + time_string +\
-    '\n' + varlabel + '     ' + (r'$r/R_\odot\ =\ %0.3f$' %rval)
-fig.text(ax_center_x, ax_ymax + 0.02*ax_delta_y, title,\
-     verticalalignment='bottom', horizontalalignment='center',\
-     fontsize=10, **csfont)   
+    # Find desired radius (by default ir=0--near outer surface)
+    if not rval is None:
+        ir = np.argmin(np.abs(a.radius/rsun - rval))
+    field = vals[:, :, ir]
+    rval = a.radius[ir]/rsun # in any case, this is the actual rvalue we get
+
+    # Display at terminal what we are plotting
+    print('Plotting moll: ' + varname + (', r/rsun = %0.3f (ir = %02i), '\
+            %(rval, ir)) + 'iter ' + fname)
+
+    plot_moll(field, a.costheta, fig=fig, ax=ax, minmax=minmax, clon=clon,\
+            varname=varname, symlog=symlog, logscale=logscale) 
+
+    # label the subplot
+    varlabel = texlabels.get(varname, 'qval = ' + varname)
+    ax.set_title(varlabel, verticalalignment='bottom', **csfont)
+
+    # Make title
+    if iplot == 0:
+        ax_xmin, ax_xmax, ax_ymin, ax_ymax = axis_range(ax)
+        ax_delta_x = ax_xmax - ax_xmin
+        ax_delta_y = ax_ymax - ax_ymin
+        ax_center_x = ax_xmin + 0.5*ax_delta_x    
+
+        if rotation:
+            time_string = ('t = %.1f ' %(t_loc/time_unit)) + time_label +\
+                    ' (1 ' + time_label + (' = %.2f days)'\
+                    %(time_unit/86400.))
+        else:
+            time_string = ('t = %.3f ' %(t_loc/time_unit)) + time_label +\
+                    ' (1 ' + time_label + (' = %.1f days)'\
+                    %(time_unit/86400.))
+
+        title = dirname_stripped +\
+            '\n' + r'$\rm{Mollweide}$' + '     '  + time_string +\
+            '\n' + (r'$r/R_\odot\ =\ %0.3f$' %rval)
+        fig.text(ax_center_x, ax_ymax + 0.02*ax_delta_y +\
+                margin_subplot_top, title,\
+             verticalalignment='bottom', horizontalalignment='center',\
+             fontsize=10, **csfont)   
+if saveplot:
+    plotdir = dirname + '/plots/moll/'
+    if not os.path.isdir(plotdir):
+        os.makedirs(plotdir)
+    savefile = plotdir + 'moll_' + tag + fname + '.png'
+    print ('Saving plot at ' + savefile)
+    plt.savefig(savefile, dpi=300)
+# always show
 plt.show()   
