87832aedaa994ec6880d22a3f72bc2aa46f8c4db
Loren <loren.matilsky@gmail.com>
2021-01-17 16:16:54 -0800

    made compute/text/freplace find and replace all occurrences of string1 with string 2 (as opposed to block1 and block2); renamed old routin freplace_byline

--- compute/text/freplace_byline.py
+++ compute/text/freplace_byline.py
@@ -11,8 +11,12 @@ args = sys.argv[1:]
 nargs = len(args)
 dirname = args[0]
 ext = args[1]
-string1 = args[2]
-string2 = args[3]
+blockfile1 = args[2]
+blockfile2 = args[3]
+
+block1 = open(blockfile1).readlines()
+nlines1 = len(block1)
+block2 = open(blockfile2).read()
 
 fnames_all = []
 for path, subdirs, files in os.walk(dirname):
@@ -26,12 +30,14 @@ for fname in fnames_all:
 print ("=========================================")
 print ("scanning %s files in %s" %(ext, dirname))
 print ("=========================================")
-print ("replacing all occurrences of ")
+print ("replacing")
 print ("=========================================")
-print (string1)
+for line in block1:
+    print (line, end='')
 print ("with")
 print ("=========================================")
-print (string2)
+for line in block2:
+    print (line, end='')
 print ("=========================================")
 
 print ("cannot open:")
@@ -41,12 +47,26 @@ fnames_changed = []
 for fname in fnames:
     try:
         st = open(fname).read()
-        if string1 in st:
-            fnames_changed.append(fname)
-            stnew = st.replace(string1, string2)
-            f = open(fname, "w")
-            f.write(stnew)
-            f.close()
+        lines = open(fname).readlines()
+        nlines = len(lines)
+        for i in range(nlines - nlines1 + 1):
+            lines_to_compare = lines[i:i+nlines1]
+            the_truth = True
+            for j in range(nlines1):
+                the_truth *= block1[j][:-1] ==\
+                    lines_to_compare[j][:len(block1[j]) - 1]
+
+            if the_truth:
+                # replace the correponding block if block 2 is different
+                block_we_compared = ''
+                for j in range(nlines1):
+                    block_we_compared += lines_to_compare[j]
+                if block2 != block_we_compared:
+                    fnames_changed.append(fname)
+                    stnew = st.replace(block_we_compared, block2)
+                    f = open(fname, "w")
+                    f.write(stnew)
+                    f.close()
     except:
         print (fname)
 
