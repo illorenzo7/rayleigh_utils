fefdb5ca93f76b81ff9a1f580365d26fc5462b22
Loren <loren.matilsky@gmail.com>
2021-01-26 14:45:57 -0800

    changing labels in moll_induction routines

--- plot/moll_induction_breakdown_theta.py
+++ plot/moll_induction_breakdown_theta.py
@@ -40,6 +40,7 @@ clon = 0
 saveplot = False
 ncol = 2
 must_smooth = False
+rootname = 'induction_breakdown_phi'
 
 args = sys.argv[2:]
 nargs = len(args)
@@ -138,20 +139,14 @@ dbtdp = a.vals[:, :, :, a.lut[847], 0]
 dbpdp = a.vals[:, :, :, a.lut[848], 0]
 
 # induction terms
-sh = prime(a.vals[:, :, :, a.lut[1601], 0])
-co = prime(a.vals[:, :, :, a.lut[1602], 0])
-ad = prime(a.vals[:, :, :, a.lut[1603], 0])
-ind_r = sh + co + ad
+sh_t = prime(a.vals[:, :, :, a.lut[1606], 0])
+co_t = prime(a.vals[:, :, :, a.lut[1607], 0])
+ad_t = prime(a.vals[:, :, :, a.lut[1608], 0])
+ind_ = sh_p + co_p + ad_p
 
-sh = prime(a.vals[:, :, :, a.lut[1606], 0])
-co = prime(a.vals[:, :, :, a.lut[1607], 0])
-ad = prime(a.vals[:, :, :, a.lut[1608], 0])
-ind_t = sh + co + ad
-
-sh = prime(a.vals[:, :, :, a.lut[1611], 0])
-co = prime(a.vals[:, :, :, a.lut[1612], 0])
-ad = prime(a.vals[:, :, :, a.lut[1613], 0])
-ind_p = sh + co + ad
+sh_p = prime(a.vals[:, :, :, a.lut[1611], 0])
+co_p = prime(a.vals[:, :, :, a.lut[1612], 0])
+ad_p = prime(a.vals[:, :, :, a.lut[1613], 0])
 
 # will need some reference state stuff
 rr = a.radius.reshape((1, 1, a.nr))
@@ -163,40 +158,12 @@ cott = cost/sint
 # now we need cost to be 1D
 cost = a.costheta
 
-# collect the terms
-terms_r = [br, ind_r]
-terms_r.append(prime(dvrdt*bt + vr*dbtdt))
-terms_r.append(-prime(dvtdt*br + vt*dbrdt))
-terms_r.append(-prime(dvpdp*br + vp*dbrdp))
-terms_r.append(prime(dvrdp*bp + vr*dbpdp))
-terms_r.append(prime(cott/rr*(vr*bt - vt*br)))
-
-terms_t = [bt, ind_t]
-terms_t.append(prime(dvtdp*bp + vt*dbpdp))
-terms_t.append(-prime(dvpdp*bt + vp*dbtdp))
-terms_t.append(-prime(dvrdr*bt + vr*dbtdr))
-terms_t.append(prime(dvtdr*br + vt*dbrdr))
-terms_t.append(prime(1./rr*(vt*br - vr*bt)))
-
-terms_p = [bp, ind_p]
-terms_p.append(prime(dvpdr*br + vp*dbrdr))
-terms_p.append(prime(-vr*dbpdr - dvrdr*bp))
-terms_p.append(prime(dvpdt*bt + vp*dbtdt))
-terms_p.append(prime(-dvtdt*bp - vt*dbpdt))
-terms_p.append(prime(1./rr*(vp*br - vr*bp)))
-
-# term labels
-labels_r = ['B_r', '[del X (v X B)]_r', '(d/dT)(vr*bt)',\
-    '-(d/dT)(vt*br)', '-(d/dP)(vp*br)', '(d/dP)(vr*bp)',\
-    '(cott/r)(vr*bt - vt*br)', 'sum last 5']
-
-labels_t = ['B_theta', '[del X (v X B)]_theta', '(d/dP)(vt*bp)',\
-    '-(d/dP)(vp*bt)', '-(d/dr)(vr*bt)', '(d/dr)(vt*br)',\
-    '(1/r)(vt*br - vr*bt)', 'sum last 5']
-
-labels_p = ['B_phi', '[del X (v X B)]_phi', '(d/dr)(vt*br)',\
-    '-(d/dr)(vr*bt)', '(1/r)(d/dt)(vp*bt)', '-(1/r)(d/dt)(vt*bp)',\
-    '(1/r)(vp*br - vr*bp)', 'sum last 5']
+ind_p_dr1 = prime(br*dvpdr + dbrdr*vp)
+ind_p_dr2 = prime(-vr*dbpdr - dvrdr*bp)
+ind_p_dt1 = prime(dvpdt*bt + vp*dbtdt)
+ind_p_dt2 = prime(-dvtdt*bp - vt*dbpdt)
+ind_p_curv = prime(1./rr*(vp*br - vr*bp))
+ind_p_tot = ind_p_dr1 + ind_p_dr2 + ind_p_dt1 + ind_p_dt2 + ind_p_curv
 
 # Other info from the shell slice
 t_loc = a.time[0]
@@ -208,8 +175,14 @@ rval = a.radius[ir]/rsun # in any case, this is the actual rvalue we get
 # free some memory
 del a
 
+# collect the terms
+terms = [bp, ind_p, ind_p_dr1, ind_p_dr2, ind_p_dt1, ind_p_dt2, ind_p_curv, ind_p_tot]
+labels = ['B_phi', '[del X (v X B)]_phi', '(d/dr)(vt*br)', '-(d/dr)(vr*bt)',\
+        '(1/r)(d/dt)(vp*bt)', '-(1/r)(d/dt)(vt*bp)', '(1/r)(vp*br - vr*bp)', 'sum last 4']
+
+print ("rms(exact - sum)/rms(exact) = ", rms(terms[1] - terms[-1])/rms(terms[1]))
 # figure dimensions
-nplots = len(terms_r)
+nplots = len(terms)
 if ncol > nplots:
     ncol = nplots # (no point in having empty columns)
 nrow = np.int(np.ceil(nplots/ncol))
@@ -238,67 +211,60 @@ margin_subplot_top = margin_subplot_top_inches/fig_height_inches
 subplot_width = subplot_width_inches/fig_width_inches
 subplot_height = subplot_height_inches/fig_height_inches
 
-# make (3) plots
-for terms, labels, suf in [(terms_r, labels_r, '_r'), (terms_t, labels_t, '_theta'), (terms_p, labels_p, '_phi')]:
-    rootname = 'induction_breakdown' + suf
-    term_tot = np.zeros_like(terms[0])
-    for i in range(2, nplots):
-        term_tot += terms[i]
-    terms.append(term_tot)
+# Main figure
+fig = plt.figure(figsize=(fig_width_inches, fig_height_inches))
+    
+print('Plotting moll: ' + rootname + (', r/rsun = %0.3f (ir = %02i), '\
+        %(rval, ir)) + 'iter ' + fname)
 
-    print ("rms(exact - sum)/rms(exact) = ", rms(terms[1] - terms[-1])/rms(terms[1]))
+# loop over desired vars and make plots
+for iplot in range(nplots):
+    ax_left = margin_x + (iplot%ncol)*(subplot_width + margin_x)
+    ax_bottom = 1. - margin_top - margin_subplot_top -\
+            subplot_height - (iplot//ncol)*(subplot_height +\
+            margin_subplot_top + margin_bottom)
+    ax = fig.add_axes((ax_left, ax_bottom, subplot_width, subplot_height))
 
-    # Main figure
-    fig = plt.figure(figsize=(fig_width_inches, fig_height_inches))
-        
-    print('Plotting moll: ' + rootname + (', r/rsun = %0.3f (ir = %02i), '\
-            %(rval, ir)) + 'iter ' + fname)
 
-    # loop over desired vars and make plots
-    for iplot in range(nplots):
-        ax_left = margin_x + (iplot%ncol)*(subplot_width + margin_x)
-        ax_bottom = 1. - margin_top - margin_subplot_top -\
-                subplot_height - (iplot//ncol)*(subplot_height +\
-                margin_subplot_top + margin_bottom)
-        ax = fig.add_axes((ax_left, ax_bottom, subplot_width, subplot_height))
+    if must_smooth and not labels[iplot] in ['B_r', 'B_theta', 'B_phi']:
+        terms[iplot] = smooth(terms[iplot], dlon)
+    field = terms[iplot][:, :, ir]
+    # Display at terminal what we are plotting
+    plot_moll(field, cost, fig=fig, ax=ax, minmax=minmax, clon=clon,\
+            varname=rootname, symlog=symlog, logscale=logscale) 
 
-        if must_smooth and not labels[iplot] in ['B_r', 'B_theta', 'B_phi']:
-            terms[iplot] = smooth(terms[iplot], dlon)
-        field = terms[iplot][:, :, ir]
-        # Display at terminal what we are plotting
-        plot_moll(field, cost, fig=fig, ax=ax, minmax=minmax, clon=clon,\
-                varname=rootname, symlog=symlog, logscale=logscale) 
+    ax.set_title(labels[iplot], verticalalignment='bottom', **csfont)
 
-        ax.set_title(labels[iplot], verticalalignment='bottom', **csfont)
+    # Make title
+    if iplot == 0:
+        ax_xmin, ax_xmax, ax_ymin, ax_ymax = axis_range(ax)
+        ax_delta_x = ax_xmax - ax_xmin
+        ax_delta_y = ax_ymax - ax_ymin
+        ax_center_x = ax_xmin + 0.5*ax_delta_x    
 
-        # Make title
-        if iplot == 0:
-            ax_xmin, ax_xmax, ax_ymin, ax_ymax = axis_range(ax)
-            ax_delta_x = ax_xmax - ax_xmin
-            ax_delta_y = ax_ymax - ax_ymin
-            ax_center_x = ax_xmin + 0.5*ax_delta_x    
+        if rotation:
+            time_string = ('t = %.1f ' %(t_loc/time_unit)) + time_label +\
+                    ' (1 ' + time_label + (' = %.2f days)'\
+                    %(time_unit/86400.))
+        else:
+            time_string = ('t = %.3f ' %(t_loc/time_unit)) + time_label +\
+                    ' (1 ' + time_label + (' = %.1f days)'\
+                    %(time_unit/86400.))
 
-            if rotation:
-                time_string = ('t = %.1f ' %(t_loc/time_unit)) + time_label +\
-                        ' (1 ' + time_label + (' = %.2f days)'\
-                        %(time_unit/86400.))
-            else:
-                time_string = ('t = %.3f ' %(t_loc/time_unit)) + time_label +\
-                        ' (1 ' + time_label + (' = %.1f days)'\
-                        %(time_unit/86400.))
+        title = dirname_stripped +\
+            '\n' + r'$\rm{Mollweide}$' + '     '  + time_string +\
+            '\n' + (r'$r/R_\odot\ =\ %0.3f$' %rval)
+        fig.text(ax_center_x, ax_ymax + 0.02*ax_delta_y +\
+                margin_subplot_top, title,\
+             verticalalignment='bottom', horizontalalignment='center',\
+             fontsize=10, **csfont)   
 
-            title = dirname_stripped +\
-                '\n' + r'$\rm{Mollweide}$' + '     '  + time_string +\
-                '\n' + (r'$r/R_\odot\ =\ %0.3f$' %rval)
-            fig.text(ax_center_x, ax_ymax + 0.02*ax_delta_y +\
-                    margin_subplot_top, title,\
-                 verticalalignment='bottom', horizontalalignment='center',\
-                 fontsize=10, **csfont)   
-
-    plotdir = dirname + '/plots/moll/'
-    if not os.path.isdir(plotdir):
-        os.makedirs(plotdir)
-    savefile = plotdir + 'moll_' + rootname + '_' + fname +\
-            ('_rval%0.3f' %rval) + '.png'
-    print ('Saving plot at ' + savefile)
-    plt.savefig(savefile, dpi=300)
+plotdir = dirname + '/plots/moll/'
+if not os.path.isdir(plotdir):
+    os.makedirs(plotdir)
+savefile = plotdir + 'moll_' + rootname + '_' + fname +\
+        ('_rval%0.3f' %rval) + '.png'
+print ('Saving plot at ' + savefile)
+plt.savefig(savefile, dpi=300)
+# always show
+plt.show()   
