37d9d783e347ddf6dbfcb71406a4b6fdae8a1bb1
Loren <loren.matilsky@gmail.com>
2021-02-07 20:54:22 -0800

    some typos in program status output

--- compute/timeavg/Shell_Avgs.py
+++ compute/timeavg/Shell_Avgs.py
@@ -226,5 +226,3 @@ if rank == 0:
     print (make_bold(format_time(t2 - t1_glob)))
     print ('data saved at ')
     print (make_bold(savefile))
-    print ('data saved at ')
-    print (make_bold(savefile))
