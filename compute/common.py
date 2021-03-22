b356d6381089205e21d664101aeb7de72649ddbf
Loren <loren.matilsky@gmail.com>
2021-03-22 13:41:23 -0700

    updated derivative routines to take any dimension array; removed unused integration routines

--- compute/common.py
+++ compute/common.py
@@ -782,100 +782,41 @@ def translate_times(time, dirname, translate_from='iter'):
             'val_day': val_day, 'val_unit': val_unit,\
             'time_unit': time_unit, 'time_label': time_label})
 
-def drad(arr, radius):
-    nt, nr = np.shape(arr)
-    two_dr = np.zeros((1,nr-2))
-    two_dr[0, :] = radius[:nr-2] - radius[2:nr]     
-    deriv = np.zeros_like(arr)
-    deriv[:,1:nr-1] = (arr[:,:nr-2] - arr[:,2:nr])/two_dr
-    deriv[:,0] = deriv[:,1]
-    deriv[:,nr-1] = deriv[:,nr-2]
-    return deriv
+def drad(arr, rr): # this works for any dimension array, as long as
+    # the radial index is the last one
+    # make the output array
+    darr = np.zeros_like(arr)
 
-def dth(arr,theta):
-    nt, nr = np.shape(arr)
-    two_dt = np.zeros((nt-2 ,1))
-    two_dt[:, 0] = theta[:nt-2] - theta[2:nt]     
-    deriv = np.zeros_like(arr)
-    deriv[1:nt-1, :] = (arr[:nt-2,:] - arr[2:nt,:])/two_dt
-    deriv[0, :] = deriv[1, :]
-    deriv[nt-1, :] = deriv[nt-2, :]
-    return deriv
+    # check for repeated radii; they are bad!
+    nr = len(rr)
+    ir_rep = []
+    for ir in range(1, nr):
+        if rr[ir] == rr[ir-1]:
+            ir_rep.append(ir)
 
-def drad_3d(arr, radius):
-    nphi, nt, nr = np.shape(arr)
-    two_dr = np.zeros((1, 1, nr-2))
-    two_dr[0, 0, :] = radius[:nr-2] - radius[2:nr]     
-    deriv = np.zeros_like(arr)
-    deriv[:, :, 1:nr-1] = (arr[:, :, :nr-2] - arr[:, :, 2:nr])/two_dr
-    deriv[:, :, 0] = deriv[:, :, 1]
-    deriv[:, :, nr-1] = deriv[:, :, nr-2]
-    return deriv
+    ir1 = 0
+    ir2 = nr - 1
+    for ir_rep_loc in ir_rep:
+        darr[..., ir1:ir_rep_loc] = np.gradient(arr[..., ir1:ir_rep_loc],\
+                rr[ir1:ir_rep_loc], axis=arr.ndim-1)
+        darr[..., ir_rep_loc] = darr[..., ir_rep_loc-1]
+        ir1 = ir_rep_loc + 1
+    # then do the last segment:
+    darr[..., ir1:ir2+1] = np.gradient(arr[..., ir1:ir2+1],\
+            rr[ir1:ir2+1], axis=arr.ndim-1)
+    return darr
 
-def dth_3d(arr, theta):
-    nphi, nt, nr = np.shape(arr)
-    two_dt = np.zeros((1, nt-2, 1))
-    two_dt[0, :, 0] = theta[:nt-2] - theta[2:nt]     
-    deriv = np.zeros_like(arr)
-    deriv[:, 1:nt-1, :] = (arr[:, :nt-2, :] - arr[:, 2:nt, :])/two_dt
-    deriv[:, 0, :] = deriv[:, 1, :]
-    deriv[:, nt-1, :] = deriv[:, nt-2, :]
-    return deriv
+def dth(arr, theta): # assumes theta falls along the second-to-last axis
+    # if array is > 1D
+    if arr.ndim < 2:
+        return np.gradient(arr, tt)
+    else:
+        return np.gradient(arr, tt, axis=arr.ndim-2)
 
-def dph_3d(arr):
-    nphi, nt, nr = np.shape(arr)
+def dph(arr): # assumes phi falls along first axis
+    nphi = np.shape(arr)[0]
     dphi = 2.*np.pi/nphi
-    arr2 = np.roll(arr, -1, axis=0)
-    arr1 = np.roll(arr, 1, axis=0)
-    deriv = (arr2 - arr1)/2./dphi
-    return deriv
-
-def deriv_1d(x, y):
-    n = len(x)
-    deriv = np.zeros(n)
-    
-    two_dx = x[2:] - x[:n-2]
-    two_dy = y[2:] - y[:n-2]
-    deriv[1:n-1] = two_dy/two_dx
-    deriv[0] = deriv[1]
-    deriv[n-1] = deriv[n-2]
-    return(deriv)
-    
-def int_1d(x, y):
-    n = len(x)
-    dx = x[1:] - x[:n-1]
-    ymid = 0.5*(y[1:] + y[:n-1])
-    return (np.sum(ymid*dx))
-
-def int_r(arr, radius, use_extrema=False):
-    int_arr = np.zeros_like(arr)
-    nt, nr = np.shape(arr)
-    rmin, rmax = np.min(radius), np.max(radius)
-    r, rw = compute_r_grid(nr, rmin, rmax, use_extrema=use_extrema)
-    rw_2d = rw.reshape((1, nr))
-
-    int_arr[:, nr - 1] = 0.  # Integral from rmin to rmin: = 0
-    for ir in range(nr - 1):
-        r0 = r[ir]
-        dr = (1./3.)*(r0**3 - rmin**3)*rw_2d/r**2/np.sum(rw[ir:])
-        int_arr[:, ir] = np.sum((arr*dr)[:, ir:], axis=1)
-    return int_arr
-
-def int_th(arr):
-    int_arr = np.zeros_like(arr)
-    nt, nr = np.shape(arr)
-    tt, tw = compute_theta_grid(nt)
-    cost, sint = np.cos(tt), np.sin(tt)
-
-    int_arr[nt - 1, :] = 0.  # Integral from theta=0 to theta=0: = 0
-
-    for it in range(nt):
-        theta0 = tt[it]
-        dt = (1. - cost[it])*tw*np.sin(tt)/np.sum(tw[it:])
-        dt_2d = dt.reshape((nt, 1))
-        int_arr[it, :] = np.sum((arr*dt_2d)[it:, :], axis=0)
-
-    return int_arr
+    return np.gradient(arr, dphi, axis=0)
 
 def get_domain_bounds(dirname):
     try:
