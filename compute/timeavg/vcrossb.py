60e155bec542d1742ca0078353c09defc86ff059
Loren <loren.matilsky@gmail.com>
2021-04-13 18:22:48 -0700

    updated vcrossb

--- compute/timeavg/vcrossb.py
+++ compute/timeavg/vcrossb.py
@@ -1,16 +1,12 @@
-# more breakdown of poloidal terms (separate derivatives) than 
-# induction_terms
-# Created by: Loren Matilsky
-# On: 01/16/2021
 ##################################################################
-# This routine computes the trace in time of the values in the G_Avgs data 
-# for a particular simulation. 
-
-# By default, the routine traces over the last 100 files of datadir, though
-# the user can specify a different range in sevaral ways:
-# -n 10 (last 10 files)
-# -range iter1 iter2 (names of start and stop data files; iter2 can be 'last')
-# -centerrange iter0 nfiles (trace about central file iter0 over nfiles)
+# Routine to average Rayleigh data in time (generic)
+# Author: Loren Matilsky
+# Created: 04/08/2021
+##################################################################
+# This routine computes the time average of v X B components from
+# Rayleigh data (AZ_Avgs, Meridional Slices), breaking up terms 
+# by Reynolds decomposition
+##################################################################
 
 # initialize communication
 from mpi4py import MPI
@@ -45,7 +41,6 @@ import numpy as np
 # data type and reading function
 import sys, os
 sys.path.append(os.environ['rapp'])
-sys.path.append(os.environ['raco'])
 from rayleigh_diagnostics import AZ_Avgs, Meridional_Slices
 reading_func1 = AZ_Avgs
 reading_func2 = Meridional_Slices
@@ -69,27 +64,15 @@ if rank == 0:
 if rank == 0:
     # Get the name of the run directory
     dirname = sys.argv[1]
+    args = sys.argv[2:]
 
     # Get the Rayleigh data directory
     radatadir1 = dirname + '/' + dataname1 + '/'
     radatadir2 = dirname + '/' + dataname2 + '/'
 
     # Get all the file names in datadir and their integer counterparts
-    file_list, int_file_list, nfiles = get_file_lists(radatadir1)
-
-    # Get desired file list from command-line arguments
-    args = sys.argv[2:]
-    nargs = len(args)
-    if nargs == 0:
-        index_first, index_last = nfiles - 101, nfiles - 1  
-        # By default average over the last 100 files
-    else:
-        index_first, index_last = get_desired_range(int_file_list, args)
-
-    # Remove parts of file lists we don't need
-    file_list = file_list[index_first:index_last + 1]
-    int_file_list = int_file_list[index_first:index_last + 1]
-    nfiles = index_last - index_first + 1
+    # Use mer slices, since there are sometimes fewer of them than azavgs
+    file_list, int_file_list, nfiles = get_file_lists(radatadir1, args)
 
     # Get the problem size
     nproc_min, nproc_max, n_per_proc_min, n_per_proc_max =\
@@ -97,32 +80,18 @@ if rank == 0:
 
     # Will need the first data file for a number of things
     a0 = reading_func1(radatadir1 + file_list[0], '')
-    nrec_full = a0.niter
-
-    # Will need nrec (last niter) to get proper time axis size
-    af = reading_func1(radatadir1 + file_list[-1], '')
-    nrec_last = af.niter
-    ntimes = (nfiles - 1)*nrec_full + nrec_last
+    # Make sure you put same number of slices in AZ_Avgs and 
+    # Meridional_Slices, or this won't work
 
     # get grid information
-    rr = a0.radius
-    ri, ro = np.min(rr), np.max(rr)
-    d = ro - ri
-    rr_depth = (ro - rr)/d
-    rr_height = (rr - ri)/d
-    sint = a0.sintheta
-    cost = a0.costheta
-    tt = np.arccos(cost)
-    tt_lat = (np.pi/2 - tt)*180/np.pi
-    nr = a0.nr
-    nt = a0.ntheta
-
-    # compute some derivative quantities for the grid
-    tt_2d, rr_2d = np.meshgrid(tt, rr, indexing='ij')
-    sint_2d = np.sin(tt_2d); cost_2d = np.cos(tt_2d)
-    cott = cost_2d/sint_2d
-    xx = rr_2d*sint_2d
-    zz = rr_2d*cost_2d
+    di_grid = get_grid_info(dirname)
+    nt = di_grid['nt']
+    nr = di_grid['nr']
+    tt = di_grid['tt']
+    rr = di_grid['rr']
+    rr_3d = di_grid['rr_3d']
+    cost_3d = di_grid['cost_3d']
+    sint_3d = di_grid['sint_3d']
 
     # Distribute file_list and my_ntimes to each process
     for k in range(nproc - 1, -1, -1):
@@ -136,50 +105,42 @@ if rank == 0:
             istart = k*my_nfiles
             iend = istart + my_nfiles
 
-        if k == nproc - 1: # last process may have nrec_last != nrec_full
-            my_ntimes = (my_nfiles - 1)*nrec_full + nrec_last
-        else:
-            my_ntimes = my_nfiles*nrec_full
-
         # Get the file list portion for rank k
         my_files = np.copy(int_file_list[istart:iend])
 
         # send  my_files, my_nfiles, my_ntimes if nproc > 1
         if k >= 1:
-            comm.send([my_files, my_nfiles, my_ntimes, ntimes], dest=k)
+            comm.send([my_files, my_nfiles], dest=k)
 else: # recieve my_files, my_nfiles, my_ntimes
-    my_files, my_nfiles, my_ntimes, ntimes = comm.recv(source=0)
+    my_files, my_nfiles = comm.recv(source=0)
 
-# Broadcast dirname, radatadir, nq, etc.
+# Broadcast dirname, radatadirs, rr, etc.
 if rank == 0:
-    meta = [dirname, radatadir1, radatadir2, nt, nr, ntimes, rr_2d,  cott]
+    meta = [dirname, radatadir1, radatadir2, tt, rr, nt, nr, rr_3d, cost_3d, sint_3d, nfiles]
 else:
     meta = None
-dirname, radatadir1, radatadir2, nt, nr, ntimes, rr_2d, cott =\
-        comm.bcast(meta, root=0)
+dirname, radatadir1, radatadir2, tt, rr, nt, nr, rr_3d, cost_3d, sint_3d, nfiles = comm.bcast(meta, root=0)
 
 # Checkpoint and time
 comm.Barrier()
 if rank == 0:
     t2 = time.time()
     print (format_time(t2 - t1))
-    print ('Considering %i %s/%s files for the trace: %s through %s'\
+    print ('Considering %i %s and %s files for the average: %s through %s'\
         %(nfiles, dataname1, dataname2, file_list[0], file_list[-1]))
-    print ("no. slices = %i" %ntimes)
+    print ("no. slices = %i" %nfiles)
     print(fill_str('computing', lent, char), end='\r')
     t1 = time.time()
 
 # Now analyze the data
 nq = 9*3
 my_vals = np.zeros((nt, nr, nq))
-# "my_vals will be a weighted sum"
-my_weight = 1./ntimes
 
 for i in range(my_nfiles):
     a = reading_func1(radatadir1 + str(my_files[i]).zfill(8), '')
     mer = reading_func2(radatadir2 + str(my_files[i]).zfill(8), '')
-    # take mean along the time axis;
 
+    my_weight = 1.0/(nfiles*a.niter)
     for j in range(a.niter):
         # full v
         vr = mer.vals[:, :, :, mer.lut[1], j]
@@ -235,10 +196,10 @@ for i in range(my_nfiles):
             my_vals[:, :, ind_off_fluc + k] = my_vals[:, :, ind_off_full + k] -\
                 my_vals[:, :, ind_off_mean + k]
 
-    if rank == 0:
-        pcnt_done = (i + 1)/my_nfiles*100.
-        print(fill_str('computing', lent, char) +\
-                ('rank 0 %5.1f%% done' %pcnt_done), end='\r')
+        if rank == 0:
+            pcnt_done = (i + 1)/my_nfiles*100.
+            print(fill_str('computing', lent, char) +\
+                    ('rank 0 %5.1f%% done' %pcnt_done), end='\r')
 
 # Checkpoint and time
 comm.Barrier()
@@ -252,16 +213,14 @@ if rank == 0:
 
 # proc 0 now collects the results from each process
 if rank == 0:
-    # Initialize zero-filled 'vals' array to store the data
-    vals = np.zeros((nt, nr, nq))
+    vals = np.zeros((nt, nr, 18))
 
     # Gather the results into this "master" array
     for j in range(nproc):
         if j >= 1:
             # Get my_ntimes, my_times, my_iters, my_vals from rank j
             my_vals = comm.recv(source=j)
-        # "my_vals" are all weighted: their sum equals the overall average
-        vals += my_vals 
+    
 else: # other processes send their data
     comm.send(my_vals, dest=0)
 
@@ -285,7 +244,7 @@ if rank == 0:
     # Get first and last iters of files
     iter1, iter2 = int_file_list[0], int_file_list[-1]
     f = open(savefile, 'wb')
-    pickle.dump({'vals': vals, 'lut': a0.lut, 'count': ntimes, 'iter1': iter1, 'iter2': iter2, 'qv': a0.qv, 'nq': a0.nq,  'rr': rr, 'rr_depth': rr_depth, 'rr_height': rr_height, 'nr': nr, 'ri': ri, 'ro': ro, 'd': d, 'tt': tt, 'tt_lat': tt_lat, 'sint': sint, 'cost': cost,'nt': nt, 'rr_2d': rr_2d, 'tt_2d': tt_2d, 'sint_2d': sint_2d, 'cost_2d': cost_2d, 'xx': xx, 'zz': zz}, f, protocol=4)
+    pickle.dump({'vals': vals}, f, protocol=4)
     f.close()
     t2 = time.time()
     print (format_time(t2 - t1))
@@ -293,5 +252,3 @@ if rank == 0:
     print (make_bold(format_time(t2 - t1_glob)))
     print ('data saved at ')
     print (make_bold(savefile))
-    print ('data saved at ')
-    print (savefile)
