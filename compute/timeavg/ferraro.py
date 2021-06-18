2b46e708b1379a0cc714434deb88489e75ebaf03
Loren <loren.matilsky@gmail.com>
2021-06-18 12:14:21 -0700

    routine to estimate Ferraro's magnetic tension torque

--- compute/timeavg/ferraro.py
+++ compute/timeavg/ferraro.py
@@ -93,13 +93,19 @@ if rank == 0:
     rr = di_grid['rr']
     tt = di_grid['tt']
     rr_2d = di_grid['rr_2d']
+    sint_2d = di_grid['sint_2d']
     cott_2d = di_grid['cott_2d']
     rr_3d = di_grid['rr_3d']
-    cott_3d = di_grid['cott_3d']
+    sint_3d = di_grid['sint_3d']
 
-    # need density derivative (for dvp/dP)
+    # need dynamic viscosity to get omega gradients
     eq = get_eq(dirname)
-    dlnrho = eq.dlnrho.reshape((1, 1, nr))
+    nu = eq.nu.reshape((1, 1, nr))
+    rho = eq.rho.reshape((1, 1, nr))
+    mu = rho*nu
+    prefactor = -1/(rho*nu*rr_3d**2*sint_3d**2)
+    geofactor = rr_3d**2*sint_3d**2/(4*np.pi) # for the angular momentum
+    # fluxes
 
     # Distribute file lists to each process
     for k in range(nproc - 1, -1, -1):
@@ -125,11 +131,11 @@ else: # recieve my_files
 # Broadcast dirname, radatadir, nq, etc.
 if rank == 0:
     meta = [dirname, radatadir1, radatadir2, nt, nr, rr, tt,\
-            rr_2d,  cott_2d, rr_3d, cott_3d, dlnrho, nfiles]
+            rr_2d,  sint_2d, cott_2d, rr_3d, sint_3d, prefactor, geofactor, nfiles]
 else:
     meta = None
 dirname, radatadir1, radatadir2, nt, nr, rr, tt, rr_2d,\
-    cott_2d, rr_3d, cott_3d, dlnrho, nfiles = comm.bcast(meta, root=0)
+    sint_2d, cott_2d, rr_3d, sint_3d, prefactor, geofactor, nfiles = comm.bcast(meta, root=0)
 
 # Checkpoint and time
 comm.Barrier()
@@ -142,7 +148,8 @@ if rank == 0:
     t1 = time.time()
 
 # Now analyze the data
-nq = 72
+nset = 5 # total in a "set" of values
+nq = 3*nset # Reynolds dcomp.
 my_vals = np.zeros((nt, nr, nq))
 
 my_nfiles = len(my_files)
@@ -153,208 +160,63 @@ for i in range(my_nfiles):
 
     my_weight = 1.0/(nfiles*a.niter)
     for j in range(a.niter):
-        # full v
-        vr = mer.vals[:, :, :, mer.lut[1], j]
-        vt = mer.vals[:, :, :, mer.lut[2], j]
-        vp = mer.vals[:, :, :, mer.lut[3], j]
-
-        # full B
+        # full B_pol
         br = mer.vals[:, :, :, mer.lut[801], j]
         bt = mer.vals[:, :, :, mer.lut[802], j]
-        bp = mer.vals[:, :, :, mer.lut[803], j]
 
-        # full poloidal vort. and current (for phi-derivatives)
-        omr = mer.vals[:, :, :, mer.lut[301], j]
-        omt = mer.vals[:, :, :, mer.lut[302], j]
-        jr = mer.vals[:, :, :, mer.lut[1001], j]
-        jt = mer.vals[:, :, :, mer.lut[1004], j]
+        # mean Omega-gradients
+        amom_visc_r = a.vals[:, :, a.lut[1813], j].reshape((1, nt, nr))
+        amom_visc_t = a.vals[:, :, a.lut[1814], j].reshape((1, nt, nr))
+        dOmdr = prefactor*amom_visc_r
+        dOmdt = prefactor*amom_visc_t
 
-        # mean v
-        vr_m = a.vals[:, :, a.lut[1], j].reshape((1, nt, nr))
-        vt_m = a.vals[:, :, a.lut[2], j].reshape((1, nt, nr))
-        vp_m = a.vals[:, :, a.lut[3], j].reshape((1, nt, nr))
-
-        # mean B
+        # mean B_pol
         br_m = a.vals[:, :, a.lut[801], j].reshape((1, nt, nr))
         bt_m = a.vals[:, :, a.lut[802], j].reshape((1, nt, nr))
-        bp_m = a.vals[:, :, a.lut[803], j].reshape((1, nt, nr))
 
-        # mean poloidal vort. and current (for phi-derivatives)
-        omr_m = a.vals[:, :, a.lut[301], j].reshape((1, nt, nr))
-        omt_m = a.vals[:, :, a.lut[302], j].reshape((1, nt, nr))
-        jr_m = a.vals[:, :, a.lut[1001], j].reshape((1, nt, nr))
-        jt_m = a.vals[:, :, a.lut[1004], j].reshape((1, nt, nr))
+        # calculate flux terms
+        Bp_meanshear = (br*dOmdr + bt*dOmdt) 
+        Bp_meanshear_m = (br_m*dOmdr + bt_m*dOmdt) 
+        # still needs to be multiplied by a time scale...
 
-        # full (poloidal) derivatives v
-        dvrdr = drad(vr, rr)
-        dvtdr = drad(vt, rr)
-        dvpdr = drad(vp, rr)
+        # these are angular momentum fluxes from magnetic tension
+        # they include the factor of 4pi (in geofactor)
+        flux_r = np.mean(geofactor*Bp_meanshear*br, axis=0)*my_weight
+        flux_t = np.mean(geofactor*Bp_meanshear*bt, axis=0)*my_weight
 
-        dvrdt = dth(vr, tt)/rr_3d
-        dvtdt = dth(vt, tt)/rr_3d
-        dvpdt = dth(vp, tt)/rr_3d
+        flux_r_m = np.mean(geofactor*Bp_meanshear_m*br_m, axis=0)*my_weight
+        flux_t_m = np.mean(geofactor*Bp_meanshear_m*bt_m, axis=0)*my_weight
 
-        # full (toroidal) derivatives v
-        dvrdp = dvpdr + (1./rr_3d)*vp + omt
-        dvtdp = dvpdt + (cott_3d/rr_3d)*vp - omr
-        dvpdp = -dlnrho*vr - dvrdr - (2./rr_3d)*vr - dvtdt -\
-                (cott_3d/rr_3d)*vt
+        torque_r = drad(flux_r, rr) + (2/rr_2d)*flux_r
+        torque_t = (1/rr_2d)*(dth(flux_t, tt) + cott_2d*flux_t)
+        torque = torque_r + torque_t
 
-        # full (poloidal) derivatives B
-        dbrdr = drad(br, rr)
-        dbtdr = drad(bt, rr)
-        dbpdr = drad(bp, rr)
+        torque_r_m = drad(flux_r_m, rr) + (2/rr_2d)*flux_r_m
+        torque_t_m = (1/rr_2d)*(dth(flux_t_m, tt) + cott_2d*flux_t_m)
+        torque_m = torque_r_m + torque_t_m
 
-        dbrdt = dth(br, tt)/rr_3d
-        dbtdt = dth(bt, tt)/rr_3d
-        dbpdt = dth(bp, tt)/rr_3d
+        # add to my_vals array
+        # first the full terms
+        indstart = 0
+        my_vals[:, :, indstart + 0] += flux_r
+        my_vals[:, :, indstart + 1] += flux_t
+        my_vals[:, :, indstart + 2] += torque_r
+        my_vals[:, :, indstart + 3] += torque_t
+        my_vals[:, :, indstart + 4] += torque
 
-        # full (toroidal) derivatives B
-        dbrdp = dbpdr + (1./rr_3d)*bp + jt
-        dbtdp = dbpdt + (cott_3d/rr_3d)*bp - jr
-        dbpdp = -dbrdr - (2./rr_3d)*br - dbtdt - (cott_3d/rr_3d)*bt
+        # mean terms
+        indstart += nset
+        my_vals[:, :, indstart + 0] += flux_r_m
+        my_vals[:, :, indstart + 1] += flux_t_m
+        my_vals[:, :, indstart + 2] += torque_r_m
+        my_vals[:, :, indstart + 3] += torque_t_m
+        my_vals[:, :, indstart + 4] += torque_m
 
-        # mean (poloidal) derivatives v
-        dvrdr_m = drad(vr_m, rr)
-        dvtdr_m = drad(vt_m, rr)
-        dvpdr_m = drad(vp_m, rr)
-
-        dvrdt_m = dth(vr_m, tt)/rr_3d
-        dvtdt_m = dth(vt_m, tt)/rr_3d
-        dvpdt_m = dth(vp_m, tt)/rr_3d
-
-        # mean (toroidal) derivatives v
-        dvrdp_m = dvpdr_m + (1./rr_3d)*vp_m + omt_m
-        dvtdp_m = dvpdt_m + (cott_3d/rr_3d)*vp_m - omr_m
-        dvpdp_m = -dlnrho*vr_m - dvrdr_m - (2./rr_3d)*vr_m - dvtdt_m -\
-                (cott_3d/rr_3d)*vt_m
-
-        # mean (poloidal) derivatives B
-        dbrdr_m = drad(br_m, rr)
-        dbtdr_m = drad(bt_m, rr)
-        dbpdr_m = drad(bp_m, rr)
-
-        dbrdt_m = dth(br_m, tt)/rr_3d
-        dbtdt_m = dth(bt_m, tt)/rr_3d
-        dbpdt_m = dth(bp_m, tt)/rr_3d
-
-        # mean (toroidal) derivatives B
-        dbrdp_m = dbpdr_m + (1./rr_3d)*bp_m + jt_m
-        dbtdp_m = dbpdt_m + (cott_3d/rr_3d)*bp_m - jr_m
-        dbpdp_m = -dbrdr_m - (2./rr_3d)*br_m - dbtdt_m -\
-                (cott_3d/rr_3d)*bt_m
-
-        # make vectors/tensors: 
-        # row indexes vector components, col indexes derivative direction
-        vv = [vr, vt, vp]
-        dvdx = [    [dvrdr, dvrdt, dvrdp],\
-                    [dvtdr, dvtdt, dvtdp],\
-                    [dvpdr, dvpdt, dvpdp] ]
-        vv_m = [vr_m, vt_m, vp_m]
-        dvdx_m = [  [dvrdr_m, dvrdt_m, dvrdp_m],\
-                    [dvtdr_m, dvtdt_m, dvtdp_m],\
-                    [dvpdr_m, dvpdt_m, dvpdp_m] ]
-
-        bb = [br, bt, bp]
-        dbdx = [    [dbrdr, dbrdt, dbrdp],\
-                    [dbtdr, dbtdt, dbtdp],\
-                    [dbpdr, dbpdt, dbpdp] ]
-        bb_m = [br_m, bt_m, bp_m]
-        dbdx_m = [  [dbrdr_m, dbrdt_m, dbrdp_m],\
-                    [dbtdr_m, dbtdt_m, dbtdp_m],\
-                    [dbpdr_m, dbpdt_m, dbpdp_m] ]
-
-        # also get fluc vectors/tensors
-        vv_p = [0, 0, 0]
-        dvdx_p = [[0 for k in range(3)] for l in range(3)]
-        bb_p = [0, 0, 0]
-        dbdx_p = [[0 for k in range(3)] for l in range(3)]
-
-        for k in range(3):
-            vv_p[k] = vv[k] - vv_m[k]
-            bb_p[k] = bb[k] - bb_m[k]
-            for l in range(3):
-                dvdx_p[k][l] = dvdx[k][l] - dvdx_m[k][l]
-                dbdx_p[k][l] = dbdx[k][l] - dbdx_m[k][l]
-
-        # get tot, pmp, ppm, mmm, mpp, ppp, energy terms
-        # induc, shear, adv, comp, [diffusion] (only for mm and pp)
-        count = 0 
-        for k in range(6): 
-            # get appropriate fields for tot, pmp, ppm, mmm, mpp, ppp
-            if k == 0:
-                bb1_loc = bb.copy() # the guy getting dotted into 
-                                    # inductive terms
-                vv_loc = vv.copy()
-                dvdx_loc = dvdx.copy()
-                bb_loc = bb.copy()
-                dbdx_loc = dbdx.copy()
-            if k == 1:
-                bb1_loc = bb_p.copy()
-                vv_loc = vv_m.copy()
-                dvdx_loc = dvdx_m.copy()
-                bb_loc = bb_p.copy()
-                dbdx_loc = dbdx_p.copy()
-            if k == 2:
-                bb1_loc = bb_p.copy()
-                vv_loc = vv_p.copy()
-                dvdx_loc = dvdx_p.copy()
-                bb_loc = bb_m.copy()
-                dbdx_loc = dbdx_m.copy()
-            if k == 3:
-                bb1_loc = bb_m.copy()
-                vv_loc = vv_m.copy()
-                dvdx_loc = dvdx_m.copy()
-                bb_loc = bb_m.copy()
-                dbdx_loc = dbdx_m.copy()
-            if k == 4 or k == 5:
-                vv_loc = vv_p.copy()
-                dvdx_loc = dvdx_p.copy()
-                bb_loc = bb_p.copy()
-                dbdx_loc = dbdx_p.copy()
-            if k == 4:
-                bb1_loc = bb_m.copy()
-            if k == 5:
-                bb1_loc = bb_p.copy()
-
-            # loop over r, theta, phi
-            for l in range(3):
-                ind_shear =     bb_loc[0]*dvdx_loc[l][0] +\
-                                bb_loc[1]*dvdx_loc[l][1] +\
-                                bb_loc[2]*dvdx_loc[l][2]
-                ind_adv =       -vv_loc[0]*dbdx_loc[l][0] -\
-                                vv_loc[1]*dbdx_loc[l][1] -\
-                                vv_loc[2]*dbdx_loc[l][2]
-                # need curvature terms for shear + adv
-                if l == 0:
-                    ind_shear += -(1./rr_3d)*(vv_loc[1]*bb_loc[1] +\
-                            vv_loc[2]*bb_loc[2])
-                    ind_adv += (1./rr_3d)*(bb_loc[1]*vv_loc[1] +\
-                            bb_loc[2]*vv_loc[2])
-                if l == 1:
-                    ind_shear += (1./rr_3d)*(vv_loc[0]*bb_loc[1] -\
-                            cott_3d*vv_loc[2]*bb_loc[2])
-                    ind_adv += -(1./rr_3d)*(bb_loc[0]*vv_loc[1] -\
-                            cott_3d*bb_loc[2]*vv_loc[2])
-                if l == 2:
-                    ind_shear += (1./rr_3d)*(vv_loc[0]*bb_loc[2] +\
-                            cott_3d*vv_loc[1]*bb_loc[2])
-                    ind_adv += -(1./rr_3d)*(bb_loc[0]*vv_loc[2] +\
-                            cott_3d*bb_loc[1]*vv_loc[2])
-                # compression
-                ind_comp = vv_loc[0]*dlnrho*bb_loc[l]
-
-                # total induction
-                ind_tot = ind_shear + ind_adv + ind_comp
-
-                my_vals[:, :, count] = np.mean(bb1_loc[l]*ind_tot, axis=0)*my_weight
-                count += 1
-                my_vals[:, :, count] = np.mean(bb1_loc[l]*ind_shear, axis=0)*my_weight
-                count += 1
-                my_vals[:, :, count] = np.mean(bb1_loc[l]*ind_adv, axis=0)*my_weight
-                count += 1
-                my_vals[:, :, count] = np.mean(bb1_loc[l]*ind_comp, axis=0)*my_weight
-                count += 1
+        # fluc terms
+        indstart += nset
+        for k in range(nset):
+            my_vals[:, :, indstart + k] +=\
+                    my_vals[:, :, k] - my_vals[:, :, k + nset]
 
     if rank == 0:
         pcnt_done = (i + 1)/my_nfiles*100.
@@ -398,7 +260,7 @@ if rank == 0:
 
     # Set the timetrace savename by the directory, what we are saving,
     # and first and last iteration files for the trace
-    savename = 'me_prod-' +\
+    savename = 'ferraro-' +\
             file_list[0] + '_' + file_list[-1] + '.pkl'
     savefile = datadir + savename
 
