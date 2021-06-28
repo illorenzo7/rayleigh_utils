4c00f92712640a85cb0a613aa041bb6b2becb718
Loren <loren.matilsky@gmail.com>
2021-06-27 17:46:45 -0700

    polytrope module, I think

--- compute/reference_state/polytrope.py
+++ compute/reference_state/polytrope.py
@@ -12,10 +12,8 @@ def compute_polytrope(ri, ro, Nrho, nr, poly_n, rho_i):
     d = ro - ri
     beta = ri/ro
     poly_gamma = (poly_n + 1.)/poly_n
-    cP = 3.5e8
-    Newton_G = 6.67408e-8
-    M_sun = 1.98891e33
-    gas_constant = cP*(1. - 1./poly_gamma)
+    msun = 1.98891e33
+    gas_constant = c_P*(1. - 1./poly_gamma)
 
     r = np.linspace(ro, ri, nr)
 
@@ -29,11 +27,11 @@ def compute_polytrope(ri, ro, Nrho, nr, poly_n, rho_i):
 
     rho_c = rho_i/zeta_i**poly_n
 
-    T_c = Newton_G*M_sun/(cP*c1*d)
+    T_c = G*msun/(c_P*c1*d)
 
     P_c = gas_constant*rho_c*T_c
 
-    S_c = cP*np.log(P_c**(1./poly_gamma)/rho_c) 
+    S_c = c_P*np.log(P_c**(1./poly_gamma)/rho_c) 
     # for an adiabatic polytrope,
             # S is constant everywhere
     rho_nd = zeta**(poly_n)
