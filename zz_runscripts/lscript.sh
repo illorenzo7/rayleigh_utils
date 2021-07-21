decca860a572fc5b72128baf2581c29b1ea7aa66
Loren <loren.matilsky@gmail.com>
2021-07-20 17:48:24 -0700

    script to make any generic lscript

---
+++ zz_runscripts/lscript.sh
@@ -0,0 +1,50 @@
+echo "#PBS -S /bin/bash" > lscript
+echo "#PBS -N $1" >> lscript
+
+modeltype=$2
+if [ $modeltype == 'bro' ]
+then
+    ncpus=28
+elif [ $modeltype == 'has' ]
+then
+    ncpus=24
+elif [ $modeltype == 'ivy' ] 
+then
+    ncpus=20
+elif [ $modeltype == 'san' ] 
+then
+    ncpus=16
+else
+    echo "unknown model type $modeltype"
+fi
+
+nprocs=$3
+
+echo "select=$(($nprocs/$ncpus + 1*($nprocs%$ncpus)))"
+select=$(($nprocs/$ncpus + 1*($nprocs%$ncpus)))
+
+nprow=`python $rau/zz_runscripts/nprow.py $ncpus`
+npcol=`python $rau/zz_runscripts/npcol.py $ncpus`
+
+echo "#PBS -l select=$select:ncpus=$ncpus:model=$modeltype" >> lscript
+echo "#PBS -q long" >> lscript
+echo "#PBS -l walltime=120:00:00" >> lscript
+echo "#PBS -j oe" >> lscript
+echo "#PBS -W group_list=s2051" >> lscript
+echo "#PBS -m e" >> lscript
+echo "#PBS -l site=needed=/home1+/nobackupp11" >> lscript
+
+echo "module purge" >> lscript
+echo "module load mpi-hpe" >> lscript
+echo "module load comp-intel" >> lscript
+
+echo "export MPI_LAUNCH_TIMEOUT=40" >> lscript
+echo "export PATH=$PATH:/u/scicon/tools/bin" >> lscript
+
+echo "export MPI_BUFS_PER_PROC=256" >> lscript
+
+echo "export MPI_CHECK_ARGS=1" >> lscript
+
+echo "cd $PBS_O_WORKDIR" >> lscript
+
+echo "/u/scicon/tools/bin/several_tries mpiexec -np 1024 ./rayleigh.opt -nprow $nprow -npcol $npcol >> logfile2" >> lscript
