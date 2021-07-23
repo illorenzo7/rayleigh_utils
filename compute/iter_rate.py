7f89b91aec36fdbba283482a8af5749cd5b8f888
Loren <loren.matilsky@gmail.com>
2021-07-23 15:26:20 -0700

    made iter_rate fancier

--- compute/iter_rate.py
+++ compute/iter_rate.py
@@ -19,7 +19,7 @@ nargs = len(args)
 # See if user wants to use a different file name than last logfile
 fname = None
 verbose = False
-nlines_to_use = 'all'
+desired_range = 'all'
 for i in range(nargs):
     arg = args[i]
     if arg == '--fname':
@@ -55,17 +55,54 @@ print ("In %s:" %fname)
 di = read_log(dirname + '/' + fname)
 iters = di['iters'][1:]
 iters_per_sec = di['iters_per_sec'][1:] # first one is zero for some reason
-iters_per_sec_io = di['iters_per_sec_io'] # first one is zero for some reason
 delta_t = di['delta_t'][1:]
-if not nlines_to_use == 'all':
-    iters = iters[-nlines_to_use:]
-    iters_per_sec = iters_per_sec[-nlines_to_use:]
-    delta_t = delta_t[-nlines_to_use:]
 
+iters_io = di['iters_io']
+iters_per_sec_io = di['iters_per_sec_io'] 
+delta_t_io = di['delta_t_io']
+
+# now adjust this by desired range
+it1, it2 = 0, len(iters)
+for i in range(nargs):
+    arg = args[i]
+    if arg == '--f':
+        it2 = int(float(args[i+1]))
+    if arg == '--n':
+        it1 = it2 - int(float(args[i+1]))
+    if arg == '--range':
+        it1 = np.argmin(np.abs(iters - float(args[i+1])))
+        it2 = np.argmin(np.abs(iters - float(args[i+2])))
+    if arg == '--centerrange':
+        itc = np.argmin(np.abs(iters - float(args[i+1])))
+        length = int(float(args[i+1]))
+        it1 = itc - length//2
+        it2 = it1 + length
+
+# safety check
+if it1 < 0:
+    it1 = 0
+if it2 > len(iters):
+    it2 = len(iters)
+
+iter1, iter2 = iters[it1], iters[it2 - 1]
+it1_io = np.argmin(np.abs(iters_io - iter1))
+it2_io = np.argmin(np.abs(iters_io - iter2))
+
+# shorten the arrays possibly (by default, no)
+iters = iters[it1:it2]
+iters_per_sec = iters_per_sec[it1:it2]
+delta_t = delta_t[it1:it2]
+
+iters_io = iters_io[it1_io:it2_io]
+iters_per_sec_io = iters_per_sec_io[it1_io:it2_io]
+delta_t_io = delta_t_io[it1_io:it2_io]
+
+
+print ("range = " + str(iter1).zfill(8) + ' to ' + str(iter2).zfill(8))
 print ("ncpu: ", di['ncpu'])
 
 dt_av = np.mean(delta_t)
-niter = iters[-1] - iters[0]
+niter = len(iters)
 secs_per_iter = 1/iters_per_sec
 secs_per_iter_io = 1/iters_per_sec_io
 runtime = np.sum(secs_per_iter)
@@ -80,7 +117,6 @@ print (make_bold("est. I/O time:  " + format_time(iotime)))
 print ("frac run     :    %.3f" %((runtime - iotime)/runtime))
 print ("frac I/O     :    %.3f" %(iotime/runtime))
 print ("niter        :  ", niter)
-print ("len(iters)   :  ", len(iters_per_sec))
 
 if verbose:
     # Get fancy now ...
