62c6e419bd59f3cb490a9c57993ee019a9ed1463
Loren <loren.matilsky@gmail.com>
2021-09-13 14:57:19 -0700

    updated all of my complicated lut stuff with Ryan's good lut stuff

--- compute/quantity_index.py
+++ compute/quantity_index.py
@@ -1,32 +1,20 @@
 """
 Author: Loren Matilsky
-Created: 03/23/2017
+Created: 09/13/21
+Wrapper for Ryan's parse_quantity
 
 This script prints the Rayleigh index or variable name associated with a
 given quantity.
 
 Takes as an argument a user-specified diagnostic quantity or 
-quantity index, and translates between the two. For example, typing
-    python quantity_index entropy   
-would produce the output '501' on the command line.
-    
-Conversely, typing
-    python quantity_index 64 -old 
-would produce the output 'entropy' on the command line.
-"""
+quantity index, and translates between the two."""
 
 import sys, lut
-from get_index import get_index
-
-lut_current = lut.lut
-        
+from common import make_bold
 quantity_or_index = sys.argv[1]
 
-try:
-    index = int(quantity_or_index) # For '1' or '2', this will be successful
-                                # for 'v_r,' this will fail
-    print ('The index ', index, ' corresponds to the quantity ',\
-           lut_current[index])
-except:  # quantity_or_index was NOT '1,' '2,' ... must be quantity string!
-    print ('The quantity ', quantity_or_index, ' has index ',\
-           get_index(lut_current, quantity_or_index))
+index, quantity = lut.parse_quantity(quantity_or_index)
+if index is None or quantity is None:
+    print ('The index or quantity', make_bold(quantity_or_index), 'does not exist in Rayleigh!')
+else:
+    print ('The index', make_bold(str(index)), 'corresponds to the quantity', make_bold(quantity))
