6177969114dce2d20d56db0e2634374298fbcd4f
Loren <loren.matilsky@colorado.edu>
2022-04-17 21:03:19 -0700

    some finnesse to mertimey

--- plot/timetrace/mertimey.py
+++ plot/timetrace/mertimey.py
@@ -23,7 +23,7 @@ dirname_stripped = strip_dirname(dirname)
 magnetism = clas0['magnetism']
 
 # defaults
-kwargs_default = dict({'the_file': None, 'ntot': 500, 'clat': 10, 'dlat': 0, 'om': None, 'rad': False, 'lon': False, 'shav': False, 'isamplevals': np.array([0]), 'samplevals': None, 'rcut': None, 'groupname': 'b'})
+kwargs_default = dict({'the_file': None, 'ntot': 500, 'rad': False, 'isamplevals': np.array([0]), 'samplevals': None, 'rcut': None, 'groupname': 'b', 'mval': 1, 'imag': False})
 kwargs_default.update(plot_timey_kwargs_default)
 
 # check for bad keys
@@ -37,34 +37,34 @@ kw.update(get_quantity_group(kw.groupname, magnetism))
 kw = update_dict(kw, clas)
 kw_plot_timey = update_dict(plot_timey_kwargs_default, clas)
 
+# check if we want the real or imaginary vals
+if kw.imag:
+    take_real = False
+else:
+    take_real = True
+
 # baseline time unit
 time_unit, time_label, rotation, simple_label = get_time_unit(dirname)
 
 # get grid info
 di_grid = get_grid_info(dirname)
 
-datatype = 'timelat'
+datatype = 'mertimelat'
+dataname = 'mertimelat'
 sampleaxis = di_grid['tt_lat']
 if kw.rad:
-    datatype = 'timerad'
-    sampleaxis = di_grid['rr']/rsun
-elif kw.lon:
-    if not kw.om is None:
-        om0 = 1/time_unit*1e9 # frame rate, nHz
-    datatype = 'timelon_clat' + lat_format(clat) + '_dlat%03.0f' %dlat
-    sampleaxis = di_grid['lons']
-elif kw.shav:
-    datatype = 'timeshav'
+    datatype = 'mertimerad'
+    dataname = 'mertimerad'
     sampleaxis = di_grid['rr']/rsun
 
-dataname = datatype
+datatype += '_mval%03i' %kw.mval
+
 if 'groupname' in kw:
     dataname += '_' + kw.groupname
 if not kw.rcut is None:
     dataname += '_rcut%0.3f' %kw.rcut
 
 #dataname += clas0['tag']
-
 # get data
 if kw.the_file is None:
     kw.the_file = get_widest_range_file(clas0['datadir'] +\
@@ -84,18 +84,6 @@ if not kw.shav:
 iter1, iter2 = get_iters_from_file(kw.the_file)
 times /= time_unit
 
-# Subtract DR, if desired
-if kw.lon:
-    if not kw.om is None:
-        print ("plotting in rotating frame om = %.1f nHz" %kw.om)
-        print ("compare this to frame rate    = %.1f nHz" %om0)
-        phi_deflections = (times*(kw.om - om0)) % 1 # between zero and one
-        nphi = len(sampleaxis)
-        for it in range(len(times)):
-            phi_deflection = phi_deflections[it]
-            nroll = int(phi_deflection*nphi)
-            vals[it] = np.roll(vals[it], -nroll, axis=0)
-
 # maybe thin data
 if not kw.ntot == 'full':
     print ("ntot = %i" %kw.ntot)
@@ -114,15 +102,17 @@ kw.samplevals = make_array(kw.samplevals)
 terms = []
 for qval in kw.qvals:
     qind = np.argmin(np.abs(qvals_avail - qval))
-    terms.append(vals[:, :, :, qind])
+    if take_real:
+        the_term = np.real(vals[:, :, :, qind])
+    else:
+        the_term = np.imag(vals[:, :, :, qind])
+    terms.append(the_term)
 
 # set figure dimensions
 sub_width_inches = 7.5
 sub_height_inches = 2.0
 margin_bottom_inches = 1/2 # space for x-axis label
 margin_top_inches = 3/4
-if kw.lon:
-    margin_top_inches =  1 + 1/4
 margin_left_inches = 5/8 # space for latitude label
 margin_right_inches = 7/8 # space for colorbar
 if 'ycut' in clas:
@@ -130,16 +120,14 @@ if 'ycut' in clas:
 nplots = len(terms)
 
 # determine desired levels to plot
-if not kw.shav: # kw.shav means integrated over latitude, so can't choose
-    # specific latitude levels
-    if not kw.samplevals is None: # isamplevals being set indirectly
-        # check for special 'all' option
-        if isall(kw.samplevals):
-            kw.isamplevals = np.arange(len(samplevals_avail))
-        else:
-            kw.isamplevals = np.zeros_like(kw.samplevals, dtype='int')
-            for i in range(len(kw.samplevals)):
-                kw.isamplevals[i] = np.argmin(np.abs(samplevals_avail - kw.samplevals[i]))
+if not kw.samplevals is None: # isamplevals being set indirectly
+    # check for special 'all' option
+    if isall(kw.samplevals):
+        kw.isamplevals = np.arange(len(samplevals_avail))
+    else:
+        kw.isamplevals = np.zeros_like(kw.samplevals, dtype='int')
+        for i in range(len(kw.samplevals)):
+            kw.isamplevals[i] = np.argmin(np.abs(samplevals_avail - kw.samplevals[i]))
 
 # Loop over the desired levels and save plots
 for isampleval in kw.isamplevals:
@@ -154,32 +142,17 @@ for isampleval in kw.isamplevals:
         axislabel = r'$r/R_\odot$'
         samplelabel = 'lat = ' + lat_format(sampleval)
         position_tag = '_lat' + lat_format(sampleval)
-    elif kw.lon:
-        axislabel = 'longitude (deg)'
-        samplelabel = 'clat = ' + lat_format(clat) + '\n' +  r'$r/R_\odot$' + ' = %.3f' %sampleval
-        if not kw.om is None:
-            samplelabel += '\n' + (r'$\Omega_{\rm{frame}}$' + ' = %.1f nHz ' + '\n' + r'$\Omega_{\rm{frame}} - \Omega_0$' + ' = %.2f nHz') %(om, om - om0)
-        else:
-            samplelabel += '\n' + r'$\Omega_{\rm{frame}} = \Omega_0$'
-
-        position_tag = '_clat' + lat_format(clat) + '_rval%.3f' %sampleval
-    elif kw.shav:
-        axislabel = r'$r/R_\odot$'
-        samplelabel = ''
-        position_tag = ''
 
     # Put some useful information on the title
     maintitle = dirname_stripped 
-    if not kw.shav:
-        maintitle += '\n' + samplelabel
+    maintitle += '\n' + samplelabel
     if kw.navg is None:
         maintitle += '\nt_avg = none'
     else:
         averaging_time = (times[-1] - times[0])/len(times)*kw.navg
         maintitle += '\n' + ('t_avg = %.1f Prot' %averaging_time)
 
-    if not kw.shav:
-        print('plotting sampleval = %0.3f (i = %02i)' %(sampleval, isampleval))
+    print('plotting sampleval = %0.3f (i = %02i)' %(sampleval, isampleval))
    
     # make plot
     fig, axs, fpar = make_figure(nplots=nplots, ncol=1, sub_width_inches=sub_width_inches, sub_height_inches=sub_height_inches, margin_left_inches=margin_left_inches, margin_right_inches=margin_right_inches, margin_top_inches=margin_top_inches, margin_bottom_inches=margin_bottom_inches)
@@ -214,9 +187,11 @@ for isampleval in kw.isamplevals:
         # save the figure
         basename = dataname + '_%08i_%08i' %(iter1, iter2)
         plotdir = my_mkdir(clas0['plotdir'] + '/' + datatype)
-        if kw.lon and not om is None:
-            basename += '_om%.0f' %om
-        savename = basename + clas0['tag'] + position_tag + '.png'
+        if take_real:
+            realtag = '_real'
+        else:
+            realtag = '_imag'
+        savename = basename + clas0['tag'] + position_tag + realtag + '.png'
         print ("saving", plotdir + '/' + savename)
         plt.savefig(plotdir + '/' + savename, dpi=200)
 
