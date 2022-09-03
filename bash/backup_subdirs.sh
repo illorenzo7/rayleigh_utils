c5d688fda206847faf29c0781eacf5fef2869d30
Loren <loren.matilsky@colorado.edu>
2022-09-03 13:34:21 -0700

    all right I think the new backup_subdirs routine is ready

--- bash/backup_subdirs.sh
+++ bash/backup_subdirs.sh
@@ -6,23 +6,52 @@
 
 # Read in desired directory and get its subdirectories
 maindir=$1
-fullpath=`readlink -f $maindir`
-backupdir=$2
+#fullpath=`readlink -f $maindir`
+#cd $fullpath
 subdirs=`find $maindir -maxdepth 1 -mindepth 1 -type d -printf '%f\n'`
 
+# move in to the desired directory
+cd $maindir
+echo $bufferstring
+echo -n "I am here: "
+echo `pwd`
+# remember current directory to cd back into
 currentdir=`pwd`
 
-# Make the corresponding directory and subdirectories on lou,
-# also launch transfers in the loop
+# get the backup directory and create it
+backupdir=$2
 mkdir -p $backupdir
+
+# Make the corresponding directory and subdirectories on lou,
+## also launch transfers in the loop
 #fullpath=`readlink -f $maindir`
 #remove="/noabackupp2/lmatilsk"
 #backupdir=${fullpath//$remove/}
+
+# make a file of transfer instructions (a list of sources and dest)
+thefile=~/transfer_instructions_tmp
+echo -n '' > $thefile
 for subdir in $subdirs
 do
-    pwd
-    mkdir $backupdir/$subdir
-    cd $fullpath/$subdir
-    shiftc --create-tar --index-tar --hosts=6 * $backupdir/$subdir/$subdir.tar
+    #pwd
+    #mkdir $backupdir/$subdir
+    #cd $fullpath/$subdir
+    #ls | tr "\n" " " >> ~/transfer_instructions_tmp
+    #shiftc --create-tar --index-tar --hosts=6 * $backupdir/$subdir/$subdir.tar
+    echo -n $subdir >> $thefile
+    echo -n ' ' >> $thefile
+    echo $backupdir/$subdir.tar >> $thefile
 done
+
+# now the actual transfer
+echo $bufferstring
+echo "running shiftc --create-tar --index-tar --hosts=6"
+echo "on $thefile, which is"
+cat $thefile
+echo $bufferstring
+shiftc --create-tar --index-tar --hosts=6 $thefile
 cd $currentdir
+echo $bufferstring
+echo -n "I returned here: "
+echo `pwd`
+echo $bufferstring
