85595a1b158ecb4b056ae957bce2b7be4598479a
Loren <loren.matilsky@colorado.edu>
2022-12-08 10:33:34 -0800

    update print_sliceinfo

--- compute/print_sliceinfo.py
+++ compute/print_sliceinfo.py
@@ -24,16 +24,20 @@ if radtype == 'merslice':
 if radtype == 'eqslice':
     dataname = 'Equatorial_Slices'
 
-# get the sampling locations
+# state what we are about to print
+print (buff_line)
+print (make_bold("for     %20s:" %dirname_stripped))
+print (make_bold("for the %20s:" %dataname))
+# get the sampled values
 di = get_sliceinfo(dirname, dataname=dataname)
+print (buff_line)
+print ("%i variables sampled" %di.nsamplevals)
 print ('qv =', arr_to_str(di.qv, '%i'))
 
 if not radtype == 'eqslice':
     print (buff_line)
     print ("%i sampling locations" %di.nsamplevals)
-    print (buff_line)
     print ('iisamplevals     =', arr_to_str(np.arange(di.nsamplevals), '%9i'))
-    print (buff_line)
     print ('isamplevals      =', arr_to_str(di.isamplevals, '%9i'))
     print (buff_line)
     print ('samplevals       = ' + arr_to_str(di.samplevals, "%9.3e"))
