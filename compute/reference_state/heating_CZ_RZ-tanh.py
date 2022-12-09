4d8f59178d75841fd72a9ed2ce83c9fa4ebaac15
Loren <loren.matilsky@colorado.edu>
2022-12-09 03:26:58 -0800

    heating CZ RZ tanh fixed

--- compute/reference_state/heating_CZ_RZ-tanh.py
+++ compute/reference_state/heating_CZ_RZ-tanh.py
@@ -17,14 +17,17 @@
 # --rstar (default rsun)
 #
 # --delta
-# Transition width delta (as a fraction of rt) default 0.030
+# Transition width delta default 0.050*rsun
 #
 # --lum
 # Luminosity to be driven through layer, default Lsun
 # 
 # --rmax
-# radius at which to normalize integral (not rmin doesn't matter because
+# radius at which to normalize integral (rmin doesn't matter because
 # the tanh will be ~zero here; revisit for very thin RZs)
+#
+# --fname
+# default custom_reference_binary. Place to read current reference state and# write heating to
 
 import numpy as np
 import sys, os
@@ -35,50 +38,34 @@ sys.path.append(os.environ['raco'])
 
 from reference_tools import equation_coefficients
 from common import *
+from cla_util import *
 
-# Set default constants
-rt = rbcz # by default transition a bit below RZ-CZ transition
-delta = 0.03
-lum = lsun
-rmax = rmax_n3
-rstar = rsun
+# Get CLAs
+args = sys.argv
+clas0, clas = read_clas_raw(args)
+dirname = clas0['dirname']
 
-# Get directory to save binary files for reference state and heating
-dirname = sys.argv[1]
-fname = 'custom_reference_binary'
+# Set default kwargs
+kw_default = dotdict(dict({'rmax': sun.r_nrho3, 'rt': sun.r_bcz, 'rstar': rsun, 'delta': 0.05*sun.r, 'lum': sun.l, 'fname': 'custom_reference_binary'}))
 
-args = sys.argv[2:]
-nargs = len(args)
-for i in range(nargs):
-    arg = args[i]
-    if arg == '--rt':
-        rt = float(args[i+1])
-    elif arg == '--rstar':
-        rstar = float(args[i+1])
-    elif arg == '--delta':
-        delta = float(args[i+1])
-    elif arg == '--fname':
-        fname = args[i+1]
-    elif arg == '--lum':
-        lum = float(args[i+1])
-    elif arg == '--rmax':
-        rmax = float(args[i+1])
+# overwrite defaults
+kw = update_dict(kw_default, clas)
 
-# Make the delta "dimensional"
-delta *= rsun
+# check for bad keys
+find_bad_keys(kw_default, clas, clas0['routinename'], justwarn=True)
 
 # Open and read the hopefully already existing reference file!
 eq = equation_coefficients()
-the_file = dirname + '/' + fname
+the_file = dirname + '/' + kw.fname
 eq.read(the_file)
 rr = eq.radius
 nr = eq.nr
 rho = eq.functions[0]
-T = eq.functions[3]
-smooth = 0.5*(1.0 + np.tanh((rr - rt)/delta)) # "detects" CZ
-profile = (rho*T - rho[0]*T[0])*smooth
+tmp = eq.functions[3]
+smooth = 0.5*(1.0 + np.tanh((rr - kw.rt)/kw.delta)) # "detects" CZ
+profile = (rho*tmp - rho[0]*tmp[0])*smooth
 
-irmax = np.argmin(np.abs(rr - rmax))
+irmax = np.argmin(np.abs(rr - kw.rmax))
 int_profile = -4*np.pi*simps((profile*rr**2.0)[irmax:], rr[irmax:])
 # remember rr is reversed
 
@@ -87,18 +74,18 @@ radial_shape = profile/int_profile
 print(buff_line)
 print("Computed heating for RZ-CZ, joined with tanh")
 print("nr                  : %i" %nr) 
-print("rstar               : %1.8e cm" %rstar) 
-print("rt/rstar            : %1.8e" %(rt/rstar) )
-print("delta/rstar         : %.8f"  %(delta/rstar))
-print("rmax/rstar          : %.8f"  %(rmax/rstar))
-print("c_10 (lum)          : %1.8e erg/s"  %lum)
+print("rstar               : %1.8e cm" %kw.rstar) 
+print("rt/rstar            : %1.8e" %(kw.rt/kw.rstar) )
+print("delta/rstar         : %.8f"  %(kw.delta/kw.rstar))
+print("rmax/rstar          : %.8f"  %(kw.rmax/kw.rstar))
+print("c_10 (lum)          : %1.8e erg/s"  %kw.lum)
 print(buff_line)
 
 # Now write to file using the equation_coefficients framework
 print("Setting f_6 and c_10")
 
 eq.set_function(radial_shape, 6)
-eq.set_constant(lum, 10) # Total luminosity normalizes heating function
+eq.set_constant(kw.lum, 10) # Total luminosity normalizes heating function
 
 print("Writing the heating to %s" %the_file)
 print("---------------------------------")
