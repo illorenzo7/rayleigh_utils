92b41ae474d5a4083b58d43046d4638dd7dde9f8
Loren <loren.matilsky@colorado.edu>
2022-12-30 01:23:41 -0800

    fixed the lscript.sh routine

---
+++ bash/npcol.py
@@ -0,0 +1,14 @@
+import numpy as np
+import sys
+
+ncpus = int(sys.argv[1])
+
+root = np.sqrt(ncpus)
+
+# find the next highest power of 2
+exp = int(np.ceil(np.log2(root)))
+
+nprow = 2**exp
+npcol = ncpus // nprow
+
+print (npcol)
