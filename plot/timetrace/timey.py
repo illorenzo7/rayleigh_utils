184875c4a5f17ff7129f304de547c87dbaae88ba
Loren <loren.matilsky@colorado.edu>
2023-01-17 14:53:23 -0800

    touched up plot/timetrace/timey

--- plot/timetrace/timey.py
+++ plot/timetrace/timey.py
@@ -24,6 +24,12 @@ magnetism = clas0['magnetism']
 
 # defaults
 kwargs_default = dict({'the_file': None, 'ntot': 500, 'clat': 10, 'dlat': 0, 'om': None, 'rad': False, 'lon': False, 'isamplevals': np.array([0]), 'samplevals': None, 'groupname': 'b'})
+
+# also need make figure kwargs
+make_figure_kwargs_default.update(timey_fig_dimensions)
+kwargs_default.update(make_figure_kwargs_default)
+
+# of course, also need plot_timey kwargs
 kwargs_default.update(plot_timey_kwargs_default)
 
 # check for bad keys
@@ -31,11 +37,18 @@ find_bad_keys(kwargs_default, clas, clas0['routinename'], justwarn=True)
 
 # overwrite defaults
 kw = update_dict(kwargs_default, clas)
+kw_plot_timey = update_dict(plot_timey_kwargs_default, clas)
+kw_make_figure = update_dict(make_figure_kwargs_default, clas)
+
+# might need two colorbars
+if not kw.ycut is None:  # need room for two colorbars
+    kw_make_figure.sub_margin_right_inches *= 2
+
 # add in groupname keys
 kw.update(get_quantity_group(kw.groupname, magnetism))
+
 # user may have wanted to change some groupname keys
 kw = update_dict(kw, clas)
-kw_plot_timey = update_dict(plot_timey_kwargs_default, clas)
 
 # baseline time unit
 time_unit, time_label, rotation, simple_label = get_time_unit(dirname)
@@ -61,7 +74,6 @@ else:
 dataname = datatype
 if 'groupname' in kw:
     dataname += '_' + kw.groupname
-
 #dataname += clas0['tag']
 
 # get data
@@ -97,17 +109,6 @@ for qval in kw.qvals:
     qind = np.argmin(np.abs(qvals_avail - qval))
     terms.append(vals[:, :, :, qind])
 
-# set figure dimensions
-sub_width_inches = 7.5
-sub_height_inches = 2.0
-margin_bottom_inches = 1/4 # space for x-axis label
-margin_top_inches = 1.25
-margin_left_inches = 5/8 # space for latitude label
-margin_right_inches = 7/8 # space for colorbar
-if 'ycut' in clas:
-    margin_right_inches *= 2
-nplots = len(terms)
-
 # determine desired levels to plot
 if not kw.samplevals is None: # isamplevals being set indirectly
     # check for special 'all' option
@@ -130,23 +131,13 @@ for isampleval in kw.isamplevals:
     else:
         position_tag = '_' + samplename + (samplefmt %sampleval)
 
-    # Put some useful information on the title
-    maintitle = dirname_stripped + '\n' +\
-            plotlabel + '\n' +\
-            'groupname = ' + kw.groupname + '\n' +\
-            samplelabel
-    if kw.navg is None:
-        maintitle += '\nt_avg = none'
-    else:
-        averaging_time = (times[-1] - times[0])/len(times)*kw.navg
-        maintitle += '\n' + ('t_avg = %.1f Prot' %averaging_time)
-
-    maintitle += '\nm=0 (lon. avg.)'
 
     print('plotting ' + samplelabel + ' (i = %02i)' %isampleval)
    
     # make plot
-    fig, axs, fpar = make_figure(nplots=nplots, ncol=1, sub_width_inches=sub_width_inches, sub_height_inches=sub_height_inches, margin_left_inches=margin_left_inches, margin_right_inches=margin_right_inches, margin_top_inches=margin_top_inches, margin_bottom_inches=margin_bottom_inches)
+    nplots = kw_make_figure.nplots = len(terms)
+    kw_make_figure.ncol = 1
+    fig, axs, fpar = make_figure(**kw_make_figure)
 
     for iplot in range(nplots):
         ax = axs[iplot, 0]
@@ -169,7 +160,22 @@ for isampleval in kw.isamplevals:
         if iplot == nplots//2:
             ax.set_ylabel(axislabel, fontsize=fontsize)
 
-    fig.text(fpar['margin_left'], 1 - fpar['margin_top'], maintitle, fontsize=fontsize, ha='left', va='bottom')
+    # Put some useful information on the title
+    maintitle = dirname_stripped + '\n' +\
+            plotlabel + '\n' +\
+            'groupname = ' + kw.groupname + '\n' +\
+            samplelabel
+    if kw.navg is None:
+        maintitle += '\nt_avg = none'
+    else:
+        averaging_time = (times[-1] - times[0])/len(times)*kw.navg
+        maintitle += '\n' + ('t_avg = %.1f Prot' %averaging_time)
+
+    maintitle += '\nm=0 (lon. avg.)'
+    margin_x = fpar['margin_left'] + fpar['sub_margin_left']
+    margin_y = default_margin/fpar['height_inches']
+    fig.text(margin_x, 1 - margin_y, maintitle,\
+             ha='left', va='top', fontsize=default_titlesize)
 
     # Save the plot
     if clas0['saveplot']:
