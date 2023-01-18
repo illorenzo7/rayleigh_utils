337812780ccfb76f946714035a1ded8a8b20c375
Loren <loren.matilsky@colorado.edu>
2023-01-17 16:51:53 -0800

    made print_eq_constants able to save data in a text file

--- compute/print_eq_constants.py
+++ compute/print_eq_constants.py
@@ -17,12 +17,15 @@ dirname_stripped = strip_dirname(dirname)
 args = sys.argv[2:]
 nargs = len(args)
 custom_name = None
+save = False
 for i in range(nargs):
     arg = args[i]
     if arg == '--fname':
         custom_name = args[i+1]
     elif arg == '--crb':
         custom_name = 'custom_reference_binary'
+    elif arg == '--save':
+        save = True
 
 # Directory with data and plots, make the plotting directory if it doesn't
 # already exist    
@@ -34,10 +37,21 @@ if custom_name is None:
     custom_name = 'equation_coefficients'
 eq.read(dirname + '/' + custom_name)
 
+print (buff_line)
 print ("For file: ", custom_name)
 print ("In directory: ", dirname_stripped)
 print ("The equation_coefficients constants are: ")
+if save:
+    savename = dirname + '/eq_constants.txt'
+    f = open(savename, 'w')
 for i in range(len(eq.constants)): # should always be 10?
     cnum = fill_str('c_%i' %(i+1), 4, ' ')
     cname = fill_str(c_dict[i+1], 15, ' ')
     print ('%s = %s = %1.3e' %(cnum, cname, eq.constants[i]))
+    if save:
+        f.write('%s = %s = %1.7e\n' %(cnum, cname, eq.constants[i]))
+if save:
+    f.close()
+    print (buff_line)
+    print ("saving constants in", savename)
+    print (buff_line)
