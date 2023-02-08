a3859bbb169f42c67c9affe1d7e5ad479d856d4d
Loren <loren.matilsky@colorado.edu>
2023-02-08 14:50:07 -0800

    prettied up print_numbers_input again

--- compute/print_numbers_input.py
+++ compute/print_numbers_input.py
@@ -23,16 +23,27 @@ rvals = clas.rvals
 if rvals is None:
     rvals = interpret_rvals(dirname, ['rmin', 'rmax'])
 
+# length of print "tabs" for formatting below
+lendef1 = 10
+lendef2 = 35
+
+# loop over shells and output numbers
 for ishell in range(len(rvals) - 1):
+    # print shell info first
     r1 = rvals[ishell]
     r2 = rvals[ishell+1]
-
-    di = get_numbers_input(dirname, r1, r2)
-    print (("Shell #%02i: r_1 = " + flt_fmt + ", r_2 = " + flt_fmt)\
-            %(ishell + 1, r1, r2))
+    print (make_bold("Shell #%02i:" %(ishell + 1)))
+    print (make_bold(fill_str('r_1', lendef1 + lendef2, ' ')), end='')
+    print (make_bold(flt_fmt %r1))
+    print (make_bold(fill_str('r_2', lendef1 + lendef2, ' ')), end='')
+    print (make_bold(flt_fmt %r2))
+    print (make_bold(fill_str('H', lendef1, ' ')), end='')
+    print (make_bold(fill_str('r_2 - r_1', lendef2, ' ')), end='')
+    print (make_bold(flt_fmt %(r2-r1)))
     print (buff_line)
-    lendef1 = 10
-    lendef2 = 35
+
+    # then non-D numbers in shell
+    di = get_numbers_input(dirname, r1, r2)
     count = 0
     for key in di.keys():
         print (fill_str(numbers_input_def[key][0], lendef1, ' '), end='')
