25ac79e8664fba6e1340e1dd5af43338eb6a28e3
Loren <loren.matilsky@colorado.edu>
2023-02-09 13:45:48 -0800

    set up infrastructure to save numbers in/out in text files later

--- compute/print_numbers_output.py
+++ compute/print_numbers_output.py
@@ -8,17 +8,25 @@ from cla_util import *
 from common import *
 from numbers_util import *
 
+# read in args
+clas0, clas = read_clas(sys.argv)
+dirname = clas0.dirname
+dirname_stripped = strip_dirname(dirname)
+magnetism = clas0.magnetism
+rotation = clas0.rotation
+
+# allowed args + defaults
+kwargs_default = dict({'save': False, 'savename': None, 'verbose': False})
+kw = update_dict(kwargs_default, clas)
+
 # print the purpose of routine
+print (buff_line)
+print ('Simulation directory: ', dirname_stripped)
 print (buff_line)
 print ("NON-DIMENSIONAL NUMBERS (OUTPUT PARAMETERS)")
 print (buff_line)
 
-# read in args
-clas0, clas = read_clas(sys.argv)
-dirname = clas0.dirname
-magnetism = clas0.magnetism
-rotation = clas0.rotation
-
+# get desired shells to average over
 rvals = clas.rvals
 if rvals is None:
     rvals = interpret_rvals(dirname, ['rmin', 'rmax'])
@@ -32,6 +40,8 @@ for ishell in range(len(rvals) - 1):
     # print shell info first
     r1 = rvals[ishell]
     r2 = rvals[ishell+1]
+    print ('\n\n')
+    print (buff_line)
     print (make_bold("Shell #%02i:" %(ishell + 1)))
     print (make_bold('r_1           = ' + flt_fmt %r1))
     print (make_bold('r_2           = ' + flt_fmt %r2))
@@ -39,7 +49,7 @@ for ishell in range(len(rvals) - 1):
     print (buff_line)
 
     # then non-D numbers in shell
-    di = get_numbers_output(dirname, r1, r2)
+    di = get_numbers_output(dirname, r1, r2, verbose=kw.verbose)
     count = 0
     for key in di.keys():
         print (fill_str(numbers_output_def[key][0], lendef1, ' '), end='')
