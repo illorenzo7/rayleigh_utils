9d2aa94d9637900d560b102048c2706e0fbb0682
Loren <loren.matilsky@colorado.edu>
2023-04-10 14:44:10 -0700

    adjusted buff_line length for print_numbers_input

--- compute/print_numbers_input.py
+++ compute/print_numbers_input.py
@@ -8,6 +8,11 @@ from cla_util import *
 from common import *
 from numbers_util import *
 
+# length of print "tabs" for formatting below
+lendef1 = 10
+lendef2 = 35
+buff_line_loc = (lendef1 + lendef2 + 9)*'='
+
 # read in args
 clas0, clas = read_clas(sys.argv)
 dirname = clas0.dirname
@@ -27,7 +32,7 @@ if kw.save:
     if kw.savedir is None:
         kw.savedir = dirname
     savefile = kw.savedir + '/' + kw.savename
-    print (buff_line)
+    print (buff_line_loc)
     print ("Saving the output below to:")
     print (savefile)
     f = open(savefile, 'w')
@@ -38,12 +43,12 @@ if kw.save:
 # Now print the output, possibly to file as well
 for print_func in print_funcs:
     # print the purpose of routine
-    print_func (buff_line)
+    print_func (buff_line_loc)
     print_func ('Simulation directory: ' + dirname_stripped)
     print_func ('Full directory: ' + os.path.abspath(dirname))
-    print_func (buff_line)
+    print_func (buff_line_loc)
     print_func ("NON-DIMENSIONAL NUMBERS (INPUT PARAMETERS)")
-    print_func (buff_line)
+    print_func (buff_line_loc)
 
     # get desired shells to average over
     rvals = clas.rvals
@@ -51,22 +56,18 @@ for print_func in print_funcs:
         rvals = interpret_rvals(dirname, ['rmin', 'rmax'])
     nshells = len(rvals) - 1
 
-    # length of print "tabs" for formatting below
-    lendef1 = 10
-    lendef2 = 35
-
     # loop over shells and output numbers
     for ishell in range(nshells):
         # print shell info first
         r1 = rvals[ishell]
         r2 = rvals[ishell+1]
         print_func ('\n\n')
-        print_func (buff_line)
+        print_func (buff_line_loc)
         print_func ("Shell %i of %i:" %(ishell + 1, nshells))
         print_func ('r_1           = ' + flt_fmt %r1)
         print_func ('r_2           = ' + flt_fmt %r2)
         print_func ('H = r_2 - r_1 = '+ flt_fmt %(r2-r1))
-        print_func (buff_line)
+        print_func (buff_line_loc)
 
         # then non-D numbers in shell
         di = get_numbers_input(dirname, r1, r2, verbose=kw.verbose)
@@ -78,7 +79,7 @@ for print_func in print_funcs:
             if (count + 1) in linebreaks_input:
                 print_func("")
             count += 1
-        print_func (buff_line)
+        print_func (buff_line_loc)
 
 # remember to close save file
 if kw.save:
