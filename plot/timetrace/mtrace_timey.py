44582951be63664dbff25c5ca8fa0692aa4505d1
Loren <loren.matilsky@colorado.edu>
2023-07-18 15:59:10 -0700

    checkpoint I dunno what for mtrace_timey

--- plot/timetrace/mtrace_timey.py
+++ plot/timetrace/mtrace_timey.py
@@ -23,7 +23,7 @@ dirname_stripped = strip_dirname(dirname)
 magnetism = clas0['magnetism']
 
 # defaults
-kwargs_default = dict({'rad': False, 'groupname': 'b', 'sampletag': '', 'the_file': None, 'isamplevals': np.array([0]), 'samplevals': None, 'rvals': None, 'qvals': 'all', 'ntot': 500, 'mval': 1, 'imag': False, 'abs': False, 'prepend': False})
+kwargs_default = dict({'rad': False, 'groupname': 'b', 'sampletag': '', 'the_file': None, 'isamplevals': np.array([0]), 'samplevals': None, 'rvals': None, 'qvals': 'all', 'ntot': 500, 'mval': 1, 'imag': False, 'abs': False, 'prepend': False, 'xminmax': None, 'xmin': None, 'xmax': None})
 
 # also need make figure kwargs
 make_figure_kwargs_default.update(timey_fig_dimensions)
