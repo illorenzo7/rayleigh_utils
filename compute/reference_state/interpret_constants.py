25e2565e47c0fb2cf70a35eca256c2cec2d80ca1
Loren <loren.matilsky@colorado.edu>
2024-04-29 14:56:08 -0700

    set diffusions routine working---but need to deal with interpolation issue possibly

---
+++ compute/reference_state/interpret_constants.py
@@ -0,0 +1,169 @@
+# Author: Loren Matilsky
+# Created: 04/27/2024
+#
+# Purpose: modify binary file with list of human-entered text paramters
+# (ra, pr, prm, etc.) and put it into ra_constants list in main_input
+
+# Parameters: output_dir (first argument), 
+
+# Command-line options:
+#
+# --fname : binary file to modify (default "customfile")
+# --tau : timescale in nondimensionalization (default "rot")
+#       options rot, visc, kappa
+# 
+
+import numpy as np
+import sys, os
+from arbitrary_atmosphere import compute_Di_v
+sys.path.append(os.environ['rapp'])
+sys.path.append(os.environ['raco'])
+from reference_tools import equation_coefficients
+from common import *
+from cla_util import *
+
+# Get CLAs
+args = sys.argv
+clas0, clas = read_clas_raw(args)
+dirname = clas0['dirname']
+
+# Set default kwargs
+kw_default = dotdict(dict({'fname': 'customfile', 'tau': 'rot'}))
+
+# overwrite defaults
+kw = update_dict(kw_default, clas)
+
+# check for bad keys
+find_bad_keys(kw_default, clas, clas0['routinename'], justwarn=True)
+
+# get the name of the metafile
+metafile = dirname + '/' + kw.fname + '_meta.txt'
+
+# get paremeters necessary for dissipation number
+f = open(metafile, 'r')
+lines = f.readlines()
+for line in lines:
+    if 'beta' in line:
+        beta = float(line.split(':')[1][:-1])
+    elif 'gamma' in line:
+        gamma = float(line.split(':')[1][:-1])
+    elif 'Nrho' in line:
+        nrho = float(line.split(':')[1][:-1])
+
+di = compute_Di_v(gamma, beta, nrho)
+
+# read in parameters from main_parameters
+parfile = dirname + '/main_parameters'
+print (buff_line)
+print ("reading", parfile)
+f = open(parfile, 'r')
+lines = f.readlines()
+f.close()
+di_par = dotdict({})
+for line in lines:
+    key, val = line.split('=')
+    key = key.strip()
+    val = float(val[:-1])
+    di_par[key] = val
+    print (key, "=", val)
+
+print("dipar = ", di_par)
+# "unpack" pr and ra
+pr = di_par.pr
+raf = di_par.raf
+
+# if model is rotating, user will have set Ro_c and sigma
+# must compute ekman number and buoyancy parameter in that case
+if 'roc' in di_par.keys():
+    rotation = True
+    ek = di_par.roc * np.sqrt(pr/raf)
+    bu = di_par.sigma**2./ek**2./pr
+else:
+    rotation = False
+    bu = di_par.bu
+
+if 'prm' in di_par.keys():
+    magnetism = True
+    prm = di_par.prm
+else:
+    magnetism = False
+
+# compute tau over taunu [will have to update if I change which parameter
+# set I choose by default...right now its Ro_c, Ra, Pr for rotating cases,
+# etc.
+if kw.tau == 'rot':
+    print ("timescale chosen: %s, tau = 1/(2Om0)" %kw.tau)
+    tau_over_taunu = ek
+elif kw.tau == 'visc':
+    print ("timescale chosen: %s, tau = H^2/nu" %kw.tau)
+    tau_over_taunu = 1.
+elif kw.tau == 'kappa':
+    print ("timescale chosen: %s, tau = H^2/kappa" %kw.tau)
+    tau_over_taunu = pr
+
+# read the existing equation coefficients file
+eq = equation_coefficients()
+the_file = dirname + '/' + kw.fname
+eq.read(the_file)
+
+# now set all the constants
+
+# c1
+if rotation:
+    eq.set_constant(ek, 1)
+else:
+    eq.set_constant(0., 1)
+
+# c2
+eq.set_constant(raf/pr*tau_over_taunu**2., 2)
+
+# c3
+eq.set_constant(1., 3)
+
+# c4
+if magnetism:
+    eq.set_constant(1., 4)
+else:
+    eq.set_constant(0., 4)
+
+# c5
+eq.set_constant(tau_over_taunu, 5)
+
+# c6
+eq.set_constant(tau_over_taunu/pr, 6)
+
+# c7
+if magnetism:
+    eq.set_constant(tau_over_taunu/prm, 7)
+else:
+    eq.set_constant(0., 7)
+
+# c8
+eq.set_constant(pr*di/raf/tau_over_taunu**2., 8)
+
+# c9
+if magnetism:
+    eq.set_constant(pr*di/prm/tau_over_taunu**2., 9)
+else:
+    eq.set_constant(0., 9)
+
+# c10
+eq.set_constant(tau_over_taunu/pr, 10)
+
+# c11
+eq.set_constant(pr*bu/raf, 11)
+
+# write the file
+print("Writing the constants to %s" %the_file)
+print(buff_line)
+eq.write(the_file)
+
+# record what we did in the meta file
+f = open(dirname + '/' + metafile, 'a')
+
+f.write(buff_line + '\n')
+f.write("Also set constants c1 thru c11 using\n")
+f.write("%s ---> %s" %(parfile, the_file))
+f.write("with the interpret_constants routine\n")
+f.close()
+print(buff_line)
--- compute/reference_state/set_diffusions_rhopower.py
+++ compute/reference_state/set_diffusions_rhopower.py
@@ -2,23 +2,21 @@
 # Created: 04/26/2024
 #
 # Purpose: modify a binary file (default name customfile) 
-# to contain heating profile that is confined to CZ, transitioning to
-# no heating of the RZ -- by default at the stable/unstable layer
-# transition
-# Must be run AFTER reference state (which includes the 
-# density/temperature) is generated
+# to contain diffusions (nu, kappa, eta) that go as rho^(power)
 
 # Parameters: output_dir (first argument), 
 
 # Command-line options:
 #
-# --alpha : ratio of RZ width to CZ width
-# --beta : ratio of bottom of CZ to top of CZ 
-# --delta : transition width between CZ and RZ via tanh matching
-# --jup : if "jup" is specified, RZ lies above CZ
-
 # --fname
 # File to read reference and save heating in (default customfile)
+# --power  : power of rho (defualt -0.5)
+# 
+# the following will have defaults set by [fname]_meta.txt
+# --alpha : ratio of RZ width to CZ width 
+# --beta : ratio of bottom of CZ to top of CZ 
+# --jup : if "jup" is specified, RZ lies above CZ
+
 
 import numpy as np
 import sys, os
@@ -54,7 +52,7 @@ for line in lines:
 f.close()
 
 # add in other default values
-kw_default.delta = 0.1316
+kw_default.power = -0.5
 
 # overwrite defaults
 kw = update_dict(kw_default, clas)
@@ -69,11 +67,9 @@ if kw.jup: # RZ above CZ
     rt = rbrz = rtcz
     rtrz = rbrz + kw.alpha
     rmin, rmax = rbcz, rtrz
-    the_sign = -1.
 else: # CZ above RZ
     rt = rtrz = rbcz
     rbrz = rtrz - kw.alpha
-    rmin, rmax = rbrz, rtcz
     the_sign = +1.
 
 # Open and read the hopefully already existing reference file!
@@ -83,22 +79,17 @@ eq.read(the_file)
 r = eq.radius
 nr = eq.nr
 rho = eq.functions[0]
-tmp = eq.functions[3]
-smooth = 0.5*(1.0 + the_sign*np.tanh((r - rt)/kw.delta)) # "detects" CZ
+diffusion = rho**(kw.power)
 
-heat = rho*tmp*smooth # used to subtract off "top" value in dimensional
-    # version. But now realize that top value was at 6.887 x 10^10 cm,
-    # way higher than I actually used (so effectively zero). Second
-    # paper reported it right. 
 
-# compute nonradiative flux
-Fnr = 1./r**2.*indefinite_integral(heat*r**2., r, rbcz)
-heat_norm = definite_integral(Fnr*r**2, r, rbcz, rtcz)
-heat_norm /= 1./3.*(rtcz**3. - rbcz**3.)
-heat /= heat_norm
+diffusion_norm = definite_integral(diffusion*r**2, r, rbcz, rtcz)
+diffusion_norm /= 1./3.*(rtcz**3. - rbcz**3.)
+diffusion /= diffusion_norm
 
 print(buff_line)
-print("Computed heating for RZ-CZ, profile ~ rho*T, joined with a tanh")
+print("Computed diffusions (nu, kappa, and etea) for RZ-CZ system,")
+print("diffusion ~ rho^%0.3f" %kw.power)
+print("normalized by CZ volume integral")
 if kw.jup:
     print ("geometry : Jovian (RZ atop CZ)")
 else:
@@ -111,24 +102,23 @@ if kw.jup:
 else:
     print("   (rbrz, rtrz=rbcz, rtrz): (%1.3f, %1.3f, %1.3f)"\
             %(rbrz,rtrz,rtcz))
-print("delta_heat : %1.4f" %kw.delta)
 print(buff_line)
 
 # Now write to file using the equation_coefficients framework
-print("Setting f_6 and c_10")
-# make an executive decision here (since normally f_6 integrates to 1)
-# leave f_6 "properly" normalized and later, set c_10 to Ek / Pr. 
-eq.set_function(heat, 6)
-eq.set_constant(1., 10) 
+print("Setting f_3, f_5, and f_7")
+for i in [3, 5, 7]:
+    eq.set_function(diffusion, i)
 
-print("Writing the heating to %s" %the_file)
+print("Writing the diffusions to %s" %the_file)
 print(buff_line)
 eq.write(the_file)
 
 # record what we did in the meta file
 f = open(dirname + '/' + metafile, 'a')
 
-f.write("Also added a heating profile with the folowing attributes\n")
+f.write("Also added custom diffusion profiles with the folowing attributes\n")
+f.write("diffusion ~ rho^%0.3f\n" %kw.power)
+f.write("normalized by CZ volume integral")
 if kw.jup:
      f.write("geometry : Jovian (RZ atop CZ)\n")
 else:
@@ -141,8 +131,7 @@ if kw.jup:
 else:
     f.write("   (rbrz, rtrz=rbcz, rtrz): (%1.3f, %1.3f, %1.3f)\n"\
             %(rbrz,rtrz,rtcz))
-f.write("delta_heat : %1.4f\n" %kw.delta)
 f.write(buff_line)
 f.close()
-print("Writing the heating metadata to %s" %metafile)
+print("Writing the diffusion metadata to %s" %metafile)
 print(buff_line)
