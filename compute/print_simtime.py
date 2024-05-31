c14ed3c6b3d4b96eb53b3ac95b28ad98186ac4a8
Loren <loren.matilsky@colorado.edu>
2024-05-30 18:11:05 -0700

    cleaned up sim time

--- compute/print_simtime.py
+++ compute/print_simtime.py
@@ -34,7 +34,39 @@ kw = update_dict(kw_default, clas)
 
 # now print the simulation time
 print (buff_line)
-if kw.gav:
+if kw.sslice:
+    sslice_dir = dirname + '/Shell_Slices/'
+    file_list, int_file_list, nfiles = get_file_lists_all(sslice_dir)
+
+    f1 = file_list[0]
+    f2 = file_list[-1]
+
+    a1 = Shell_Slices(sslice_dir + f1, '')
+    a2 = Shell_Slices(sslice_dir + f2, '')
+
+    t1 = a1.time[0]
+    t2 = a2.time[-1]
+
+    iter1 = a1.iters[0]
+    iter2 = a2.iters[-1]
+    print ("simtime(): Got timing info from Shell_Slices/ directory")
+    print ("iters:", f1, " to ", f2)
+    print ("times: %1.3e to %1.3e (simulation units)" %(t1, t2))
+
+elif kw.gtr:
+    if kw.the_file == None:
+        kw.the_file = get_widest_range_file(dirname + '/data/', 'G_Avgs_trace')
+    di = get_dict(kw.the_file)
+    times = di['times']
+    iters = di['iters']
+    t1, t2 = times[0], times[-1]
+    iter1, iter2 = iters[0], iters[-1]
+    print ("simtime(): Got timing info from data/*trace_G_Avgs* file")
+    print ("fname = ", kw.the_file)
+    print ("iters:", str(iter1).zfill(8), " to ", str(iter2).zfill(8))
+    print ("times: %1.3e to %1.3e (simulation units)" %(t1, t2))
+
+elif kw.gav:
     gav_dir = dirname + '/G_Avgs/'
     file_list, int_file_list, nfiles = get_file_lists_all(gav_dir)
 
@@ -51,42 +83,14 @@ if kw.gav:
     iter2 = a2.iters[-1]
     print ("simtime(): Got timing info from G_Avgs/ directory")
     print ("iters:", f1, " to ", f2)
+    print ("times: %1.3e to %1.3e (simulation units)" %(t1, t2))
 
-elif kw.sslice:
-    sslice_dir = dirname + '/Shell_Slices/'
-    file_list, int_file_list, nfiles = get_file_lists_all(sslice_dir)
-
-    f1 = file_list[0]
-    f2 = file_list[-1]
-
-    a1 = Shell_Slices(sslice_dir + f1, '')
-    a2 = Shell_Slices(sslice_dir + f2, '')
-
-    t1 = a1.time[0]
-    t2 = a2.time[-1]
-
-    iter1 = a1.iters[0]
-    iter2 = a2.iters[-1]
-    print ("simtime(): Got timing info from Shell_Slices/ directory")
-    print (f1, " to ", f2)
-
-elif kw.gtr:
-    if kw.the_file == None:
-        kw.the_file = get_widest_range_file(datadir, 'G_Avgs_trace')
-    di = get_dict(the_file)
-    times = di['times']
-    iters = di['iters']
-    t1, t2 = times[0], times[-1]
-    iter1, iter2 = iters[0], iters[-1]
-    print ("simtime(): Got timing info from data/*trace_G_Avgs* file")
-    print ("fname = ", kw.the_file)
-
-# calculate simulation time (seconds)
+# calculate simulation time (sim units)
 simtime = t2 - t1
 
 # run time in millions of iterations
 print (buff_line)
-print ('Delta_t = %7.2f M iter' %((iter2 - iter1)/1.0e6))
+print ('Delta_t = %7.3f M iter' %((iter2 - iter1)/1.0e6))
 
 # run time in thermal diffusion times
 fmt = "%1.3e"
@@ -101,10 +105,6 @@ if rotation:
 # more stuff in verbose mode
 if kw.verbose:
     print (buff_line)
-    print (("Delta_t = " + fmt + " sec") %simtime)
-    print (("Delta_t = " + fmt + " days") %(simtime/86400.))
-
-    print (buff_line)
     print ('iter1 = %08i' %iter1)
     print ('iter2 = %08i' %iter2)
 
@@ -117,8 +117,8 @@ if kw.verbose:
 
 # Print the various time scales
 print (buff_line)
-print (("1 TDT      = " + fmt + " sec = " + fmt + " days" ) %(eq.tdt, eq.tdt/86400.))
+print (("1 TDT      = " + fmt + " simulation units") %eq.tdt)
 if rotation:
-    print (("1 rotation = " + fmt + " sec = " + fmt + " days" ) %(eq.prot, eq.prot/86400.))
+    print (("1 rotation = " + fmt + " simulation units") %eq.prot)
     print(("1 TDT      = " + fmt + " rotations") %(eq.tdt/eq.prot))
 print (buff_line)
