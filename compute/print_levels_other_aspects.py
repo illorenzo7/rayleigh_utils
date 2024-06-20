fdde89f4140f351551a976fe3b6ae5a736097fcc
Loren <loren.matilsky@colorado.edu>
2024-06-20 13:04:32 -0700

    updated how aspect ratio is interpreted

--- compute/print_levels_other_aspects.py
+++ compute/print_levels_other_aspects.py
@@ -9,18 +9,25 @@ sys.path.append(os.environ['raco'])
 
 from common import *
 
-#args = sys.argv[1:]
-#nargs = len(args)
-#for i in range(nargs):
-#    arg = args[i]
-#    if arg == '--fname':
-#        custom_name = args[i+1]
+jup = False # if True, aspect is considered to be on top (RZ on top), not bottom
+args = sys.argv[1:]
+nargs = len(args)
+for i in range(nargs):
+    arg = args[i]
+    if arg == '--jup':
+        jup = True
 #    elif arg == '--crb':
 #        custom_name = 'custom_reference_binary'
 
 aspect = float(sys.argv[1]) # depth of top layer over bottom layer
 base_levels = np.array([0.05, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 0.95])
-levsbot = 1./(1.+aspect)*base_levels
-levstop = aspect/(1.+aspect)*base_levels + 1./(1.+aspect)
-all_levels = list(levsbot) + [1./(1.+aspect)] + list(levstop)
+if jup:
+    levsbot = 1./(1.+aspect)*base_levels
+    levstop = aspect/(1.+aspect)*base_levels + 1./(1.+aspect)
+    levmid = 1./(1.+aspect)
+else:
+    levsbot = aspect/(1.+aspect)*base_levels
+    levstop = 1./(1.+aspect)*base_levels + aspect/(1.+aspect)
+    levmid = aspect/(1.+aspect)
+all_levels = list(levsbot) + [levmid] + list(levstop)
 print(all_levels)
