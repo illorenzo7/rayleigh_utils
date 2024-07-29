66010c5514e1c585c36dd7fc6fe78b34045ced7d
Loren <loren.matilsky@colorado.edu>
2024-07-29 12:47:58 -0700

    made lscript_all handle rome nodes too

--- bash/lscript_all.sh
+++ bash/lscript_all.sh
@@ -1,5 +1,5 @@
 # make 4 $fname files, one for each node type
-declare -a arr=('bro' 'has' 'ivy' 'san' 'sky_ele' 'cas_ait')
+declare -a arr=('bro' 'has' 'ivy' 'san' 'sky_ele' 'cas_ait' 'mil_ait' 'rom_ait')
 
 for modeltype in "${arr[@]}"
 do 
@@ -27,6 +27,10 @@ do
     then
         ncpus=40
         execext='.avx512'
+    elif [ $modeltype == 'mil_ait' ] || [ $modeltype == 'rom_ait' ] 
+    then
+        ncpus=128
+        execext='.rome2.28'
     fi
 
     nprocs=$2
@@ -51,11 +55,20 @@ do
     echo >> $fname
 
     echo "module purge" >> $fname
-    echo "module load mpi-hpe" >> $fname
-    echo "module load comp-intel" >> $fname
+    if [ $modeltype == 'mil_ait' ] || [ $modeltype == 'rom_ait' ]
+    then
+        echo "module load gcc/13.2 mpi-hpe/mpt" >> $fname
+        echo "fftwlib=/home4/nfeather/software/fftw-3.3.10-Rome/" >> $fname
+        echo "blaslib=/home4/nfeather/software/OpenBLAS-0.3.27-Rome/" >> $fname
+        echo "export LD_LIBRARY_PATH=\$blaslib\\lib:\$fftwlib\\lib:\$LD_LIBRARY_PATH " >> $fname
+    else
+        echo "module load mpi-hpe" >> $fname
+        echo "module load comp-intel" >> $fname
+    fi
 
     echo >> $fname
 
+    echo "export OMP_NUM_THREADS=1" >> $fname
     echo "export MPI_LAUNCH_TIMEOUT=40" >> $fname
     echo "export PATH=\$PATH:/u/scicon/tools/bin" >> $fname
     echo "export MPI_BUFS_PER_PROC=256" >> $fname
