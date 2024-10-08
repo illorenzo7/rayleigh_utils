0713273fb6a00114c3708534b4e8b6838e3ba946
Loren <loren.matilsky@colorado.edu>
2024-10-08 15:59:19 -0700

    Barrier() call seems to work now, after system flux

--- compute/test_mpi.py
+++ compute/test_mpi.py
@@ -4,12 +4,15 @@
 ##################################################################
 ##################################################################
 
-# initialize communication
+# initialize communication and import modules
 from mpi4py import MPI
+import numpy as np
+import sys
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 
-comm.Barrier()
+comm.Barrier() # sync processes
+
 if rank == 0:
     print ("=============")
     nproc = comm.Get_size()
@@ -21,13 +24,15 @@ if rank == 0:
 
 # modules needed by everyone
 import numpy as np
+
+comm.Barrier() # sync processes
+
 a = np.random.rand(100)
 mean = np.mean(a)
 print ("rank %i checking in. I computed the mean of 100 random numbers: %.3f" %(rank, mean))
-# data type and reading function
 
-# Make sure everyone computes their mean before rank 0 collects it
-comm.Barrier()
+sys.stdout.flush()
+comm.Barrier() # sync processes
 
 # proc 0 now collects the results from each process
 if rank == 0:
@@ -38,13 +43,9 @@ if rank == 0:
             mean = comm.recv(source=j)
         totmean += mean
     totmean /= nproc
-else: # other processes send their data
-    comm.send(mean, dest=0)
 
-# Make sure proc 0 collects all data
-comm.Barrier()
-
-# proc 0 saves the data
-if rank == 0:
     print("=============")
     print("rank 0 here: the mean of %i means = %.3f" %(nproc,totmean))
+
+else: # other processes send their data
+    comm.send(mean, dest=0)
