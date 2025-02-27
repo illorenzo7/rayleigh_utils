54b4fa6c43c9b520f8035465a8f85a9ca5823466
Loren <loren.matilsky@colorado.edu>
2025-02-27 13:13:44 -0800

    start the purge of kwargs --> kw

--- compute/reference_state/polytrope.py
+++ compute/reference_state/polytrope.py
@@ -9,13 +9,13 @@ from common import g_univ, sun
 from cla_util import *
 
 
-compute_polytrope2_kwargs_default = dict({'Nrho': 3, 'r0': sun.rbcz, 'r1': None, 'rho0': sun.rho_bcz, 'T0': sun.tmp_bcz, 'poly_mass': msun, 'poly_n': 1.5, 'gas_constant_star': sun.gas_constant})
+kw_compute_polytrope2_default = dict({'nrho': 3, 'r0': sun.rbcz, 'r1': None, 'rho0': sun.rho_bcz, 'T0': sun.tmp_bcz, 'poly_mass': msun, 'poly_n': 1.5, 'gas_constant_star': sun.gas_constant})
 
-def compute_polytrope2(**kwargs):
-    # overwrite defaults
-    kw = update_dict(compute_polytrope2_kwargs_default, kwargs)
+def compute_polytrope2(**kw):
     # check for bad keys
-    find_bad_keys(compute_polytrope2_kwargs_default, kwargs, 'compute_polytrope2()')
+    find_bad_keys(kw_compute_polytrope2_default, kw, 'compute_polytrope2()')
+    # overwrite defaults
+    kw = update_dict(kw_compute_polytrope2_default, kw)
 
     poly_a = g_univ*kw.poly_mass/((kw.poly_n + 1)*kw.gas_constant_star*kw.T0*kw.r0)
     nr = 10000 # make this grid super fine
@@ -23,8 +23,8 @@ def compute_polytrope2(**kwargs):
         rmax = kw.r0*poly_a/(poly_a - 1) # at rmax, rho = 0
         rtmp = np.linspace(kw.r0, rmax, nr)
         rho_ratio = (poly_a*(kw.r0/rtmp) + (1 - poly_a))**kw.poly_n
-        Nrho_tmp = np.log(1/rho_ratio)
-        ir1 = np.argmin(np.abs(Nrho_tmp - kw.Nrho))
+        nrho_tmp = np.log(1/rho_ratio)
+        ir1 = np.argmin(np.abs(nrho_tmp - kw.nrho))
         kw.r1 = rtmp[ir1]
 
     r = np.linspace(kw.r1, kw.r0, nr) 
