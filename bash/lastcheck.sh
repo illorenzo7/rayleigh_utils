df4371f6f83797f9b3a882b74df9a6eda3486569
Loren <loren.matilsky@colorado.edu>
2025-07-30 13:44:32 -0700

    lastcheck routine works

--- bash/lastcheck.sh
+++ bash/lastcheck.sh
@@ -9,10 +9,6 @@ while IFS= read -r line; do
     if (( line_count == 0 )); then
         # Assign the line to checkpoint_iter
         checkpoint_iter=$line
-        # Take the absolute value of checkpoint_iter
-        if (( checkpoint_iter < 0 )); then
-            checkpoint_iter=$(( -checkpoint_iter )) # Multiply by -1 to get the absolute value
-        fi
         checkpoint_name=$checkpoint_iter
     elif (( line_count == 1 )); then
         checkpoint_name=quicksave_$line
@@ -20,5 +16,7 @@ while IFS= read -r line; do
     ((line_count++)) # Increment line count for each line read
 done < Checkpoints/last_checkpoint
 
-echo "Variable 1 (integer): $checkpoint_iter"
-echo "Variable 2 (integer): $checkpoint_name"
+# make tar ball
+echo "Creating lastcheck_$checkpoint_iter.tar"
+echo "........................."
+tar cvf lastcheck_$checkpoint_iter.tar Checkpoints/last_checkpoint Checkpoints/checkpoint_log Checkpoints/$checkpoint_name
