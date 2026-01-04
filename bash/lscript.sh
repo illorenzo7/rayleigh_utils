797e9141106750bec9a27917d5da809646d32812
Loren <loren.matilsky@colorado.edu>
2026-01-04 13:10:49 -0800

    fixed up lscript.sh and working on lscript_all.sh

--- bash/lscript.sh
+++ bash/lscript.sh
@@ -2,26 +2,22 @@ echo "#PBS -S /bin/bash" > lscript
 echo "#PBS -N $1" >> lscript
 
 modeltype=$2
-if [ $modeltype == 'bro' ]
+if [ $modeltype == 'bro_ele' ]
 then
     ncpus=28
     execext='.avx2'
-elif [ $modeltype == 'has' ]
-then
-    ncpus=24
-    execext='.avx2'
-elif [ $modeltype == 'ivy' ] 
-then
-    ncpus=20
-    execext='.avx'
-elif [ $modeltype == 'san' ] 
-then
-    ncpus=16
-    execext='.avx'
 elif [ $modeltype == 'sky_ele' ] || [ $modeltype == 'cas_ait' ] 
 then
     ncpus=40
     execext='.avx512'
+elif [ $modeltype == 'rom_ait' ] 
+then
+    ncpus=128
+    execext='.rome.2.28'
+elif [ $modeltype == 'mil_ait' ] 
+then
+    ncpus=128
+    execext='.milan.2.28'
 else
     echo "unknown model type $modeltype"
 fi
@@ -37,19 +33,32 @@ fi
 nprow=`python $rau/bash/nprow.py $nprocs`
 npcol=`python $rau/bash/npcol.py $nprocs`
 
+group=${4:-s7614} # charge it to s7614 by default
+
 echo "#PBS -l select=$select:ncpus=$ncpus:model=$modeltype" >> lscript
 echo "#PBS -q long" >> lscript
 echo "#PBS -l walltime=120:00:00" >> lscript
 echo "#PBS -j oe" >> lscript
-echo "#PBS -W group_list=s2328" >> lscript
+echo "#PBS -W group_list=$group" >> lscript
 echo "#PBS -m e" >> lscript
 echo "#PBS -l site=needed=/home1+/nobackupp17" >> lscript
 
 echo >> lscript
 
 echo "module purge" >> lscript
-echo "module load mpi-hpe" >> lscript
-echo "module load comp-intel" >> lscript
+
+if [ $modeltype == 'bro_ele' ] || [ $modeltype == 'sky_ele' ] || [ $modeltype == 'cas_ait' ] 
+then
+    echo "module load mpi-hpe" >> lscript
+    echo "module load comp-intel" >> lscript
+elif [ $modeltype == 'sky_ele' ] || [ $modeltype == 'sky_ele' ] 
+then
+    echo "module load gcc/13.2 mpi-hpe/mpt" >> lscript
+    echo "fftwlib=/home4/nfeather/software/fftw-3.3.10-Rome/" >> lscript
+    echo "blaslib=/home4/nfeather/software/OpenBLAS-0.3.27-Rome/" >> lscript
+    echo "export LD_LIBRARY_PATH=$blaslib\lib:$fftwlib\lib:$LD_LIBRARY_PATH" >> lscript
+    echo "export OMP_NUM_THREADS=1" >> lscript
+fi
 
 echo >> lscript
 
