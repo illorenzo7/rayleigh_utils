8566d692a6900b0494e62842e9667a968d5fbaaa
Loren <loren.matilsky@colorado.edu>
2026-06-26 03:00:39 -0700

    new spec 2d routine almost ready

--- plot/spec/spec2d.py
+++ plot/spec/spec2d.py
@@ -24,11 +24,11 @@ dirname = clas0.dirname
 dirname_stripped = strip_dirname(dirname)
 
 # SPECIFIC ARGS
-kw_default = dotdict(dict({'irvals': 0, 'rvals': None, 'rav': True, 'varname': 'vr', 'groupname': None, 'prepend': False, 'dpi': 300, 'minmaxlm': None, 'minmaxl': None, 'minmaxm': None, 'minmax': None, 'log': True}))
+kw_default = dotdict(dict({'irvals': 0, 'rvals': None, 'rav': True, 'varname': 'vr', 'groupname': None, 'prepend': False, 'dpi': 300, 'minmaxlm': None, 'minmaxl': None, 'minmaxm': None, 'minmax': None, 'log': False}))
 
 # fig dimensions
 # 1 row of 3 figures side by side: Power vs lm, Power vs l, Power vs m
-fig_dimensions = dotdict({'width_inches': 10., 'sub_aspect': 1, 'sub_margin_right_inches': 1/2, 'nrow': 1, 'ncol': 3, 'margin_top_inches': 1})
+fig_dimensions = dotdict({'width_inches': 10., 'sub_aspect': 1, 'sub_margin_right_inches': 1/2, 'nrow': 2, 'ncol': 3, 'margin_top_inches': 1.25})
 
 # Rayleigh data dir
 radatadir = dirname + '/Shell_Spectra/'
@@ -59,7 +59,8 @@ lut = di['lut']
 # get desired sampling locations
 sliceinfo = get_sliceinfo(dirname, 'Shell_Spectra')
 if kw.rav: # just average over radial direction (simple)
-    vals = np.mean(vals, axis=2, keepdims=True)
+    rvals_nd = sliceinfo.rvals.reshape((1, 1, sliceinfo.nr, 1))
+    vals = np.mean(vals*rvals_nd**2, axis=2, keepdims=True)/np.mean(sliceinfo.rvals**2)
     nrvals = 1
 else: # plotting specific level
     # first the indices must be an array
@@ -69,7 +70,7 @@ else: # plotting specific level
     if not kw.rvals is None: # rvals have been set directly
         # need the available sampling locations
         if isall(kw.rvals):
-            kw.irvals = np.arange(sliceinfo.nrvals)
+            kw.irvals = np.arange(sliceinfo.nr)
         else:
             kw.rvals = make_array(kw.rvals)
             kw.irvals = inds_from_vals(sliceinfo.rvals, kw.rvals)
@@ -82,15 +83,17 @@ print (buff_line)
 
 # print varnames
 print (buff_line)
+texlabels = dotdict({'v': r'$\mathbf{v}$', 'b': r'$\mathbf{B}$', 'om': r'$\mathbf{\omega}$', 'j': r'$\mathbf{\mathcal{J}}$'})
 
 if kw.groupname is None: # it's a simple variable
-    varlabel = kw.varname
-    print ("plotting varname = " + kw.varname)
+    varlabel_title, varlabel = get_label(kw.varname)
+    print ("plotting sum(" + kw.varname + ")^2")
     qval = var_indices[kw.varname]
     field_all_radii = vals[..., lut[qval]]
 else:
     varlabel = kw.groupname
-    print ("plotting sum(" + kw.varname + ")^2")
+    varlabel_title = texlabels[kw.groupname]    
+    print ("plotting sum(" + kw.groupname + ")^2")
     the_group = get_quantity_group(kw.groupname, clas0['magnetism'])
     qvals = the_group['qvals']
     field_all_radii = np.zeros_like(vals[...,0])
@@ -99,7 +102,7 @@ else:
 
 # make the plotting directory
 iter1, iter2 = get_iters_from_file(kw.the_file)
-plotdir = clas0['plotdir'] + '/spec2d_' + clas0['tag']
+plotdir = clas0['plotdir'] + '/spec2d' + clas0['tag']
 plotdir = my_mkdir(plotdir)
 
 # prepare the grid
@@ -117,8 +120,10 @@ for irval in range(nrvals):
     # figure out the r-label
     if kw.rav:
         rlabel = 'rav'
+        rlabel_title = 'volume average'
     else:
         rlabel = 'rval%.3f' %rval
+        rlabel_title = '%.3f' %rval
 
     savename += '_' + rlabel + '.png'
 
@@ -126,7 +131,6 @@ for irval in range(nrvals):
         savename = dirname_stripped + '_' + savename
 
     savefile = plotdir + '/' + savename
-    print("plotting...")
 
     # now make the plot
     fig, axs, fpar = make_figure(**kw_make_figure)
@@ -137,7 +141,8 @@ for irval in range(nrvals):
     my_pcolormesh(field, fig, ax, **kw_my_pcolormesh)
     ax.set_xlabel(r'$\ell$', fontsize=fontsize)
     ax.set_ylabel(r'$m$', fontsize=fontsize)
-    ax.set_title('2D power spectrum', fontsize=fontsize)
+    the_title = '2D power spectrum of |' + varlabel_title + r'$|^2$'
+    ax.set_title(the_title, fontsize=fontsize)
 
     lmin, lmax = ax.get_xlim()
     mmin, mmax = ax.get_ylim()
@@ -148,8 +153,9 @@ for irval in range(nrvals):
     ax.bar(lvals, spec_l, width=1)
     ax.set_xlim(lmin, lmax)
     ax.set_xlabel(r'$\ell$', fontsize=fontsize)
-    ax.set_ylabel('Power', fontsize=fontsize)
-    ax.set_title(r'$\Sigma_m$' + ' Power', fontsize=fontsize)
+    ax.set_ylabel('power', fontsize=fontsize)
+    the_title = r'$\ell$' + '-power: ' + r'$\Sigma_{m=0}^\ell|$' + varlabel_title + r'$|^2$'
+    ax.set_title(the_title, fontsize=fontsize)
 
     # make power spectrum vs m
     ax = axs[0, 2]
@@ -157,15 +163,52 @@ for irval in range(nrvals):
     ax.bar(mvals, spec_m, width=1)
     ax.set_xlim(mmin, mmax)
     ax.set_xlabel(r'$m$', fontsize=fontsize)
-    ax.set_ylabel('Power', fontsize=fontsize)
-    ax.set_title(r'$\Sigma_\ell$' + ' Power', fontsize=fontsize)
+    ax.set_ylabel('power', fontsize=fontsize)
+    the_title = r'$m$' + '-power: ' + r'$\Sigma_{\ell=m}^{\ell_{\rm max}}|$' + varlabel_title + r'$|^2$'
+
+    ax.set_title(the_title, fontsize=fontsize)
+
+    # find ell = m power
+    ax = axs[1, 0]
+    spec_sectoral = np.diag(field)
+    ax.bar(lvals, spec_sectoral, width=1)
+    ax.set_xlim(lmin, lmax)
+    ax.set_xlabel(r'$\ell=m$', fontsize=fontsize)
+    ax.set_ylabel('power', fontsize=fontsize)
+    the_title = 'sectoral power: ' + r'$|$' + varlabel_title + r'$|^2(\ell=m)$'
+    ax.set_title(the_title, fontsize=fontsize)
+
+    # make sectoral power spectrum vs ell
+    field_sectoral_removed = np.copy(field)
+    for lmval in lvals:
+        field_sectoral_removed[lmval, lmval] = 0.
+
+    ax = axs[1, 1]
+    spec_l = np.sum(field_sectoral_removed, axis=1)
+    ax.bar(lvals, spec_l, width=1)
+    ax.set_xlim(lmin, lmax)
+    ax.set_xlabel(r'$\ell$', fontsize=fontsize)
+    ax.set_ylabel('power', fontsize=fontsize)
+    the_title = 'nonsectoral ' + r'$\ell$' + '-power: ' + r'$\Sigma_{m=0}^{\ell-1}|$' + varlabel_title + r'$|^2$'
+    ax.set_title(the_title, fontsize=fontsize)
+
+    # make power spectrum vs m
+    ax = axs[1, 2]
+    spec_m = np.sum(field_sectoral_removed, axis=0)
+    ax.bar(mvals, spec_m, width=1)
+    ax.set_xlim(mmin, mmax)
+    ax.set_xlabel(r'$m$', fontsize=fontsize)
+    ax.set_ylabel('power', fontsize=fontsize)
+    the_title = 'nonsectoral ' + r'$m$' + '-power: ' + r'$\Sigma_{\ell=m+1}^{\ell_{\rm max}}|$' + varlabel_title + r'$|^2$'
+    ax.set_title(the_title, fontsize=fontsize)
 
     # now set main title
     t1, t2 = get_times_from_file(dirname, kw.the_file)
     time_string = get_time_string(dirname, t1=t1,t2=t2,SF=5)
 
     maintitle = dirname_stripped + '\n' +\
-            varlabel + '\n' +\
+            'var = ' + varlabel_title + '\n' +\
+            r'$r=$' + rlabel_title + '\n' +\
             time_string
     fig.text(0.5, 1 - 1/8/fpar['height_inches'], maintitle, ha='center', va='top', fontsize=fontsize)
 
